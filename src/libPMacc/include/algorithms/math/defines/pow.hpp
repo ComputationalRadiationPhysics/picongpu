4bf8dd743ed93ab7c713c5c08a7e851a38ec4058
René Widera <r.widera@hzdr.de>
2013-09-18 15:02:47 +0200

    add pow definition and double implementation
    - add specialisation for double (C++98 standard)

---
+++ src/libPMacc/include/algorithms/math/defines/pow.hpp
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2013 René Widera
+ *
+ * This file is part of libPMacc. 
+ * 
+ * libPMacc is free software: you can redistribute it and/or modify 
+ * it under the terms of of either the GNU General Public License or 
+ * the GNU Lesser General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version. 
+ * libPMacc is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License and the GNU Lesser General Public License 
+ * for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * and the GNU Lesser General Public License along with libPMacc. 
+ * If not, see <http://www.gnu.org/licenses/>. 
+ */
+
+#pragma once
+
+namespace PMacc
+{
+namespace algorithms
+{
+
+namespace math
+{
+
+namespace detail
+{
+
+template<typename T1, typename T2>
+struct Pow;
+
+} //namespace detail
+
+
+/** Raised the base to the power exponent
+ *
+ * @param base base value
+ * @param exponent power exponent 
+ * @return base rased to the power exponent 
+ */
+template<typename T1, typename T2>
+HDINLINE static typename detail::Pow< T1, T2 >::result pow(const T1& base,const T2& exponent)
+{
+    return detail::Pow< T1, T2 > ()(base, exponent);
+}
+
+} //namespace math
+} //namespace algorithms
+}//namespace PMacc
