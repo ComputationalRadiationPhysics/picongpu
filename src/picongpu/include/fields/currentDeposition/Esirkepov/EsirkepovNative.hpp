8f41091846210406b62f52cee7ccc449b933d307
René Widera <r.widera@hzdr.de>
2014-01-21 12:47:15 +0100

    add documentation

--- src/picongpu/include/fields/currentDeposition/Esirkepov/EsirkepovNative.hpp
+++ src/picongpu/include/fields/currentDeposition/Esirkepov/EsirkepovNative.hpp
@@ -147,6 +147,7 @@ struct EsirkepovNative
         {
             float_X W = DS(line.pos0.z() - i, line.pos1.z() - i) * tmp;
             accumulated_J += -this->charge * (float_X(1.0) / float_X(CELL_VOLUME * DELTA_T)) * W * cellEdgeLength;
+            /*this branch differgence speedup this algorith because we had fewer collisions*/
             if(accumulated_J!=float_X(0.0))
                 atomicAddWrapper(&((*cursorJ(0, 0, i)).z()), accumulated_J);
         }
