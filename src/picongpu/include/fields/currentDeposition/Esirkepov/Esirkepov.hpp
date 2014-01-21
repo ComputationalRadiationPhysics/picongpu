6210cd40a3a69b21575bec559b6899ae9e2567a3
René Widera <r.widera@hzdr.de>
2014-01-21 11:48:00 +0100

    improve speed of Esirkepov current deposition

--- src/picongpu/include/fields/currentDeposition/Esirkepov/Esirkepov.hpp
+++ src/picongpu/include/fields/currentDeposition/Esirkepov/Esirkepov.hpp
@@ -202,7 +202,8 @@ struct Esirkepov
         {
             float_X W = DS(line.pos0.z() - i, line.pos1.z() - i) * tmp;
             accumulated_J += -this->charge * (float_X(1.0) / float_X(CELL_VOLUME * DELTA_T)) * W * cellEdgeLength;
-            atomicAddWrapper(&((*cursorJ(0, 0, i)).z()), accumulated_J);
+            if(accumulated_J!=float_X(0.0))
+                atomicAddWrapper(&((*cursorJ(0, 0, i)).z()), accumulated_J);
         }
     }
 
