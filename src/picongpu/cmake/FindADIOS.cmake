7e0436e6954abeecd03fdc524b39189f89e2ca0f
Axel Huebl <axel.huebl@web.de>
2014-03-13 00:33:31 +0100

    Try to parse adios_config output
    should create generic cmake library lists with
    absolute paths

--- src/picongpu/cmake/FindADIOS.cmake
+++ src/picongpu/cmake/FindADIOS.cmake
@@ -1,6 +1,15 @@
 # - Find ADIOS library, routines for scientific, parallel IO
 #   https://www.olcf.ornl.gov/center-projects/adios/
 #
+# Use this module by invoking find_package with the form:
+#   find_package(ADIOS
+#     [version] [EXACT]     # Minimum or EXACT version, e.g. 1.6.0
+#     [REQUIRED]            # Fail with an error if ADIOS or a required
+#                           #   component is not found
+#     [QUIET]               # ...
+#     [COMPONENTS <...>]    # Compiled in components, ignored
+#   )
+#
 # Module that finds the includes and libraries for a working ADIOS install.
 # This module invokes the `adios_config` script that should be installed with
 # the other ADIOS tools.
@@ -16,7 +25,7 @@
 #
 # Not used for now:
 #   ADIOS_DEFINITIONS    - Compiler definitions you should add with
-#                          add_definitons(${ADIOS_DEFINITIONS})
+#                          add_definitions(${ADIOS_DEFINITIONS})
 #
 
 ################################################################################
@@ -61,57 +70,83 @@ find_path(ADIOS_ROOT_DIR
   DOC "ADIOS ROOT location"
 )
 
-# we found something in ADIOS_ROOT
-if(ADIOS_ROOT_DIR)
-    message(STATUS "Found ADIOS: "${ADIOS_ROOT_DIR})
+# check `adios_config` program
+execute_process(COMMAND adios_config -l
+                OUTPUT_VARIABLE ADIOS_LINKFLAGS
+                RESULT_VARIABLE ADIOS_CONFIG_RETURN)
+if(NOT ADIOS_CONFIG_RETURN EQUAL 0)
+    set(ADIOS_CONFIG_RETURN FALSE)
+    message(STATUS "Can NOT find adios_config helper - check your PATH")
+else()
+    set(ADIOS_CONFIG_RETURN TRUE)
+    # trim trailing newlines
+    string(REGEX REPLACE "(\r?\n)+$" "" ADIOS_LINKFLAGS "${ADIOS_LINKFLAGS}")
+endif()
 
+# we found something in ADIOS_ROOT and adios_config works
+if(ADIOS_ROOT_DIR AND ADIOS_CONFIG_RETURN)
+    # ADIOS headers
     list(APPEND ADIOS_INCLUDE_DIRS ${ADIOS_ROOT_DIR}/include)
-    list(APPEND ADIOS_LIBRARIES ${ADIOS_ROOT_DIR}/lib/libadios.a)
 
-    # find mxml installation
-    find_path(MXML_ROOT_DIR
-      NAMES include/mxml.h lib/libmxml.so
-      PATHS ENV MXML_ROOT
-      DOC "MXML ROOT location (ADIOS dependency)"
-    )
+    # check for compiled in dependencies
+    message(STATUS "ADIOS linker flags (unparsed): ${ADIOS_LINKFLAGS}")
 
-    if(MXML_ROOT_DIR)
-        message(STATUS "Found MXML: "${MXML_ROOT_DIR})
+    # find all library paths -L
+    set(ADIOS_LIBRARY_DIRS "")# ${CMAKE_PREFIX_PATH}
+    string(REGEX MATCHALL "-L([A-Za-z_0-9/\\.-]+)" _ADIOS_LIBDIRS "${ADIOS_LINKFLAGS}")
+    foreach(_LIBDIR ${_ADIOS_LIBDIRS})
+        string(REPLACE "-L" "" _LIBDIR ${_LIBDIR})
+        list(APPEND ADIOS_LIBRARY_DIRS ${_LIBDIR})
+    endforeach()
 
-        list(APPEND ADIOS_INCLUDE_DIRS ${MXML_ROOT_DIR}/include)
-        list(APPEND ADIOS_LIBRARIES ${MXML_ROOT_DIR}/lib/libmxml.a)
+    #message(STATUS "ADIOS DIRS to look for libs: ${ADIOS_LIBRARY_DIRS}")
 
-    else(MXML_ROOT_DIR)
-        set(ADIOS_FOUND FALSE)
-        message(STATUS "Could NOT find MXML (ADIOS dependency)")
-    endif(MXML_ROOT_DIR)
+    # parse all -lname libraries and find an absolute path for them
+    string(REGEX MATCHALL "-l([A-Za-z_0-9\\.-]+)" _ADIOS_LIBS "${ADIOS_LINKFLAGS}")
 
-    # check for further dependencies (right now Dataspaces)
-    execute_process(COMMAND adios_config -l
-                    OUTPUT_VARIABLE ADIOS_LINKFLAGS)
-    message(STATUS "Additional linker flags for ADIOS: ${ADIOS_LINKFLAGS}")
+    foreach(_LIB ${_ADIOS_LIBS})
+        string(REPLACE "-l" "lib" _LIB ${_LIB})
 
-    # find dataspaces installation (if compiled in)
-    if("${ADIOS_LINKFLAGS}" MATCHES "ldspaces")
-        find_path(DATASPACES_ROOT_DIR
-          NAMES include/dataspaces.h lib/libdspaces.a
-          PATHS ENV DATASPACES_ROOT
-          DOC "DATASPACES ROOT location (ADIOS dependency)"
-        )
+        # find static lib: absolute path in -L then default
+        set(_LE "a")
+        unset(_LIB_DIR CACHE)
+        unset(_LIB_DIR)
+        set(_LIB_PREF "")
+        find_path(_LIB_DIR NAMES "${_LIB}.${_LE}" PATHS ${ADIOS_LIBRARY_DIRS})
+        if(NOT _LIB_DIR)
+            set(_LIB_PREF "lib/")
+            find_path(_LIB_DIR NAMES "${_LIB_PREF}${_LIB}.${_LE}")
+        endif(NOT _LIB_DIR)
 
-       if(DATASPACES_ROOT_DIR)
-            message(STATUS "Found Dataspaces: "${DATASPACES_ROOT_DIR})
+        # not found? find shared lib!
+        if(NOT _LIB_DIR)
+            set(_LE "so")
+            set(_LIB_PREF "")
+            find_path(_LIB_DIR NAMES "${_LIB}.${_LE}" PATHS ${ADIOS_LIBRARY_DIRS})
+            if(NOT _LIB_DIR)
+                set(_LIB_PREF "lib/")
+                find_path(_LIB_DIR NAMES "${_LIB_PREF}${_LIB}.${_LE}")
+            endif(NOT _LIB_DIR)
+        endif(NOT _LIB_DIR)
 
-            list(APPEND ADIOS_INCLUDE_DIRS ${DATASPACES_ROOT_DIR}/include)
-            list(APPEND ADIOS_LIBRARIES ${DATASPACES_ROOT_DIR}/lib/libdspaces.a)
-            list(APPEND ADIOS_LIBRARIES ${DATASPACES_ROOT_DIR}/lib/libdscommon.a)
-            list(APPEND ADIOS_LIBRARIES ${DATASPACES_ROOT_DIR}/lib/libdart.a)
-
-        else(DATASPACES_ROOT_DIR)
+        # found?
+        if(_LIB_DIR)
+            message(STATUS "Found ${_LIB} in ${_LIB_DIR}/${_LIB_PREF}")
+            list(APPEND ADIOS_LIBRARIES "${_LIB_DIR}/${_LIB_PREF}${_LIB}.${_LE}")
+        else(_LIB_DIR)
             set(ADIOS_FOUND FALSE)
-            message(STATUS "Could NOT find DATASPACES (ADIOS dependency)")
-        endif(DATASPACES_ROOT_DIR)
-    endif()
+            message(STATUS "ADIOS: Could NOT find library '${_LIB}.a'/'${_LIB}.so'")
+        endif(_LIB_DIR)
+
+    endforeach()
+
+    # simplify lists and check for missing components (not implemented)
+    set(ADIOS_MISSING_COMPONENTS "")
+    foreach(COMPONENT ${ADIOS_FIND_COMPONENTS})
+        string(TOUPPER ${COMPONENT} COMPONENT)
+        list(APPEND ADIOS_MISSING_COMPONENTS ${COMPONENT})
+    endforeach()
+    #message(STATUS "ADIOS required components: ${ADIOS_FIND_COMPONENTS}")
 
     # add the version string
     execute_process(COMMAND adios_config -v
@@ -119,9 +154,9 @@ if(ADIOS_ROOT_DIR)
     # trim trailing newlines
     string(REGEX REPLACE "(\r?\n)+$" "" ADIOS_VERSION "${ADIOS_VERSION}")
 
-else(ADIOS_ROOT_DIR)
+else(ADIOS_ROOT_DIR AND ADIOS_CONFIG_RETURN)
     set(ADIOS_FOUND FALSE)
-endif(ADIOS_ROOT_DIR)
+endif(ADIOS_ROOT_DIR AND ADIOS_CONFIG_RETURN)
 
 # unset checked variables if not found
 if(NOT ADIOS_FOUND)
