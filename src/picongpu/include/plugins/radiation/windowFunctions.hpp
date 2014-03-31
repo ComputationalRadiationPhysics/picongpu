e6ba50206fd0750dde1ab2c161599e60527b8916
Pausch <rp6038@hypnos2.fz-rossendorf.de>
2014-03-31 13:20:57 +0200

    fix whitespaces

--- src/picongpu/include/plugins/radiation/windowFunctions.hpp
+++ src/picongpu/include/plugins/radiation/windowFunctions.hpp
@@ -73,7 +73,7 @@ namespace picongpu
          *          sharp spacial boundaries
 	 */
 	float_X x = position_x - float_X(0.5)*L_x;
-	return float_X(math::abs(x) <= float_X(0.5)*L_x * (float_X(1.0)- 
+	return float_X(math::abs(x) <= float_X(0.5)*L_x * (float_X(1.0)-
 					   float_X(2.0)/L_x * math::abs(x) );
       }
     };
@@ -82,3 +82,4 @@ namespace picongpu
 
 
 }  /* namespace picongpu */
+
