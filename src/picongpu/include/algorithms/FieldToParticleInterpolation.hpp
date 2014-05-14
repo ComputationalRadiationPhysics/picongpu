fde9440bf6dd08ef2f010113be489e36779e7b27
René Widera <r.widera@hzdr.de>
2014-05-14 14:43:47 +0200

    refactor ShiftCoordinateSystem
    - refactor ShiftCoordinateSystem that we has well defined states of the new coordinate system
    - add functionality that every dimension can have its on defined particle shape support

--- src/picongpu/include/algorithms/FieldToParticleInterpolation.hpp
+++ src/picongpu/include/algorithms/FieldToParticleInterpolation.hpp
@@ -1,21 +1,21 @@
 /**
  * Copyright 2013 Axel Huebl, Heiko Burau, Rene Widera
  *
- * This file is part of PIConGPU. 
- * 
- * PIConGPU is free software: you can redistribute it and/or modify 
- * it under the terms of the GNU General Public License as published by 
- * the Free Software Foundation, either version 3 of the License, or 
- * (at your option) any later version. 
- * 
- * PIConGPU is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
- * GNU General Public License for more details. 
- * 
- * You should have received a copy of the GNU General Public License 
- * along with PIConGPU.  
- * If not, see <http://www.gnu.org/licenses/>. 
+ * This file is part of PIConGPU.
+ *
+ * PIConGPU is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PIConGPU is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PIConGPU.
+ * If not, see <http://www.gnu.org/licenses/>.
  */
 
 
@@ -31,10 +31,10 @@ namespace picongpu
 {
 
 /** interpolate field which are defined on a grid to a point inside of a grid
- * 
- * interpolate around of a point from -AssignmentFunction::support/2 to 
+ *
+ * interpolate around of a point from -AssignmentFunction::support/2 to
  * (AssignmentFunction::support+1)/2
- * 
+ *
  * \tparam GridShiftMethod functor which shift coordinate system that al value are
  * located on corner
  * \tparam AssignmentFunction AssignmentFunction which is used for interpolation
@@ -46,14 +46,14 @@ struct FieldToParticleInterpolation
     typedef typename T_Shape::ChargeAssignmentOnSupport AssignmentFunction;
     static const int supp = AssignmentFunction::support;
 
-    static const int lowerMargin = supp / 2;
+    static const int lowerMargin = supp / 2 ;
     static const int upperMargin = (supp + 1) / 2;
     typedef typename PMacc::math::CT::make_Int<simDim,lowerMargin>::type LowerMargin;
     typedef typename PMacc::math::CT::make_Int<simDim,upperMargin>::type UpperMargin;
 
     /*(supp + 1) % 2 is 1 for even supports else 0*/
     static const int begin = -supp / 2 + (supp + 1) % 2;
-    static const int end = supp / 2;
+    static const int end = begin+supp-1;
 
     template<class Cursor, class VecVector_ >
     HDINLINE float3_X operator()(Cursor field, const floatD_X & particlePos,
@@ -68,25 +68,27 @@ struct FieldToParticleInterpolation
          * is turned so that E_scalar does the interpolation for the z-component.
          */
 
-        /** _1[mpl::int_<0>()] means: 
+        /** _1[mpl::int_<0>()] means:
          * Create a functor which returns [0] applied on the first paramter.
          * Here it is: return the x-component of the field-vector.
          * _1[mpl::int_<0>()] is equivalent to _1[0] but has no runtime cost.
          */
 
+        typedef typename PMacc::math::CT::make_Int<simDim,supp>::type Supports;
+
         BOOST_AUTO(field_x, PMacc::cursor::make_FunctorCursor(field, _1[mpl::int_ < 0 > ()]));
         floatD_X pos_tmp(particlePos);
-        ShiftCoordinateSystem<supp>()(field_x, pos_tmp, fieldPos.x());
+        ShiftCoordinateSystem<Supports>()(field_x, pos_tmp, fieldPos.x());
         float_X result_x = InterpolationMethod::template interpolate<AssignmentFunction, begin, end > (field_x, pos_tmp);
 
         BOOST_AUTO(field_y, PMacc::cursor::make_FunctorCursor(field, _1[mpl::int_ < 1 > ()]));
         pos_tmp = particlePos;
-        ShiftCoordinateSystem<supp>()(field_y, pos_tmp, fieldPos.y());
+        ShiftCoordinateSystem<Supports>()(field_y, pos_tmp, fieldPos.y());
         float_X result_y = InterpolationMethod::template interpolate<AssignmentFunction, begin, end > (field_y, pos_tmp);
 
         BOOST_AUTO(field_z, PMacc::cursor::make_FunctorCursor(field, _1[mpl::int_ < 2 > ()]));
         pos_tmp = particlePos;
-        ShiftCoordinateSystem<supp>()(field_z, pos_tmp, fieldPos.z());
+        ShiftCoordinateSystem<Supports>()(field_z, pos_tmp, fieldPos.z());
         float_X result_z = InterpolationMethod::template interpolate<AssignmentFunction, begin, end > (field_z, pos_tmp);
 
         return float3_X(result_x, result_y, result_z);
@@ -98,7 +100,7 @@ namespace traits
 {
 
 /*Get margin of a solver
- * class must define a LowerMargin and UpperMargin 
+ * class must define a LowerMargin and UpperMargin
  */
 template<class AssignMethod, class InterpolationMethod>
 struct GetMargin<picongpu::FieldToParticleInterpolation<AssignMethod, InterpolationMethod> >
