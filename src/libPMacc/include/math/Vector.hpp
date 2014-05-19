7c4fe98adbc9f7dde20b2e395efd025584743bd6
René Widera <r.widera@hzdr.de>
2014-05-19 13:54:00 +0200

    add missing types to includes
    - include ConstVector.hpp
    - include compile-time/UInt.hpp

--- src/libPMacc/include/math/Vector.hpp
+++ src/libPMacc/include/math/Vector.hpp
@@ -26,8 +26,10 @@
 #include "math/vector/UInt.hpp"
 #include "math/vector/Size_t.hpp"
 #include "math/vector/Float.hpp"
+#include "math/ConstVector.hpp"
 #include "math/vector/compile-time/Vector.hpp"
 #include "math/vector/compile-time/Int.hpp"
 #include "math/vector/compile-time/Size_t.hpp"
+#include "math/vector/compile-time/UInt.hpp"
 
 #include "math/vector/Vector.tpp"
