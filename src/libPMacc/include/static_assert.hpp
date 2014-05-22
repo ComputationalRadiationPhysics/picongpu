1af3cad2940580aa07b316e1522e3dbc635da31d
René Widera <r.widera@hzdr.de>
2014-05-22 11:35:56 +0200

    refactor static assert
    use  BOOST_MPL_ASSERT_MSG assert to evaluate conditions

--- src/libPMacc/include/static_assert.hpp
+++ src/libPMacc/include/static_assert.hpp
@@ -1,29 +1,30 @@
 /**
- * Copyright 2013 Axel Huebl, Felix Schmitt, Rene Widera
+ * Copyright 2013-2014 Axel Huebl, Felix Schmitt, Rene Widera
  *
- * This file is part of libPMacc. 
- * 
- * libPMacc is free software: you can redistribute it and/or modify 
- * it under the terms of of either the GNU General Public License or 
- * the GNU Lesser General Public License as published by 
- * the Free Software Foundation, either version 3 of the License, or 
- * (at your option) any later version. 
- * libPMacc is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
- * GNU General Public License and the GNU Lesser General Public License 
- * for more details. 
- * 
- * You should have received a copy of the GNU General Public License 
- * and the GNU Lesser General Public License along with libPMacc. 
- * If not, see <http://www.gnu.org/licenses/>. 
- */ 
- 
+ * This file is part of libPMacc.
+ *
+ * libPMacc is free software: you can redistribute it and/or modify
+ * it under the terms of of either the GNU General Public License or
+ * the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libPMacc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License and the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and the GNU Lesser General Public License along with libPMacc.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
 
-#ifndef STATIC_ASSERT_HPP
-#define	STATIC_ASSERT_HPP
+
+#pragma once
 
 #include "ppFunctions.hpp"
+#include <boost/mpl/assert.hpp>
 
 namespace PMacc
 {
@@ -31,71 +32,27 @@ namespace PMacc
     struct StaticAssertError
     {
     };
-
-    /*Switch between two types by a condition
-     * @param Cond condition which must true or false 
-     * @param T1 first type which set if condition is true
-     * @param T2 second type which is set if condition is false
-     * 
-     * internel type name is "type"
-     */
-    template<bool Cond, typename T1, typename T2>
-    struct GetTypeIf;
-
-    template<typename T1, typename T2>
-    struct GetTypeIf < true, T1, T2>
-    {
-        typedef T1 type;
-    };
-
-    template<typename T1, typename T2>
-    struct GetTypeIf < false, T1, T2>
-    {
-        typedef T2 type;
-    };
 }
 
-#define PMACC_STATIC_ASSERT_MSG_DO2(cond,msg,unique_id)               \
-typedef ::PMacc::GetTypeIf<cond,const int,::PMacc::StaticAssertError>::type msg ;\
-typedef const int TRUE_CONDITION;                                    \
-TRUE_CONDITION PMACC_JOIN(TRUE_CONDITION,unique_id)=msg();       \
-return PMACC_JOIN(TRUE_CONDITION,unique_id);
-
-/*! Hide static assert in a struct to use it in function, global and namespace scope
- * nvcc has a special version For all other compiler there is a second 
- * BOOST_STATIC_ASSERT check to catch all asserts inside a struct/class which are
- * not tested by some compiler.
+/** call BOOST_MPL_ASSERT_MSG and add unique id to message
+ * @param cond an integral constant expression
+ * @param msg a message those must a valid variable name (composition of characters_,A-Z,a-z)
+ * @param unique_id pre compiler unique id
  */
-#ifdef __CUDACC__
+#define PMACC_STATIC_ASSERT_MSG_DO2(cond,msg,unique_id)                        \
+    BOOST_MPL_ASSERT_MSG(cond,PMACC_JOIN(msg,PMACC_JOIN(_________,unique_id)),(PMacc::StaticAssertError))
 
-#define PMACC_STATIC_ASSERT_MSG_DO(cond,msg,unique_id) \
-class PMACC_JOIN(PMACC_STATIC_ASSERT_,unique_id){ \
-int msg (){ \
-PMACC_STATIC_ASSERT_MSG_DO2(cond,msg,unique_id); \
-} \
-}
-#else
-#include <boost/static_assert.hpp>
-
-#define PMACC_STATIC_ASSERT_MSG_DO(cond,msg,unique_id) \
-class PMACC_JOIN(PMACC_STATIC_ASSERT_,unique_id){ \
-int msg (){ \
-PMACC_STATIC_ASSERT_MSG_DO2(cond,msg,unique_id); \
-} \
-BOOST_STATIC_ASSERT_MSG(cond,msg); \
-}
-#endif
 
 /*! static assert with error message
  * @param cond A condition which return true or false.
  * @param msg A message which is shown if the condition is false. Msg must a valid c++ variable name (etc. _only_human_make_mistakes)
  */
-#define PMACC_STATIC_ASSERT_MSG(cond,msg) PMACC_STATIC_ASSERT_MSG_DO(cond,msg,__COUNTER__)
+#define PMACC_STATIC_ASSERT_MSG(cond,msg) PMACC_STATIC_ASSERT_MSG_DO2(cond,msg,__COUNTER__)
 
 /*! static assert
  * @param cond A condition which return true or false.
  */
-#define PMACC_STATIC_ASSERT(cond) PMACC_STATIC_ASSERT_MSG(cond,STATIC_ASSERTION_FAILURE)
+#define PMACC_STATIC_ASSERT(cond) PMACC_STATIC_ASSERT_MSG_DO2(cond,STATIC_ASSERTION_FAILURE,__COUNTER__)
 
 /*! static assert wrapper which is easier to use than \see PMACC_STATIC_ASSERT_MSG
  * @param msg A message which is shown if the condition is false. Msg must a valid c++ variable name (etc. _only_human_make_mistakes)
@@ -109,10 +66,10 @@ BOOST_STATIC_ASSERT_MSG(cond,msg); \
 #define PMACC_CASSERT(...) PMACC_STATIC_ASSERT((__VA_ARGS__))
 
 
-/*! static assert for undefined const variables 
+/*! static assert for undefined const variables
  *    using the SFINAE principle
- * 
- * @param msg A message which is shown if the variable does not exist in the namespace nmspace
+ *
+ * @param msg A message which is shown if the variable does not exist in the namespace
  * @param nmspace The name of the namespace
  * @param var The variable to look for.
  */
@@ -130,6 +87,3 @@ BOOST_STATIC_ASSERT_MSG(cond,msg); \
     using fallback::var;                \
   }                                     \
   PMACC_CASSERT_MSG( msg, ((sizeof(msg::var))!=(sizeof(msg::fallback::var))) );
-
-#endif	/* STATIC_ASSERT_HPP */
-
