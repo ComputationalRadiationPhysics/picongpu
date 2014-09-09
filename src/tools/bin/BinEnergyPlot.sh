224007c7c8e7dc000d6f76717a4b3e0c84a800fc
PrometheusPi <r.pausch@hzdr.de>
2014-09-09 15:14:33 +0200

    add flags to energy histogram script

--- src/tools/bin/BinEnergyPlot.sh
+++ src/tools/bin/BinEnergyPlot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright 2013 Axel Huebl, Rene Widera
+# Copyright 2013-2014 Axel Huebl, Rene Widera, Richard Pausch
 #
 # This file is part of PIConGPU.
 #
@@ -19,23 +19,102 @@
 # If not, see <http://www.gnu.org/licenses/>.
 #
 
+# info on usage of this program
+
+# set default label
+LABEL="electrons"
+
+function usage()
+{ 
+ echo "Usage:"
+ echo "  $0 [-h] [-l labelForLegend] pathToSpectraFile timeStep "
+ echo 
+ echo "  -h                  - show usage of $0"
+ echo "  -l labelForLegend   - set name of label"
+ echo "                        default \"$LABEL\""
+ echo "  pathToSpectraFile   - file containg Energy Histogram"
+ echo "  timeStep            - time step to be shown"
+}
+
+
+# set up optgets
+
+# Start processing of option flags at index 1 of all arguments.
+OPTIND=1
+# by seting OPTERR=1, error of the underlying optget are  reported
+OPTERR=1
+
+while getopts "hl:" FLAG "$@"
+do
+    # check for help flag
+    if [ "$FLAG" = "h" ]
+    then
+        usage
+        exit 0
+    fi
+
+    #  check for label flag
+    if [ "$FLAG" = "l" ]
+    then
+        LABEL=$OPTARG
+    fi
+
+    # If the label after the -l argument is missing, the getopt 
+    # routine returns ":" 
+    if [ "$VALUE" = ":" ]
+    then
+        echo "Flag -$OPTARG requires an argument."
+        usage
+        exit 2
+    fi
+
+    # If an unknown flag is used, getopts returns a "?"
+    if [ "$VALUE" = "?" ]
+    then
+        echo "Unknown flag -$OPTARG"
+        usage
+        exit 2
+    fi
+done
+
+
+# The first argument after the flags is at index $OPTIND
+# shift the argumentlist 
+shift `expr $OPTIND - 1`
+
+# check whether the first  argument is a file
 if test ! -f $1; then
  echo "$1 is not a file"
- echo "Usage:"
- echo "  $0 pathToSpectraFile.dat TIMESTEP ParticleNameForLegend"
  exit 1
 fi
 
-initCall="$0 $*"
+
+# directory of program
 bindir=`dirname $0`/
 
+# read out gnuplot script
 script=`cat $bindir/../share/gnuplot/BinEnergyPlot.gnuplot`
 
-tmp1=`echo $1 | sed 's/\//\\\\\//g'`
-tmp2=`echo $2 | bc | sed 's/\//\\\\\//g'`
-tmp3=`echo $3 | sed 's/\//\\\\\//g'`
-bindirEsc=`echo $bindir | sed 's/\//\\\\\//g'`
+# helper function to  correctly escape paths in script replacement
+escapeForLater()
+{
+    sed 's/\//\\\//g'
+}
 
-script=`echo "$script" | sed -e "s/FILENAME/"$tmp1"/g" | sed -e "s/TIMESTEP/"$tmp2"/g" | sed -e "s/BINDIR/"$bindirEsc"/g" | sed -e "s/PARTICLES/"$tmp3"/g"`
+#  prepare arguments for later sed replacements in gnuplot script
+tmp1=`echo $1 | escapeForLater`
+tmp2=`echo $2 | bc | escapeForLater`
+tmp3=`echo $LABEL | escapeForLater`
+bindirEsc=`echo "$bindir" | escapeForLater`
 
+# adjust default gnuplot script 
+script=$( echo "$script" | sed -e "s/FILENAME/$tmp1/g" )
+script=$( echo "$script" | sed -e "s/TIMESTEP/$tmp2/g" )
+script=$( echo "$script" | sed -e "s/BINDIR/$bindirEsc/g" )
+script=$( echo "$script" | sed -e "s/PARTICLES/$tmp3/g" )
+
+
+# run gnuplot script
 echo   "$script" | gnuplot -persist
+
+
