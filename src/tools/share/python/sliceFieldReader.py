36a5e2bc7661290f84145b3ffb218ffec4419b5d
PrometheusPi <r.pausch@hzdr.de>
2014-11-14 01:15:23 +0100

    add doc strings fix typo

--- src/tools/share/python/sliceFieldReader.py
+++ src/tools/share/python/sliceFieldReader.py
@@ -3,14 +3,30 @@ import StringIO as _StringIO
 
 
 def readFieldSlices(File):
+    """
+    Function to read one data file from PIConGPUs SliceFieldPrinter plug-in
+    and returns the field data as array of size [N_y, N_x, 3], with
+    N_y, N_x the size of the slice.
+
+    Parameters:
+    -----------
+    File: either a file of type file which points to the data file or
+          a filename of type str with the path to the data file
+
+    Returns:
+    --------
+    numpy-array with field data
+    """
     # case: file or filename
     if type(File) is file:
         theFile = File
     elif type(File) is str:
         theFile = file(File, 'r')
     else:
+        # if neither trow arrow
         raise IOError("the argument - {} - is not a file".format(File))
 
+    # determine size of slice:
     N_x = None
     N_y = 0
 
@@ -30,8 +46,10 @@ def readFieldSlices(File):
             # set N_x if line was valid
             N_x = N_x_local
 
+    # create data storage for slice of field data
     data = _numpy.empty((N_y, N_x, 3))
 
+    # read file again
     theFile.seek(0)
 
     # go through all valid lines
@@ -43,3 +61,6 @@ def readFieldSlices(File):
             data[y,x,:] = _numpy.genfromtxt(_StringIO.StringIO(fieldValue[1:-1]), delimiter=",")
             
     return data
+
+
+
