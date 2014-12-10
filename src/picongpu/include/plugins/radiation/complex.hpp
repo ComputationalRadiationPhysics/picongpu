3f3da83b68afb6263f4b6bae6647e4b39e9adc59
Axel Huebl <axel.huebl@web.de>
2014-12-09 17:06:37 -0800

    Address Review Comments
    most from @PrometheusPi

--- src/picongpu/include/plugins/radiation/complex.hpp
+++ src/picongpu/include/plugins/radiation/complex.hpp
@@ -75,14 +75,14 @@ public:
         return Complex_T(lhs.real + rhs.real, lhs.imaginary + rhs.imaginary);
     }
 
-    HDINLINE friend Complex_T operator+(const Complex_T& lhs, const T& rhs)
+    HDINLINE friend Complex_T operator+(const Complex_T& lhs, const T& rhs_scalar)
     {
-        return Complex_T(lhs.real + rhs, lhs.imaginary);
+        return Complex_T(lhs.real + rhs_scalar, lhs.imaginary);
     }
 
-    HDINLINE friend Complex_T operator+(const T& lhs, const Complex_T& rhs)
+    HDINLINE friend Complex_T operator+(const T& lhs_scalar, const Complex_T& rhs)
     {
-        return Complex_T(lhs + rhs.real, rhs.imaginary);
+        return Complex_T(lhs_scalar + rhs.real, rhs.imaginary);
     }
 
     // substraction
@@ -96,9 +96,9 @@ public:
         return Complex_T(lhs.real - rhs, lhs.imaginary);
     }
 
-    HDINLINE friend Complex_T operator-(const T& lhs, const Complex_T& rhs)
+    HDINLINE friend Complex_T operator-(const T& lhs_scalar, const Complex_T& rhs)
     {
-        return Complex_T(lhs - rhs.real, -rhs.imaginary);
+        return Complex_T(lhs_scalar - rhs.real, -rhs.imaginary);
     }
 
     // multiplication
@@ -108,49 +108,48 @@ public:
                          lhs.imaginary * rhs.real + lhs.real * rhs.imaginary);
     }
 
-    HDINLINE friend Complex_T operator*(const Complex_T& lhs, const T& rhs)
+    HDINLINE friend Complex_T operator*(const Complex_T& lhs, const T& rhs_scalar)
     {
-        return Complex_T(lhs.real * rhs, lhs.imaginary * rhs);
+        return Complex_T(lhs.real * rhs_scalar, lhs.imaginary * rhs_scalar);
     }
 
-    HDINLINE friend Complex_T operator*(const T& lhs, const Complex_T& rhs)
+    HDINLINE friend Complex_T operator*(const T& lhs_scalar, const Complex_T& rhs)
     {
-        return Complex_T(lhs * rhs.real, lhs * rhs.imaginary);
+        return Complex_T(lhs_scalar * rhs.real, lhs_scalar * rhs.imaginary);
     }
 
     // Division
-    HDINLINE friend Complex_T operator/(const Complex_T& lhs, const T& rhs)
+    HDINLINE friend Complex_T operator/(const Complex_T& lhs, const T& rhs_scalar)
     {
-        return Complex_T(lhs.real / rhs, lhs.imaginary / rhs);
+        return Complex_T(lhs.real / rhs_scalar, lhs.imaginary / rhs_scalar);
     }
 
-    HDINLINE friend Complex_T operator/(const T& lhs, const Complex_T& rhs)
+    HDINLINE friend Complex_T operator/(const T& lhs_scalar, const Complex_T& rhs)
     {
-        return Complex_T(lhs * rhs.real/(rhs.real*rhs.real+rhs.imaginary*rhs.imaginary),
-                         -lhs * rhs.imaginary/( util::square(rhs.real)+util::square(rhs.imaginary) ));
+        return Complex_T( lhs_scalar * rhs.real / ( util::square(rhs.real) + util::square(rhs.imaginary) ),
+                         -lhs_scalar * rhs.imaginary / ( util::square(rhs.real) + util::square(rhs.imaginary) ) );
     }
 
     HDINLINE friend Complex_T operator/(const Complex_T& lhs, const Complex_T& rhs)
     {
-        return lhs*Complex_T(rhs.real/(rhs.real*rhs.real+rhs.imaginary*rhs.imaginary),
-                            -rhs.imaginary/( util::square(rhs.real)+util::square(rhs.imaginary) ));
+        return lhs*Complex_T( rhs.real / ( util::square(rhs.real) + util::square(rhs.imaginary) ),
+                             -rhs.imaginary / ( util::square(rhs.real) + util::square(rhs.imaginary) ) );
     }
 
     HDINLINE static Complex_T<T > csqrt(const Complex_T<T >& other)
     {
-        Complex_T<T > helper = other;
         if (other.real<=0.0 && other.imaginary==0.0) {
-            return Complex_T<T > (0.0,sqrt(-helper.real));
+            return Complex_T<T > (0.0,sqrt(-other.real));
         }
         else {
-            return sqrt(helper.abs_64())*(other+other.abs_64())/(other+other.abs_64()).abs_64();
+            return sqrt(other.abs_64())*(other+other.abs_64())/(other+other.abs_64()).abs_64();
         }
     }
 
     HDINLINE static Complex_T<T > cpow(const Complex_T<T >& other, const T& exponent)
     {
-        Complex_T<T > helper = other;
-        return pow(helper.abs_64(),exponent)*cexp(Complex_T<T >(0.,1.)*helper.arg()*exponent);
+        return pow( other.abs_64(), exponent) *
+               cexp( Complex_T<T >( 0.,1. ) * other.arg() * exponent );
     }
 
     // Complex exponential function
