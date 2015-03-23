44074bf32ee0e59e4259654f9600905457784083
PrometheusPi <r.pausch@hzdr.de>
2015-03-23 17:19:42 +0100

    add phase to laserPolynom

--- src/picongpu/include/fields/laserProfiles/laserPolynom.hpp
+++ src/picongpu/include/fields/laserProfiles/laserPolynom.hpp
@@ -59,7 +59,7 @@ HINLINE float3_X laserLongitudinal(uint32_t currentStep, float_X& phase)
     elong.x() = float_X(
                        float_64(AMPLITUDE) *
                        Tpolynomial(tau)
-                       * math::sin(omegaLaser * (runTime - T_rise))
+                       * math::sin(omegaLaser * (runTime - T_rise) + LASER_PHASE)
                        );
 
     phase = 0.0f;
