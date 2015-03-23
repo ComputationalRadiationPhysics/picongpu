8a4d675b03fde35581fe1dab7d8da92dcfcb8086
Marco Garten <m.garten@hzdr.de>
2015-03-23 13:12:56 +0100

    Allow for a simulation start from neutral atoms
    If ionization is chosen to be enabled by the parameter set then no electrons
    will be initialized but only ions. Immediately after initializing them their
    attribute `boundElectrons_` will be set to the number of protons chosen for
    the respective ion species.

---
+++ src/picongpu/include/particles/manipulators/SetAttributeImpl.hpp
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2015 Marco Garten
+ *
+ * This file is part of PIConGPU.
+ *
+ * PIConGPU is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PIConGPU is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PIConGPU.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "simulation_defines.hpp"
+
+
+namespace picongpu
+{
+namespace particles
+{
+namespace manipulators
+{
+
+template<typename T_ParamClass, typename T_ValueFunctor, typename T_SpeciesType>
+struct SetAttributeImpl : private T_ValueFunctor
+{
+    typedef T_ParamClass ParamClass;
+    typedef T_SpeciesType SpeciesType;
+    typedef typename MakeIdentifier<SpeciesType>::type SpeciesName;
+
+    typedef T_ValueFunctor ValueFunctor;
+
+    HINLINE SetAttributeImpl(uint32_t currentStep)
+    {
+
+    }
+
+    template<typename T_Particle>
+    DINLINE void operator()(const DataSpace<simDim>& localCellIdx, T_Particle& particle, const  bool isParticle)
+    {
+        typedef typename SpeciesType::FrameType FrameType;
+
+        if (isParticle)
+        {
+            /** Number of bound electrons at initial state of the neutral atom */
+            const float_X protonNumber = GetAtomicNumbers<T_Particle>::type::numberOfProtons;
+
+            /* in this case: 'assign' the number of protons to the number of bound electrons
+             * \see particleConfig.param for the ValueFunctor */
+            ValueFunctor::operator()(particle[boundElectrons_], protonNumber);
+        }
+    }
+
+};
+
+} //namespace manipulators
+} //namespace particles
+} //namespace picongpu
