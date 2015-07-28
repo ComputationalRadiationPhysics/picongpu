a273e9435779c5b9773270292d80524fefb27c7a
René Widera <r.widera@hzdr.de>
2015-07-28 14:23:45 +0200

    add preprocessor macros to create structs
    - add facilities macros
    - add struct macros

---
+++ src/libPMacc/include/preprocessor/facilities.hpp
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2015 Rene Widera
+ *
+ * This file is part of libPMacc.
+ *
+ * libPMacc is free software: you can redistribute it and/or modify
+ * it under the terms of either the GNU General Public License or
+ * the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libPMacc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License and the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and the GNU Lesser General Public License along with libPMacc.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+/** echo given input */
+#define PMACC_PP_ECHO(...) __VA_ARGS__
+
+/** echo given input with delay */
+#define PMACC_PP_DEFER_ECHO() PMACC_PP_ECHO
+
+/** get the first element of a preprocessor pair */
+#define PMACC_PP_FIRST(first,second) first
+
+/** get the first element of a preprocessor pair with delay */
+#define PMACC_PP_DEFER_FIRST() PMACC_PP_FIRST
+
+
+/** get the second element of a preprocessor pair */
+#define PMACC_PP_SECOND(first,second) second
+
+/** get the second element of a preprocessor pair with delay */
+#define PMACC_PP_DEFER_SECOND() PMACC_PP_SECOND
+
+/** remove parentheses
+ *
+ * transform (...) to ...
+ */
+#define PMACC_PP_REMOVE_PAREN(...) PMACC_PP_DEFER_ECHO() __VA_ARGS__
+
+/** remove parentheses with delay */
+#define PMACC_PP_DEFER_REMOVE_PAREN() PMACC_PP_REMOVE_PAREN
