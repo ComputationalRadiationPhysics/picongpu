44cc4cf1f2921770b10433635e264ecfcdb6d1cf
Erik Zenker <erikzenker@hotmail.com>
2015-11-12 09:23:21 +0100

    Added pmacc license header

--- src/libPMacc/pmaccConfig.cmake
+++ src/libPMacc/pmaccConfig.cmake
@@ -1,3 +1,25 @@
+# Copyright 2015 Erik Zenker, Rene Widera, Axel Huebl
+#
+# This file is part of libPMacc.
+#
+# libPMacc is free software: you can redistribute it and/or modify
+# it under the terms of either the GNU General Public License or
+# the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# libPMacc is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License and the GNU Lesser General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# and the GNU Lesser General Public License along with libPMacc.
+# If not, see <http://www.gnu.org/licenses/>.
+#
+
+
 # - Config file for the pmacc package
 # It defines the following variables
 #  pmacc_INCLUDE_DIRS - include directories for pmacc
@@ -121,7 +143,6 @@ set(pmacc_LIBRARIES ${pmacc_LIBRARIES} ${MPI_CXX_LIBRARIES})
 ################################################################################
 # PNGwriter
 ################################################################################
-
 find_package(PNGwriter MODULE REQUIRED)
 
 if(PNGwriter_FOUND)
