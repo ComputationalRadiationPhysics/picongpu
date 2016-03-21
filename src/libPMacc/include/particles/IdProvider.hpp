8a9a3674c4ce224e17cdabdacb81c8b56417e3ea
Flamefire <git@grundis.de>
2016-03-21 13:09:47 +0100

    Create IdProvider.def

--- src/libPMacc/include/particles/IdProvider.hpp
+++ src/libPMacc/include/particles/IdProvider.hpp
@@ -22,7 +22,7 @@
 
 #pragma once
 
-#include "pmacc_types.hpp"
+#include "IdProvider.def"
 #include "Environment.hpp"
 #include "eventSystem/EventSystem.hpp"
 #include "algorithms/reverseBits.hpp"
@@ -54,112 +54,80 @@ namespace PMacc {
 
     }  // namespace idDetail
 
-    /**
-     * Provider for globally unique ids (even across ranks)
-     * Implemented for use in static contexts
-     */
-    class IdProvider
+    void IdProvider::init()
     {
-    public:
-        /** Initializes the state so it is ready for use
-         */
-        static void init()
-        {
-            const uint64_t globalUniqueStartId = getStartId();
-            setNextId(globalUniqueStartId);
-            // Instantiate kernel
-            getNewIdHost();
-            // Reset to start value
-            setNextId(globalUniqueStartId);
-        }
+        const uint64_t globalUniqueStartId = getStartId();
+        setNextId(globalUniqueStartId);
+        // Instantiate kernel
+        getNewIdHost();
+        // Reset to start value
+        setNextId(globalUniqueStartId);
+    }
 
-        /** Sets the next id to a given value (e.g. after a restart)
-         */
-        static void setNextId(const uint64_t nextId)
-        {
-            __cudaKernel(idDetail::setNextId)(1, 1)(nextId);
-        }
+    void IdProvider::setNextId(const uint64_t nextId)
+    {
+        __cudaKernel(idDetail::setNextId)(1, 1)(nextId);
+    }
 
-        /** Returns the next id without changing the current state (e.g. for saving)
-         */
-        static uint64_t getNextId()
-        {
-            HostDeviceBuffer<uint64_cu, 1> nextIdBuf(DataSpace<1>(1));
-            __cudaKernel(idDetail::getNextId)(1, 1)(nextIdBuf.getDeviceBuffer().getDataBox());
-            nextIdBuf.deviceToHost();
-            return nextIdBuf.getHostBuffer().getDataBox()(0);
-        }
+    uint64_t IdProvider::getNextId()
+    {
+        HostDeviceBuffer<uint64_cu, 1> nextIdBuf(DataSpace<1>(1));
+        __cudaKernel(idDetail::getNextId)(1, 1)(nextIdBuf.getDeviceBuffer().getDataBox());
+        nextIdBuf.deviceToHost();
+        return nextIdBuf.getHostBuffer().getDataBox()(0);
+    }
 
-        /** Functor that returns a new id each time it is called
-         *  Modifies the state of the IdProvider */
-        struct GetNewId
-        {
-            DINLINE uint64_cu operator()() const
-            {
-                return getNewId();
-            }
-        };
-
-        /** Function that returns a new id each time it is called
-         *  Modifies the state of the IdProvider  */
-        HDINLINE static uint64_cu getNewId()
-        {
+    HDINLINE uint64_cu IdProvider::getNewId()
+    {
 #ifdef __CUDA_ARCH__
-            return nvidia::atomicAllInc(&idDetail::nextId);
+        return nvidia::atomicAllInc(&idDetail::nextId);
 #else
-            // IMPORTANT: This calls a kernel. So make sure this kernel is instantiated somewhere before!
-            return getNewIdHost();
+        // IMPORTANT: This calls a kernel. So make sure this kernel is instantiated somewhere before!
+        return getNewIdHost();
 #endif
-        }
+    }
 
-        /**
-         * Return true, if an overflow of the counter is detected and hence there might be duplicate ids
+    bool IdProvider::isOverflown()
+    {
+        // Get current value
+        uint64_t nextId = getNextId();
+        // Get start value
+        uint64_t globalUniqueStartId = getStartId();
+
+        /* Start value contains globally unique bits in the high order bits
+         * Hence compare all high order bits till the last set one and check,
+         * if any of them differs from the start value --> overflow
          */
-        static bool isOverflown()
+        // How far do we need to shift to get the highest bit
+        BOOST_STATIC_CONSTEXPR int bitsToHighest = sizeof(uint64_t) * CHAR_BIT - 1;
+        while(globalUniqueStartId)
         {
-            // Get current value
-            uint64_t nextId = getNextId();
-            // Get start value
-            uint64_t globalUniqueStartId = getStartId();
-
-            /* Start value contains globally unique bits in the high order bits
-             * Hence compare all high order bits till the last set one and check,
-             * if any of them differs from the start value --> overflow
-             */
-            // How far do we need to shift to get the highest bit
-            BOOST_STATIC_CONSTEXPR int bitsToHighest = sizeof(uint64_t) * CHAR_BIT - 1;
-            while(globalUniqueStartId)
-            {
-                // Compare highest bit
-                if(globalUniqueStartId >> bitsToHighest != nextId >> bitsToHighest)
-                    return true;
-                // Proceed to next bit
-                globalUniqueStartId <<= 1;
-            }
-            return false;
+            // Compare highest bit
+            if(globalUniqueStartId >> bitsToHighest != nextId >> bitsToHighest)
+                return true;
+            // Proceed to next bit
+            globalUniqueStartId <<= 1;
         }
+        return false;
+    }
 
-    private:
-        /** Returns the first id for the current rank */
-        static uint64_t getStartId()
-        {
-            uint64_t rank = Environment<>::get().GridController().getGlobalRank();
+    uint64_t IdProvider::getStartId()
+    {
+        uint64_t rank = Environment<>::get().GridController().getGlobalRank();
 
-            /* We put the rank into the upper bits to have the lower bits for counting up and still
-             * getting unique numbers. Reversing the bits instead of shifting gives some more room
-             * as the upper bits of the rank are often also zero
-             */
-            return reverseBits(rank);
-        }
+        /* We put the rank into the upper bits to have the lower bits for counting up and still
+         * getting unique numbers. Reversing the bits instead of shifting gives some more room
+         * as the upper bits of the rank are often also zero
+         */
+        return reverseBits(rank);
+    }
 
-        /** Host version for getting a new id (changing the state) */
-        static uint64_t getNewIdHost()
-        {
-            HostDeviceBuffer<uint64_cu, 1> newIdBuf(DataSpace<1>(1));
-            __cudaKernel(idDetail::getNewId)(1, 1)(newIdBuf.getDeviceBuffer().getDataBox(), GetNewId());
-            newIdBuf.deviceToHost();
-            return newIdBuf.getHostBuffer().getDataBox()(0);
-        }
-    };
+    uint64_t IdProvider::getNewIdHost()
+    {
+        HostDeviceBuffer<uint64_cu, 1> newIdBuf(DataSpace<1>(1));
+        __cudaKernel(idDetail::getNewId)(1, 1)(newIdBuf.getDeviceBuffer().getDataBox(), GetNewId());
+        newIdBuf.deviceToHost();
+        return newIdBuf.getHostBuffer().getDataBox()(0);
+    }
 
 }  // namespace PMacc
