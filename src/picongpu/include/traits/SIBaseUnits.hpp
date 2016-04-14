b62c6af9e56527aae5322efc7d8f75c04b4e182a
Axel Huebl <axel.huebl@plasma.ninja>
2016-04-14 10:31:57 -0700

    Make SIBaseUnits a General Class
    - not only relevant for particles
    - more central organized

---
+++ src/picongpu/include/traits/SIBaseUnits.hpp
@@ -0,0 +1,48 @@
+/**
+ * Copyright 2015-2016 Axel Huebl
+ *
+ * This file is part of PIConGPU.
+ *
+ * PIConGPU is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PIConGPU is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PIConGPU.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+namespace picongpu
+{
+namespace traits
+{
+
+    /* openPMD uses the powers of the 7 SI base measures to describe
+     * the unit of a record
+     * \see http://git.io/vROmP */
+    BOOST_CONSTEXPR_OR_CONST uint32_t NUnitDimension = 7;
+
+    // pre-C++11 "scoped enumerator" work-around
+    namespace SIBaseUnits {
+    enum SIBaseUnits_t
+    {
+        length = 0,                   // L
+        mass = 1,                     // M
+        time = 2,                     // T
+        electricCurrent = 3,          // I
+        thermodynamicTemperature = 4, // theta
+        amountOfSubstance = 5,        // N
+        luminousIntensity = 6,        // J
+    };
+    }
+
+} // namespace traits
+} // namespace picongpu
