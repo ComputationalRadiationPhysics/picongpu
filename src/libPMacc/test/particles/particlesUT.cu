37180b179d720275ebf33d04293bfbcb88e0b526
Flamefire <git@grundis.de>
2016-10-07 11:42:15 +0200

    Add test for IDProvider

---
+++ src/libPMacc/test/particles/particlesUT.cu
@@ -0,0 +1,32 @@
+/**
+ * Copyright 2016-2016 Alexander Grund
+ *
+ * This file is part of libPMacc.
+ *
+ * libPMacc is free software: you can redistribute it and/or modify
+ * it under the terms of either the GNU General Public License or
+ * the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libPMacc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License and the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and the GNU Lesser General Public License along with libPMacc.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "PMaccFixture.hpp"
+#include <boost/test/unit_test.hpp>
+
+#if TEST_DIM == 2
+    BOOST_GLOBAL_FIXTURE(PMaccFixture2D);
+#else
+    BOOST_GLOBAL_FIXTURE(PMaccFixture3D);
+#endif
+
+#include "IdProvider.hpp"
