7b1f9e342b2f57e4bb8cc924a01db3c067919f28
Axel Huebl <axel.huebl@plasma.ninja>
2017-01-25 18:01:10 +0100

    Fix Wrong ADIOS Replace

--- src/picongpu/include/plugins/adios/NDScalars.hpp
+++ src/picongpu/include/plugins/adios/NDScalars.hpp
@@ -124,7 +124,7 @@ struct ReadNDScalars
         std::string datasetName = params.adiosBasePath + name;
 
         ADIOS_VARINFO* varInfo;
-        ADIOS_CMD_EXPECT_NONnullptr( varInfo = adios_inq_var(params.fp, datasetName.c_str()) );
+        ADIOS_CMD_EXPECT_NONNULL( varInfo = adios_inq_var(params.fp, datasetName.c_str()) );
         if(varInfo->ndim != simDim)
             throw std::runtime_error(std::string("Invalid dimensionality for ") + name);
         if(varInfo->type != traits::PICToAdios<T_Scalar>().type)
