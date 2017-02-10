29b39f5ca50373890e99cdc435d55d0237456afe
Axel Huebl <axel.huebl@plasma.ninja>
2017-02-10 13:20:30 +0100

    Species Attribute: superconfig
    Adds a superconfiguration attribute that can be used on ions
    to calculate population kinetics.

---
+++ src/picongpu/include/particles/flylite/types/Superconfig.hpp
@@ -0,0 +1,50 @@
+/** Copyright 2017 Axel Huebl
+ *
+ * This file is part of PIConGPU.
+ *
+ * PIConGPU is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PIConGPU is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PIConGPU.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "pmacc_types.hpp"
+#include "math/Vector.hpp"
+
+
+namespace picongpu
+{
+namespace flylite
+{
+namespace types
+{
+    /** Ion Superconfiguration
+     *
+     * This is the attribute type for an ion's hydrogenic superconfiguration.
+     *
+     * @tparam T_Type the float type to use, e.g. float_64
+     * @tparam T_populations the number of populations to store for each ion,
+     *                       range: [0, 255]
+     */
+    template<
+        typename T_Type,
+        uint8_t T_populations
+    >
+    using Superconfig = PMacc::math::Vector<
+        T_Type,
+        T_populations
+    >;
+} // namespace types
+} // namespace flylite
+} // namespace picongpu
