18d57a1db2f70010525e5c949a5fde66ecf26631
René Widera <r.widera@hzdr.de>
2017-02-20 13:46:40 +0100

    add radiation helper classes
    - add `GetRadiationFlag` - read radiation flag from a particle
    - add `ExecuteParticleFilter` - filter particles before the radiation calculation

---
+++ src/picongpu/include/plugins/radiation/ExecuteParticleFilter.hpp
@@ -0,0 +1,92 @@
+/* Copyright 2017 Rene Widera
+ *
+ * This file is part of PIConGPU.
+ *
+ * PIConGPU is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PIConGPU is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PIConGPU.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "simulation_defines.hpp"
+
+#include "traits/HasIdentifier.hpp"
+
+#include <memory>
+
+
+namespace picongpu
+{
+namespace radiation
+{
+
+    /** read the `radiationFlag` of a species */
+    template< bool hasFilter >
+    struct ExecuteParticleFilter
+    {
+        /** get the attribute value of `radiationFlag`
+         *
+         * @param particle particle to be used
+         * @return value of the attribute `radiationFlag`
+         */
+        template< typename T_Species >
+        void operator()( std::shared_ptr<T_Species> species, const uint32_t currentStep )
+        {
+            auto radiationEnableFunctor = picongpu::radiation::FilterEnableRadiation( currentStep );
+            species->manipulateAllParticles(
+                currentStep,
+                radiationEnableFunctor
+            );
+        }
+    };
+
+    /** specialization
+     *
+     * specialization for the case that the species not owns the attribute
+     * `radiationFlag`
+     */
+    template< >
+    struct ExecuteParticleFilter< false >
+    {
+        /** get the attribute value of `radiationFlag`
+         *
+         * @param particle to be used
+         * @return always true
+         */
+        template< typename T_Species >
+        void operator()( const std::shared_ptr<T_Species>, const uint32_t currentStep )
+        {
+        }
+    };
+
+    /** execute the particle filter on a species
+     *
+     * There is no need to check if a particle filter is defined
+     *
+     * @tparam T_Species species type
+     * @param species species to be filtered
+     */
+    template< typename T_Species >
+    void executeParticleFilter( std::shared_ptr<T_Species>& species, const uint32_t currentStep )
+    {
+        constexpr bool hasRadiationFilter = ::PMacc::traits::HasIdentifier<
+          typename T_Species::FrameType,
+          radiationFlag
+        >::type::value;
+
+        return ExecuteParticleFilter< hasRadiationFilter >{}( species, currentStep );
+    }
+
+} // namespace radiation
+} // namespace picongpu
---
+++ src/picongpu/include/plugins/radiation/GetRadiationFlag.hpp
@@ -0,0 +1,83 @@
+/* Copyright 2017 Rene Widera
+ *
+ * This file is part of PIConGPU.
+ *
+ * PIConGPU is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PIConGPU is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PIConGPU.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "simulation_defines.hpp"
+
+#include "traits/HasIdentifier.hpp"
+
+
+namespace picongpu
+{
+    /** read the `radiationFlag` of a species */
+    template< bool hasRadiationFlag >
+    struct GetRadiationFlag
+    {
+        /** get the attribute value of `radiationFlag`
+         *
+         * @param particle particle to be used
+         * @return value of the attribute `radiationFlag`
+         */
+        template< typename T_Particle >
+        HDINLINE bool operator()( const T_Particle& particle ) const
+        {
+            return particle[ radiationFlag_ ];
+        }
+    };
+
+    /** specialization
+     *
+     * specialization for the case that the species not owns the attribute
+     * `radiationFlag`
+     */
+    template< >
+    struct GetRadiationFlag< false >
+    {
+        /** get the attribute value of `radiationFlag`
+         *
+         * @param particle to be used
+         * @return always true
+         */
+        template< typename T_Particle >
+        HDINLINE bool operator()( const T_Particle& ) const
+        {
+            return true;
+        }
+    };
+
+    /** get the value of the particle attribute `radiationFlag`
+     *
+     * Allow to read out the value of the attribute `radiationFlag` also if
+     * it is not defined for the particle.
+     *
+     * @tparam T_Particle particle type
+     * @param particle valid particle
+     * @return particle attribute value `radiationFlag`, always `true` if attribute `radiationFlag` is not defined
+     */
+    template< typename T_Particle >
+    HDINLINE bool getRadiationFlag( const T_Particle& particle )
+    {
+        constexpr bool hasRadiationFlag = PMacc::traits::HasIdentifier<
+            typename T_Particle::FrameType,
+            radiationFlag
+        >::type::value;
+        return GetRadiationFlag< hasRadiationFlag >{}( particle );
+    }
+} // namespace picongpu
