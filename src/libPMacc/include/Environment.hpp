c493ae0549390bf214ecffe959323f6b97101369
René Widera <r.widera@hzdr.de>
2017-02-22 16:57:20 +0100

    refactor `PMacc::Environment`
    - create `detail::Environment` for all singletons without dimension
    - add `detial::EnvironmentContext` to hold context information variables
      for `Environment<>` independent of number of simulation dimensions
    - add documentation

--- src/libPMacc/include/Environment.hpp
+++ src/libPMacc/include/Environment.hpp
@@ -36,129 +36,369 @@
 #include "mappings/simulation/Filesystem.hpp"
 #include "eventSystem/events/EventPool.hpp"
 #include "Environment.def"
+#include "communication/manager_common.hpp"
+#include "assert.hpp"
 
 #include <cuda_runtime.h>
+#include <mpi.h>
 
 namespace PMacc
 {
 
-/**
- * Global Environment singleton for Picongpu
- */
+namespace detail
+{
+    /** collect state variables of the environment context
+     *
+     * This class handle the initialization and finalize of the
+     * MPI context and the selection of the GPU.
+     */
+    class EnvironmentContext
+    {
 
-template <unsigned DIM>
-class Environment
+        friend Environment;
+
+        friend PMacc::Environment<DIM1>;
+        friend PMacc::Environment<DIM2>;
+        friend PMacc::Environment<DIM3>;
+
+        EnvironmentContext( ) :
+            m_isMpiInitialized( false ),
+            m_isDeviceSelected( false ),
+            m_isSubGridDefined( false )
+        {
+        }
+
+        /** initialization state of MPI */
+        bool m_isMpiInitialized;
+
+        /** state if a computing device is selected */
+        bool m_isDeviceSelected;
+
+        /** state if the SubGrid is defined */
+        bool m_isSubGridDefined;
+
+        /** get the singleton EnvironmentContext
+         *
+         * @return instance of EnvironmentContext
+         */
+        static EnvironmentContext& getInstance()
+        {
+            static EnvironmentContext instance;
+            return instance;
+        }
+
+        /** state of the MPI context
+         *
+         * @return true if MPI is initialized else false
+         */
+        bool isMpiInitialized()
+        {
+            return m_isMpiInitialized;
+        }
+
+        /** is a computing device selected
+         *
+         * @return true if device is selected else false
+         */
+        bool isDeviceSelected()
+        {
+            return m_isDeviceSelected;
+        }
+
+        /** is the SubGrid defined
+         *
+         * @return true if SubGrid is defined, else false
+         */
+        bool isSubGridDefined()
+        {
+            return m_isSubGridDefined;
+        }
+
+        /** initialize the environment
+         *
+         * After this call it is allowed to use MPI.
+         */
+        void init();
+
+        /** cleanup the environment */
+        void finalize();
+
+        /** select a computing device
+         *
+         * After this call it is allowed to use the computing device.
+         *
+         * @param deviceNumber number of the device
+         */
+        void setDevice(int deviceNumber);
+
+    };
+
+    /** PMacc environment
+     *
+     * Get access to all PMacc singleton classes those not depend on a dimension.
+     */
+    struct Environment
+    {
+        Environment()
+        {
+        }
+
+        /** cleanup the environment */
+        void finalize()
+        {
+            EnvironmentContext::getInstance().finalize();
+        }
+
+        /** get the singleton StreamController
+         *
+         * @return instance of StreamController
+         */
+        PMacc::StreamController& StreamController()
+        {
+            PMACC_ASSERT_MSG(
+                EnvironmentContext::getInstance().isDeviceSelected(),
+                "Environment< DIM >::initDevices() must be called before this method!"
+            );
+            return StreamController::getInstance();
+        }
+
+        /** get the singleton Manager
+         *
+         * @return instance of Manager
+         */
+        PMacc::Manager& Manager()
+        {
+            return Manager::getInstance();
+        }
+
+        /** get the singleton TransactionManager
+         *
+         * @return instance of TransactionManager
+         */
+        PMacc::TransactionManager& TransactionManager() const
+        {
+            PMACC_ASSERT_MSG(
+                EnvironmentContext::getInstance().isDeviceSelected(),
+                "Environment< DIM >::initDevices() must be called before this method!"
+            );
+            return TransactionManager::getInstance();
+        }
+
+        /** get the singleton EnvironmentController
+         *
+         * @return instance of EnvironmentController
+         */
+        PMacc::EnvironmentController& EnvironmentController()
+        {
+            PMACC_ASSERT_MSG(
+                EnvironmentContext::getInstance().isMpiInitialized(),
+                "Environment< DIM >::initDevices() must be called before this method!"
+            );
+            return EnvironmentController::getInstance();
+        }
+
+        /** get the singleton Factory
+         *
+         * @return instance of Factory
+         */
+        PMacc::Factory& Factory()
+        {
+            PMACC_ASSERT_MSG(
+                EnvironmentContext::getInstance().isMpiInitialized() &&
+                EnvironmentContext::getInstance().isDeviceSelected(),
+                "Environment< DIM >::initDevices() must be called before this method!"
+            );
+            return Factory::getInstance();
+        }
+
+        /** get the singleton EventPool
+         *
+         * @return instance of EventPool
+         */
+        PMacc::EventPool& EventPool()
+        {
+            PMACC_ASSERT_MSG(
+                EnvironmentContext::getInstance().isDeviceSelected(),
+                "Environment< DIM >::initDevices() must be called before this method!"
+            );
+            return EventPool::getInstance();
+        }
+
+        /** get the singleton ParticleFactory
+         *
+         * @return instance of ParticleFactory
+         */
+        PMacc::ParticleFactory& ParticleFactory()
+        {
+            return ParticleFactory::getInstance();
+        }
+
+        /** get the singleton DataConnector
+         *
+         * @return instance of DataConnector
+         */
+        PMacc::DataConnector& DataConnector()
+        {
+            return DataConnector::getInstance();
+        }
+
+        /** get the singleton PluginConnector
+         *
+         * @return instance of PluginConnector
+         */
+        PMacc::PluginConnector& PluginConnector()
+        {
+            return PluginConnector::getInstance();
+        }
+
+        /** get the singleton MemoryInfo
+         *
+         * @return instance of MemoryInfo
+         */
+        nvidia::memory::MemoryInfo& MemoryInfo()
+        {
+            PMACC_ASSERT_MSG(
+                EnvironmentContext::getInstance().isDeviceSelected(),
+                "Environment< DIM >::initDevices() must be called before this method!"
+            );
+            return nvidia::memory::MemoryInfo::getInstance();
+        }
+
+        /** get the singleton SimulationDescription
+         *
+         * @return instance of SimulationDescription
+         */
+        simulationControl::SimulationDescription& SimulationDescription()
+        {
+            return simulationControl::SimulationDescription::getInstance();
+        }
+    };
+} // namespace detail
+
+/** Global Environment singleton for PMacc
+ */
+template< uint32_t T_dim >
+class Environment : public detail::Environment
 {
 public:
 
-    PMacc::GridController<DIM>& GridController()
+    /** get the singleton GridController
+     *
+     * @return instance of GridController
+     */
+    PMacc::GridController< T_dim >& GridController()
     {
-        return PMacc::GridController<DIM>::getInstance();
+        PMACC_ASSERT_MSG(
+            detail::EnvironmentContext::getInstance().isMpiInitialized(),
+            "Environment< DIM >::initDevices() must be called before this method!"
+        );
+        return PMacc::GridController< T_dim >::getInstance();
     }
 
-    PMacc::StreamController& StreamController()
+    /** get the singleton SubGrid
+     *
+     * @return instance of SubGrid
+     */
+    PMacc::SubGrid< T_dim >& SubGrid()
     {
-        return StreamController::getInstance();
+        PMACC_ASSERT_MSG(
+            detail::EnvironmentContext::getInstance().isSubGridDefined(),
+            "Environment< DIM >::initGrids() must be called before this method!"
+        );
+        return PMacc::SubGrid< T_dim >::getInstance();
     }
 
-    PMacc::Manager& Manager()
+    /** get the singleton Filesystem
+     *
+     * @return instance of Filesystem
+     */
+    PMacc::Filesystem< T_dim >& Filesystem()
     {
-        return Manager::getInstance();
+        return PMacc::Filesystem< T_dim >::getInstance();
     }
 
-    PMacc::TransactionManager& TransactionManager() const
+    /** get the singleton Environment< DIM >
+     *
+     * @return instance of Environment<DIM >
+     */
+    static Environment< T_dim >& get()
     {
-        return TransactionManager::getInstance();
-    }
-
-    PMacc::SubGrid<DIM>& SubGrid()
-    {
-        return PMacc::SubGrid<DIM>::getInstance();
-    }
-
-    PMacc::EnvironmentController& EnvironmentController()
-    {
-        return EnvironmentController::getInstance();
-    }
-
-    PMacc::EventPool& EventPool()
-    {
-        return EventPool::getInstance();
-    }
-
-    PMacc::Factory& Factory()
-    {
-        return Factory::getInstance();
-    }
-
-    PMacc::ParticleFactory& ParticleFactory()
-    {
-        return ParticleFactory::getInstance();
-    }
-
-    PMacc::DataConnector& DataConnector()
-    {
-        return DataConnector::getInstance();
-    }
-
-    PMacc::PluginConnector& PluginConnector()
-    {
-        return PluginConnector::getInstance();
-    }
-
-    nvidia::memory::MemoryInfo& MemoryInfo()
-    {
-        return nvidia::memory::MemoryInfo::getInstance();
-    }
-
-    simulationControl::SimulationDescription& SimulationDescription()
-    {
-        return simulationControl::SimulationDescription::getInstance();
-    }
-
-    PMacc::Filesystem<DIM>& Filesystem()
-    {
-        return PMacc::Filesystem<DIM>::getInstance();
-    }
-
-    static Environment<DIM>& get()
-    {
-        static Environment<DIM> instance;
+        static Environment< T_dim > instance;
         return instance;
     }
 
-    void initDevices(DataSpace<DIM> devices, DataSpace<DIM> periodic)
+    /** create and initialize the environment of PMacc
+     *
+     * Usage of MPI or device(accelerator) function calls before this method
+     * are not allowed.
+     *
+     * @param devices number of devices per simulation dimension
+     * @param periodic periodicity each simulation dimension
+     *                 (0 == not periodic, 1 == periodic)
+     */
+    void initDevices(
+        DataSpace< T_dim > devices,
+        DataSpace< T_dim > periodic
+    )
     {
-        PMacc::GridController<DIM>::getInstance().init(devices, periodic);
+        // initialize the MPI context
+        detail::EnvironmentContext::getInstance().init();
 
-        PMacc::Filesystem<DIM>::getInstance();
+        // create singleton instances
+        GridController().init( devices, periodic );
 
-        setDevice((int) (PMacc::GridController<DIM>::getInstance().getHostRank()));
+        EnvironmentController();
 
-        StreamController::getInstance().activate();
+        Filesystem();
 
-        TransactionManager::getInstance();
+        detail::EnvironmentContext::getInstance().setDevice(
+            static_cast<int>( GridController().getHostRank() )
+        );
+
+        StreamController().activate();
+
+        MemoryInfo();
+
+        TransactionManager();
+
+        SimulationDescription();
 
     }
 
-    void initGrids(DataSpace<DIM> gridSizeGlobal, DataSpace<DIM> gridSizeLocal, DataSpace<DIM> gridOffset)
+    /** initialize the computing domain information of PMacc
+     *
+     * @param globalDomainSize size of the global simulation domain [cells]
+     * @param localDomainSize size of the local simulation domain [cells]
+     * @param localDomainOffset local domain offset [cells]
+     */
+    void initGrids(
+        DataSpace< T_dim > globalDomainSize,
+        DataSpace< T_dim > localDomainSize,
+        DataSpace< T_dim > localDomainOffset
+    )
     {
-        PMacc::SubGrid<DIM>::getInstance().init(gridSizeLocal, gridSizeGlobal, gridOffset);
+        PMACC_ASSERT_MSG(
+            detail::EnvironmentContext::getInstance().isMpiInitialized(),
+            "Environment< DIM >::initDevices() must be called before this method!"
+        );
 
-        EnvironmentController::getInstance();
+        detail::EnvironmentContext::getInstance().m_isSubGridDefined = true;
 
-        DataConnector::getInstance();
+        // create singleton instances
+        SubGrid().init(
+            localDomainSize,
+            globalDomainSize,
+            localDomainOffset
+        );
 
-        PluginConnector::getInstance();
+        DataConnector();
 
-        nvidia::memory::MemoryInfo::getInstance();
-
-        simulationControl::SimulationDescription::getInstance();
+        PluginConnector();
     }
 
-    void finalize()
-    {
-    }
+    Environment(const Environment&) = delete;
+
+    Environment& operator=(const Environment&) = delete;
 
 private:
 
@@ -166,11 +406,41 @@ private:
     {
     }
 
-    Environment(const Environment&);
+    ~Environment()
+    {
 
-    Environment& operator=(const Environment&);
+    }
 
-    void setDevice(int deviceNumber)
+};
+
+namespace detail
+{
+
+    void EnvironmentContext::init()
+    {
+        m_isMpiInitialized = true;
+
+        // MPI_Init with NULL is allowed since MPI 2.0
+        MPI_CHECK(MPI_Init(NULL,NULL));
+    }
+
+    void EnvironmentContext::finalize()
+    {
+        if( m_isMpiInitialized )
+        {
+            PMacc::Environment<>::get().Manager().waitForAllTasks();
+            // Required by scorep for flushing the buffers
+            cudaDeviceSynchronize();
+            m_isMpiInitialized = false;
+            /* Free the MPI context.
+             * The gpu context is freed by the `StreamController`, because
+             * MPI and CUDA are independent.
+             */
+            MPI_CHECK(MPI_Finalize());
+        }
+    }
+
+    void EnvironmentContext::setDevice(int deviceNumber)
     {
         int num_gpus = 0; //number of gpus
         cudaGetDeviceCount(&num_gpus);
@@ -244,10 +514,12 @@ private:
                 CUDA_CHECK(rc); /*error message*/
             }
         }
-    }
-};
 
-}
+        m_isDeviceSelected = true;
+    }
+
+} // namespace detail
+} // namespace PMacc
 
 /* No namespace for macro defines */
 
