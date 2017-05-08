81e86bdd1e11c7412bedbef8179432a7c1a11e96
Axel Huebl <axel.huebl@plasma.ninja>
2017-05-08 16:22:02 +0200

    Doc Typo: IdxConfig

--- src/libPMacc/include/mappings/threads/IdxConfig.hpp
+++ src/libPMacc/include/mappings/threads/IdxConfig.hpp
@@ -38,7 +38,7 @@ namespace threads
      *
      * @tparam T_domainSize number of indices in the domain
      * @tparam T_workerSize number of worker working on @p T_domainSize
-     * @tparam T_simdSize SMID width
+     * @tparam T_simdSize SIMD width
      */
     template<
         uint32_t T_domainSize,
@@ -51,7 +51,7 @@ namespace threads
         static constexpr uint32_t domainSize = T_domainSize;
         /** number of worker (threads) working on @p domainSize */
         static constexpr uint32_t workerSize = T_workerSize;
-        /** SMID width */
+        /** SIMD width */
         static constexpr uint32_t simdSize = T_simdSize;
 
         /** number of collective iterations needed to address all indices */
