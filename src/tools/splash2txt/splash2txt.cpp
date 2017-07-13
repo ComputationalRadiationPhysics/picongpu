7952af2a85d2513079d58130925125fce3ce096d
Axel Huebl <axel.huebl@plasma.ninja>
2017-07-13 14:15:18 +0200

    splash2txt: C++98
    Keep splash2txt in C++98 so we do not need to
    artificially push up it's CMakeLists.txt requirements
    to C++11.

--- src/tools/splash2txt/splash2txt.cpp
+++ src/tools/splash2txt/splash2txt.cpp
@@ -218,7 +218,7 @@ int main( int argc, char** argv )
         return 1;
     }
 
-    ITools *tools = nullptr;
+    ITools *tools = NULL;
     switch ( options.fileMode)
     {
         case FM_SPLASH: tools = new ToolsSplashParallel( options, mpi_topology, *outStream );
