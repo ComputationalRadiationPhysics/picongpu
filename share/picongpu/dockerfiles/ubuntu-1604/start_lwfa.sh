1dee96aec53058352c12327e9078c9a432c13d9c
Axel Huebl <axel.huebl@plasma.ninja>
2017-07-17 15:08:29 +0200

    More LWFA Niceness
    - more SMs pre-build for quicker startup
    - awesome cfg for example one-GPU viz in LWFA

--- share/picongpu/dockerfiles/ubuntu-1604/start_lwfa.sh
+++ share/picongpu/dockerfiles/ubuntu-1604/start_lwfa.sh
@@ -3,9 +3,18 @@
 
 isaac &
 
-mpirun -n 1 paramSets/lwfa/bin/picongpu \
-    -d 1 1 1 \
-    -g 32 32 32 \
-    -s 1000 \
-    --softRestarts 10 \
-    --isaac.period 1
+echo ""
+echo "Let's watch a laser-plasma movie!"
+echo "  http://plasma.ninja/isaac_1_3_0/interface.htm"
+echo ""
+
+# wait until server is up
+sleep 5
+
+# start PIConGPU
+cd paramSets/lwfa
+tbg \
+  -s bash \
+  -c etc/picongpu/0001gpus_isaac.cfg \
+  -t etc/bash/bash_mpirun.tpl \
+  /tmp/lwfa_001
