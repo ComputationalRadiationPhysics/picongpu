03fab0b9b00adcedbc86379d0295245a5eff31a3
René Widera <r.widera@hzdr.de>
2017-08-01 11:28:47 +0200

    add review comments
    - format file with new code style
    - change/remove some comments

--- include/picongpu/plugins/EnergyParticles.hpp
+++ include/picongpu/plugins/EnergyParticles.hpp
@@ -20,8 +20,6 @@
 
 #pragma once
 
-#include <mpi.h>
-
 #include "picongpu/simulation_defines.hpp"
 #include "picongpu/algorithms/KinEnergy.hpp"
 #include "picongpu/plugins/ISimulationPlugin.hpp"
@@ -73,7 +71,7 @@ namespace picongpu
             typename T_DBox,
             typename T_Mapping
         >
-        DINLINE void operator()(
+        DINLINE void operator( )(
             T_ParBox pb,
             T_DBox gEnergy,
             T_Mapping mapper
@@ -129,7 +127,7 @@ namespace picongpu
                 }
             );
 
-            __syncthreads();
+            __syncthreads( );
 
             DataSpace< simDim > const superCellIdx( mapper.getSuperCellIndex(
                 DataSpace< simDim >( blockIdx )
@@ -157,7 +155,7 @@ namespace picongpu
                 }
             );
 
-            while( frame.isValid() )
+            while( frame.isValid( ) )
             {
                 // loop over all particles in the frame
                 ForEachIdx< ParticleDomCfg > forEachParticle( workerIdx );
@@ -174,10 +172,7 @@ namespace picongpu
                             auto particle = frame[ linearIdx ];
                             float3_X const mom = particle[ momentum_ ];
                             // compute square of absolute momentum of the particle
-                            float_X const mom2 =
-                                mom.x() * mom.x() +
-                                mom.y() * mom.y() +
-                                mom.z() * mom.z();
+                            float_X const mom2 = math::abs2( mom );
                             float_X const weighting = particle[ weighting_ ];
                             float_X const mass = attribute::getMass(
                                 weighting,
@@ -186,7 +181,7 @@ namespace picongpu
                             float_X const c2 = SPEED_OF_LIGHT * SPEED_OF_LIGHT;
 
                             // calculate kinetic energy of the macro particle
-                            localEnergyKin += KinEnergy<>()(
+                            localEnergyKin += KinEnergy<>( )(
                                 mom,
                                 mass
                             );
@@ -232,7 +227,7 @@ namespace picongpu
             );
 
             // wait that all virtual threads updated the shared memory energies
-            __syncthreads();
+            __syncthreads( );
 
             // add energies on global level using global memory
             ForEachIdx< MasterOnly >{ workerIdx }(
@@ -256,41 +251,57 @@ namespace picongpu
         }
     };
 
-    template<class ParticlesType>
+    template< typename ParticlesType >
     class EnergyParticles : public ISimulationPlugin
     {
     private:
-    typedef MappingDesc::SuperCellSize SuperCellSize;
+        //! energy values (global on GPU)
+        GridBuffer<
+            float_64,
+            DIM1
+        > * gEnergy;
+        MappingDesc * cellDescription;
 
-        GridBuffer<float_64, DIM1> *gEnergy; /* energy values (global on GPU) */
-        MappingDesc *cellDescription;
-        uint32_t notifyPeriod; /* periodocity of computing the particle energy */
+        //! periodicity of computing the particle energy
+        uint32_t notifyPeriod;
 
-        std::string pluginName; /* name (used for output file too) */
-        std::string pluginPrefix; /* prefix used for command line arguments */
-        std::string filename; /* output file name */
+        //! name (used for output file too)
+        std::string pluginName;
 
-        std::ofstream outFile; /* file output stream */
-        bool writeToFile;   /* only rank 0 creates a file */
+        //! prefix used for command line arguments
+        std::string pluginPrefix;
 
-        mpi::MPIReduce reduce; /* MPI reduce to add all energies over several GPUs */
+        //! output file name
+        std::string filename;
+
+        //! file output stream
+        std::ofstream outFile;
+
+        /** only one MPI rank creates a file
+         *
+         * true if this MPI rank creates the file, else false
+         */
+        bool writeToFile;
+
+        //! MPI reduce to add all energies over several GPUs
+        mpi::MPIReduce reduce;
 
     public:
 
-        EnergyParticles() :
-        pluginName("EnergyParticles: calculate the energy of a species"),
-        pluginPrefix(ParticlesType::FrameType::getName() + std::string("_energy")),
-        filename(pluginPrefix + ".dat"),
-        gEnergy(nullptr),
-        cellDescription(nullptr),
-        notifyPeriod(0),
-        writeToFile(false)
+        EnergyParticles( ) :
+            pluginName( "EnergyParticles: calculate the energy of a species" ),
+            pluginPrefix( ParticlesType::FrameType::getName( ) + std::string( "_energy" ) ),
+            filename( pluginPrefix + ".dat" ),
+            gEnergy( nullptr ),
+            cellDescription( nullptr ),
+            notifyPeriod( 0 ),
+            writeToFile( false )
         {
-            /* register this plugin */
-            Environment<>::get().PluginConnector().registerPlugin(this);
+            // register this plugin
+            Environment< >::get( ).PluginConnector( ).registerPlugin( this );
         }
 
-        virtual ~EnergyParticles()
+        virtual ~EnergyParticles( )
         {
 
         }
@@ -298,160 +309,196 @@ namespace picongpu
         /** this code is executed if the current time step is supposed to compute
          * the energy
          */
-        void notify(uint32_t currentStep)
+        void notify( uint32_t currentStep )
         {
-            /* call the method that calls the plugin kernel */
-            calculateEnergyParticles < CORE + BORDER > (currentStep);
+            // call the method that calls the plugin kernel
+            calculateEnergyParticles < CORE + BORDER > ( currentStep );
         }
 
-        /** method used by plugin controller to get --help description **/
-        void pluginRegisterHelp(boost::program_options::options_description& desc)
+        ///! method used by plugin controller to get --help description
+        void pluginRegisterHelp( boost::program_options::options_description& desc )
         {
-            desc.add_options()
-                ((pluginPrefix + ".period").c_str(),
-                 boost::program_options::value<uint32_t > (&notifyPeriod),
-                 "compute kinetic and total energy [for each n-th step] enable plugin by setting a non-zero value");
+            desc.add_options( )(
+                ( pluginPrefix + ".period").c_str( ),
+                boost::program_options::value< uint32_t >( &notifyPeriod ),
+                "compute kinetic and total energy [for each n-th step] enable plugin by setting a non-zero value"
+            );
         }
 
         //! method giving the plugin name (used by plugin control)
-        std::string pluginGetName() const
+        std::string pluginGetName( ) const
         {
             return pluginName;
         }
 
-      /** set cell description in this plugin **/
-        void setMappingDescription(MappingDesc *cellDescription)
+        //! set cell description in this plugin
+        void setMappingDescription( MappingDesc *cellDescription )
         {
             this->cellDescription = cellDescription;
         }
 
     private:
 
-        /** method to initialize plugin output and variables **/
-        void pluginLoad()
+        //! method to initialize plugin output and variables
+        void pluginLoad( )
         {
-            if (notifyPeriod > 0) /* only if plugin is called at least once */
+            // only if plugin is called at least once
+            if( notifyPeriod > 0 )
             {
-                /* decide which MPI-rank writes output: */
-                writeToFile = reduce.hasResult(mpi::reduceMethods::Reduce());
+                // decide which MPI-rank writes output
+                writeToFile = reduce.hasResult( mpi::reduceMethods::Reduce( ) );
 
-                /* create two ints on gpu and host: */
-                gEnergy = new GridBuffer<float_64, DIM1 > (DataSpace<DIM1 > (2));
+                // create two ints on gpu and host
+                gEnergy = new GridBuffer<
+                    float_64,
+                    DIM1
+                >( DataSpace< DIM1 >( 2 ) );
 
-                if (writeToFile) /* only MPI rank that writes to file: */
+                // only MPI rank that writes to file
+                if( writeToFile )
                 {
-                    /* open output file */
-                    outFile.open(filename.c_str(), std::ofstream::out | std::ostream::trunc);
+                    // open output file
+                    outFile.open(
+                        filename.c_str( ),
+                        std::ofstream::out | std::ostream::trunc
+                    );
 
-                    /* error handling: */
-                    if (!outFile)
+                    // error handling
+                    if( !outFile )
                     {
-                        std::cerr << "Can't open file [" << filename
-                                  << "] for output, diasble plugin output. " << std::endl;
+                        std::cerr <<
+                            "Can't open file [" <<
+                            filename <<
+                            "] for output, diasble plugin output. " <<
+                            std::endl;
                         writeToFile = false;
                     }
 
-                    /* create header of the file */
+                    // create header of the file
                     outFile << "#step Ekin_Joule E_Joule" << " \n";
                 }
 
-                /* set how often the plugin should be executed while PIConGPU is running */
-                Environment<>::get().PluginConnector().setNotificationPeriod(this, notifyPeriod);
+                // set how often the plugin should be executed while PIConGPU is running
+                Environment<>::get( ).PluginConnector( ).setNotificationPeriod(
+                    this,
+                    notifyPeriod
+                );
             }
         }
 
-        /** method to quit plugin **/
-        void pluginUnload()
+        //! method to quit plugin
+        void pluginUnload( )
         {
-            if (notifyPeriod > 0) /* only if plugin is called at least once */
+            // only if plugin is called at least once
+            if( notifyPeriod > 0 )
             {
-                if (writeToFile)
+                if( writeToFile )
                 {
-                    outFile.flush();
-                    outFile << std::endl; /* now all data is written to file */
+                    outFile.flush( );
+                    // flush cached data to file
+                    outFile << std::endl;
 
-                    /* error handling: */
-                    if (outFile.fail())
+                    if( outFile.fail( ) )
                         std::cerr << "Error on flushing file [" << filename << "]. " << std::endl;
-                    outFile.close();
+                    outFile.close( );
                 }
-
-                __delete(gEnergy); /* free global memory on GPU */
+                // free global memory on GPU
+                __delete( gEnergy );
             }
         }
 
-        void restart(uint32_t restartStep, const std::string restartDirectory)
+        void restart(
+            uint32_t restartStep,
+            std::string const restartDirectory
+        )
         {
             if( !writeToFile )
                 return;
 
-            writeToFile = restoreTxtFile( outFile,
-                                          filename,
-                                          restartStep,
-                                          restartDirectory );
+            writeToFile = restoreTxtFile(
+                outFile,
+                filename,
+                restartStep,
+                restartDirectory
+            );
         }
 
-        void checkpoint(uint32_t currentStep, const std::string checkpointDirectory)
+        void checkpoint(
+            uint32_t currentStep,
+            std::string const checkpointDirectory
+        )
         {
             if( !writeToFile )
                 return;
 
-            checkpointTxtFile( outFile,
-                               filename,
-                               currentStep,
-                               checkpointDirectory );
+            checkpointTxtFile(
+                outFile,
+                filename,
+                currentStep,
+                checkpointDirectory
+            );
         }
 
-        /** method to call analysis and plugin-kernel calls **/
-        template< uint32_t AREA>
-        void calculateEnergyParticles(uint32_t currentStep)
+        //! method to call analysis and plugin-kernel calls
+        template< uint32_t AREA >
+        void calculateEnergyParticles( uint32_t currentStep )
         {
-            DataConnector &dc = Environment<>::get().DataConnector(); /* get data connector */
-            /* use data connector to get particle data */
-            auto particles = dc.get< ParticlesType >( ParticlesType::FrameType::getName(), true );
+            DataConnector &dc = Environment<>::get( ).DataConnector( );
 
-            gEnergy->getDeviceBuffer().setValue(0.0); /* init global energy with zero */
+            // use data connector to get particle data
+            auto particles = dc.get< ParticlesType >(
+                ParticlesType::FrameType::getName( ),
+                true
+            );
+
+            // initialize global energies with zero
+            gEnergy->getDeviceBuffer( ).setValue( 0.0 );
 
             constexpr uint32_t numWorkers = pmacc::traits::GetNumWorkers<
                 pmacc::math::CT::volume< SuperCellSize >::type::value
             >::value;
 
-            AreaMapping<AREA, MappingDesc> mapper(*cellDescription);
-            /* kernel call = sum all particle energies on GPU */
+            AreaMapping<
+                AREA,
+                MappingDesc
+            > mapper( *cellDescription );
+
             PMACC_KERNEL( KernelEnergyParticles< numWorkers >{ } )(
-                mapper.getGridDim(),
+                mapper.getGridDim( ),
                 numWorkers
             )(
-                particles->getDeviceParticlesBox(),
-                gEnergy->getDeviceBuffer().getDataBox(),
+                particles->getDeviceParticlesBox( ),
+                gEnergy->getDeviceBuffer( ).getDataBox( ),
                 mapper
             );
 
-            dc.releaseData( ParticlesType::FrameType::getName() );
+            dc.releaseData( ParticlesType::FrameType::getName( ) );
 
-            gEnergy->deviceToHost(); /* get energy from GPU */
+            // get energy from GPU
+            gEnergy->deviceToHost( );
 
-            float_64 reducedEnergy[2]; /* create storage for kinetic and total energy */
+            // create storage for the global reduced result
+            float_64 reducedEnergy[2];
 
-            /* add energies from all GPUs using MPI: */
+            // add energies from all GPUs using MPI
             reduce(
-                nvidia::functors::Add(),
+                nvidia::functors::Add( ),
                 reducedEnergy,
-                gEnergy->getHostBuffer().getBasePointer(),
+                gEnergy->getHostBuffer( ).getBasePointer( ),
                 2,
-                mpi::reduceMethods::Reduce()
+                mpi::reduceMethods::Reduce( )
             );
 
             /* print timestep, kinetic energy and total energy to file: */
-            if (writeToFile)
+            if( writeToFile )
             {
-                typedef std::numeric_limits< float_64 > dbl;
+                using dbl = std::numeric_limits< float_64 >;
 
-                outFile.precision(dbl::digits10);
+                outFile.precision( dbl::digits10 );
                 outFile << currentStep << " "
                         << std::scientific
-                        << reducedEnergy[0] * UNIT_ENERGY << " "
-                        << reducedEnergy[1] * UNIT_ENERGY << std::endl;
+                        << reducedEnergy[ 0 ] * UNIT_ENERGY << " "
+                        << reducedEnergy[ 1 ] * UNIT_ENERGY << std::endl;
             }
         }
 
