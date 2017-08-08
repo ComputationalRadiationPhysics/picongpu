4f964462699e54eeb4366b3ecf11d6f4f4d27318
Axel Huebl <axel.huebl@plasma.ninja>
2017-08-08 18:52:06 +0200

    Boost 1.62.0: Compile Suite

--- buildsystem/CompileSuite/autoTests/new_commits.sh
+++ buildsystem/CompileSuite/autoTests/new_commits.sh
@@ -93,7 +93,7 @@ touch "$thisDir"runGuard
             # modify compile environment (forwarded to CMake)
             #export PIC_COMPILE_SUITE_CMAKE="-DPIC_ENABLE_PNG=OFF -DCUDA_ARCH=35"
             . /etc/profile
-            module load gcc/4.9.4 boost/1.57.0 cmake/3.7.0 cuda/7.5.18 openmpi/1.10.4
+            module load gcc/4.9.4 boost/1.62.0 cmake/3.7.0 cuda/7.5.18 openmpi/1.10.4
             module load libSplash/1.6.0 adios/1.10.0
             module load pngwriter/0.5.6 rivlib/1.0.2
             module load libjpeg-turbo/1.5.1 icet/2.1.1 jansson/2.9 isaac/1.1.0
