f6eb68c0f6ca418d1e85903f7c3567c1287fa06c
René Widera <r.widera@hzdr.de>
2017-08-08 14:57:17 +0200

    replace `CUDA_ARCH` with `ALPAKA_CUDA_ARCH`

--- buildsystem/CompileSuite/autoTests/new_commits.sh
+++ buildsystem/CompileSuite/autoTests/new_commits.sh
@@ -91,7 +91,7 @@ touch "$thisDir"runGuard
             cd $cnf_builddir
 
             # modify compile environment (forwarded to CMake)
-            #export PIC_COMPILE_SUITE_CMAKE="-DPIC_ENABLE_PNG=OFF -DCUDA_ARCH=35"
+            #export PIC_COMPILE_SUITE_CMAKE="-DPIC_ENABLE_PNG=OFF -DALPAKA_CUDA_ARCH=35"
             . /etc/profile
             module load gcc/4.9.4 boost/1.62.0 cmake/3.7.0 cuda/7.5.18 openmpi/1.10.4
             module load libSplash/1.6.0 adios/1.10.0
