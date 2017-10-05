4c675b0eef730a138deaac4c8a5e95e7898e178d
Sebastian Starke <s.starke@hzdr.de>
2017-10-05 16:35:29 +0200

    ui_params: name params exactly as in picongpu

--- share/picongpu/examples/LaserWakefield/lib/python/picongpu/ui_params.py
+++ share/picongpu/examples/LaserWakefield/lib/python/picongpu/ui_params.py
@@ -15,22 +15,22 @@ from eupraxia_picongpu.parameters import *
 
 PARAMETER_LIST = [
     LogScaledParameter(
-        name="Density", ptype="compile", unit="1/m^3",
+        name="Base_Density_SI", ptype="compile", unit="1/m^3",
         default=25, slider_min=20, slider_max=26,
         slider_step=1, base=10),
 
     UiParameter(
-        name="Laser_A0", ptype="compile", unit="",
+        name="_A0", ptype="compile", unit="",
         default=1.5, slider_min=0.1, slider_max=50.01,
         slider_step=0.1),
 
     LinearScaledParameter(
-        name="Laser_Wavelength", ptype="compile", unit="m",
+        name="Wave_Length_SI", ptype="compile", unit="m",
         default=0.8, slider_min=0.4, slider_max=1400.1,
         slider_step=0.1, scale_factor=1.e-6),
 
     LinearScaledParameter(
-        name="Laser_Pulse_Length", ptype="compile", unit="s",
+        name="Pulse_Length_SI", ptype="compile", unit="s",
         default=5, slider_min=1, slider_max=150,
         slider_step=1, scale_factor=1.e-15),
 ]
