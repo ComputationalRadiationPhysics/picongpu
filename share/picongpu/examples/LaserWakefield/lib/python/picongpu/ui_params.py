4b8457b8ddcef8ff43871ee5f580fe24ce75882a
Sebastian Starke <s.starke@hzdr.de>
2017-10-05 13:09:50 +0200

    ui_params: add version header, linebreaks, fix Laser_Pulse_Length case and comments

--- share/picongpu/examples/LaserWakefield/lib/python/picongpu/ui_params.py
+++ share/picongpu/examples/LaserWakefield/lib/python/picongpu/ui_params.py
@@ -1,20 +1,36 @@
+"""
+This file is part of PIConGPU.
+
+Copyright 2017 PIConGPU contributors
+Authors: Sebastian Starke, Jeffrey Kelling
+License: GPLv3+
+"""
+
 from eupraxia_picongpu.parameters import *
 
-# if more params are added, make sure to add their names and sensible default
-# values to uiParamDefines.param within picongpu simulation_defines and adapt
-# the *.param files that define those values
-# in order to visualize parameters they need to be at least of type UiParameter or inherited
+# If more params are added, make sure to adapt the corresponding
+# *.param files that define those values within picongpu.
+# In order to visualize parameters they need to be at least of
+# class UiParameter or inherited
 
 PARAMETER_LIST = [
-    LogScaledParameter(name="Density", ptype="compile", unit="1/m^3",
-                       default=25, slider_min=20, slider_max=26, slider_step=1, base=10),
+    LogScaledParameter(
+        name="Density", ptype="compile", unit="1/m^3",
+        default=25, slider_min=20, slider_max=26,
+        slider_step=1, base=10),
 
-    UiParameter(name="Laser_A0", ptype="compile",
-                unit="", default=1.5, slider_min=0.1, slider_max=50.01, slider_step=0.1),
+    UiParameter(
+        name="Laser_A0", ptype="compile", unit="",
+        default=1.5, slider_min=0.1, slider_max=50.01,
+        slider_step=0.1),
 
-    LinearScaledParameter(name="Laser_Wavelength", ptype="compile", unit="m", default=0.8,
-                          slider_min=0.4, slider_max=1400.1, slider_step=0.1, scale_factor=1.e-6),
+    LinearScaledParameter(
+        name="Laser_Wavelength", ptype="compile", unit="m",
+        default=0.8, slider_min=0.4, slider_max=1400.1,
+        slider_step=0.1, scale_factor=1.e-6),
 
-    LinearScaledParameter(name="LASER_PULSE_LENGTH", ptype="compile", unit="s", default=5,
-                          slider_min=1, slider_max=150, slider_step=1, scale_factor=1.e-15),
+    LinearScaledParameter(
+        name="Laser_Pulse_Length", ptype="compile", unit="s",
+        default=5, slider_min=1, slider_max=150,
+        slider_step=1, scale_factor=1.e-15),
 ]
