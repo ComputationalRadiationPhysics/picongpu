ca670d99bd663a034b6ac0ae5987f57522ab3137
Axel Huebl <axel.huebl@plasma.ninja>
2017-11-17 18:14:51 +0100

    Docker: Fix LWFA Startup for MPI
    Properly start a login on bash, even with tbg.
    Make call shorter (just "lwfa").

--- share/picongpu/dockerfiles/ubuntu-1604/start_lwfa.sh
+++ share/picongpu/dockerfiles/ubuntu-1604/start_lwfa.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -l
 #
 
 isaac &
@@ -15,7 +15,7 @@ sleep 5
 # start PIConGPU
 cd /opt/picInputs/lwfa
 tbg \
-  -s bash \
+  -s "bash -l" \
   -c etc/picongpu/0001gpus_isaac.cfg \
   -t etc/picongpu/bash/mpirun.tpl \
   /tmp/lwfa_001
