cab054d5748489e079841f8a01c1235ea7720f3c
Sebastian Starke <s.starke@hzdr.de>
2017-12-05 08:07:42 +0100

    Group parameters in laserWakefield example

--- share/picongpu/examples/LaserWakefield/lib/python/picongpu/params.py
+++ share/picongpu/examples/LaserWakefield/lib/python/picongpu/params.py
@@ -21,30 +21,35 @@ from picongpu.input.parameters import LogScaledParameter, LinearScaledParameter
 pico = 1.e-12
 dt_r = 1. / 1.39e-16 * pico
 
-PARAMETER_LIST = [
-    LogScaledParameter(
-        name="Base_Density_SI", ptype="compile", unit="1/m^3",
-        default=25.0, slider_min=20.0, slider_max=26.0,
-        slider_step=1, base=10),
+PARAMETERS = {
+    'laser': [
+        LinearScaledParameter(
+            name="_A0", ptype="compile", unit="",
+            default=1.5, slider_min=0.1, slider_max=50.01,
+            slider_step=0.1),
 
-    LinearScaledParameter(
-        name="_A0", ptype="compile", unit="",
-        default=1.5, slider_min=0.1, slider_max=50.01,
-        slider_step=0.1),
+        LinearScaledParameter(
+            name="Wave_Length_SI", ptype="compile", unit="nm",
+            default=800.0, slider_min=400.0, slider_max=1400.0,
+            slider_step=1, scale_factor=1.e-9),
 
-    LinearScaledParameter(
-        name="Wave_Length_SI", ptype="compile", unit="nm",
-        default=800.0, slider_min=400.0, slider_max=1400.0,
-        slider_step=1, scale_factor=1.e-9),
+        LinearScaledParameter(
+            name="Pulse_Length_SI", ptype="compile", unit="fs",
+            default=5.0, slider_min=1.0, slider_max=150.0,
+            slider_step=1, scale_factor=1.e-15),
+    ],
+    'target': [
+        LogScaledParameter(
+            name="Base_Density_SI", ptype="compile", unit="1/m^3",
+            default=25.0, slider_min=20.0, slider_max=26.0,
+            slider_step=1, base=10),
+    ],
+    'resolution': [
+        LinearScaledParameter(
+            name="TBG_steps", ptype="run", unit="ps",
+            default=1.0, slider_min=0.1, slider_max=10.0,
+            slider_step=0.1, scale_factor=dt_r, dtype=int,
+            label="simulation time")
 
-    LinearScaledParameter(
-        name="Pulse_Length_SI", ptype="compile", unit="fs",
-        default=5.0, slider_min=1.0, slider_max=150.0,
-        slider_step=1, scale_factor=1.e-15),
-
-    LinearScaledParameter(
-        name="TBG_steps", ptype="run", unit="ps",
-        default=1.0, slider_min=0.1, slider_max=10.0,
-        slider_step=0.1, scale_factor=dt_r, dtype=int,
-        label="simulation time")
-]
+    ]
+}
