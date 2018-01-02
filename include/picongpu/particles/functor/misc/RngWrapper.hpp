0bf747ea6abd869f8b1c0f53b9e8394ba4457118
René Widera <r.widera@hzdr.de>
2018-01-02 10:02:52 +0100

    wrapper for random number generator
    Add wrapper to hide the alpaka accelerator which is needed to create rng numbers.
    
    - add class `RngWrapper`

---
+++ include/picongpu/particles/functor/misc/RngWrapper.hpp
@@ -0,0 +1,74 @@
+/* Copyright 2017 Rene Widera
+ *
+ * This file is part of PIConGPU.
+ *
+ * PIConGPU is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PIConGPU is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PIConGPU.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <pmacc/types.hpp>
+
+#include <functional>
+
+
+namespace picongpu
+{
+namespace particles
+{
+namespace functor
+{
+namespace misc
+{
+
+    /** wraps an random number generator together with an alpaka accelerator
+     *
+     * This class allows to generate random numbers without passing the accelerator
+     * to each functor call.
+     *
+     * @tparam T_Acc type of the alpaka accelerator
+     * @tparam T_Rng type of the random number generator
+     */
+    template<
+        typename T_Acc,
+        typename T_Rng
+    >
+    struct RngWrapper
+    {
+        DINLINE RngWrapper(
+            T_Acc const & acc,
+            T_Rng const & rng
+
+        ) :
+            m_acc( &acc ),
+            m_rng( rng )
+        { }
+
+        //! generate a random number
+        DINLINE
+        typename T_Rng::result_type
+        operator()()
+        {
+            return m_rng( *m_acc );
+        }
+
+        T_Acc const * m_acc;
+        mutable T_Rng m_rng;
+    };
+
+} // namepsace misc
+} // namespace functor
+} // namespace particles
+} // namespace picongpu
