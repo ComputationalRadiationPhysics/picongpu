4e18e8744065e8d3388cd0d9f9e22087439d1b51
Sebastian Starke <s.starke@hzdr.de>
2018-02-01 08:46:44 +0100

    PhaseSpaceVis: member signatures like in reader

--- lib/python/picongpu/visualization/phase_space_visualizer.py
+++ lib/python/picongpu/visualization/phase_space_visualizer.py
@@ -18,47 +18,54 @@ class Visualizer(object):
     Class for creating a matplotlib plot of phase space diagrams.
     """
 
-    def __init__(self, layout_config):
+    def __init__(self, run_directory):
         """
         Parameters
         ----------
-        layout_config: dictionary
-            containing information about particle species (e.g. 'e' for
-            electrons) and phase space momentum string (e.g. xpx, ypy, ypx)
+        run_directory : string
+            path to the run directory of PIConGPU
+            (the path before ``simOutput/``)
         """
+        if run_directory is None:
+            raise ValueError('The run_directory parameter can not be None!')
 
-        self.species = layout_config.get('particle_species', 'e')
-        self.ps = layout_config.get('phase_space_momentum', 'ypy')
+        # initialize the data-reader
+        self.phase_space = PhaseSpace(run_directory)
+        self.plt_obj = None
 
         # for unit-conversion from SI (taken from picongpu readthedocs)
         self.mu = 1.e6
         self.e_mc_r = 1. / (9.1e-31 * 2.9979e8)
 
-        self.phase_space = None
-        self.plt_obj = None
-
-    def visualize(self, path, iteration, ax):
+    def visualize(self, iteration, ax, species='e', species_filter='all',
+                  ps=None, **kwargs):
         """
         Creates a phase space plot on the provided axes object for
         the data of the given iteration using matpotlib.
 
         Parameters
         ----------
-        path: string
-            full path to the 'run' subdirectory of an experiment.
-
         iteration: int
             the iteration number for which data will be plotted.
-
         ax: matplotlib axes object
             the part of the figure where this plot will be shown.
+        species : string
+            short name of the particle species, e.g. 'e' for electrons
+            (defined in ``speciesDefinition.param``)
+        species_filter: string
+            name of the particle species filter, default is 'all'
+            (defined in ``particleFilters.param``)
+        ps : string
+            phase space selection in order: spatial, momentum component,
+            e.g. 'ypy' or 'ypx'
+        kwargs: dict
+            possible additional keyword args (e.g. for styling).
+            NOTE: no options from this parameter are considered yet!
         """
-
-        self.phase_space = PhaseSpace(path)
-
         data, meta = self.phase_space.get(
-            species=self.species,
-            ps=self.ps,
+            species=species,
+            species_filter=species_filter,
+            ps=ps,
             iteration=iteration)
 
         if self.plt_obj is None:
@@ -95,17 +102,19 @@ if __name__ == '__main__':
         def usage():
             print("usage:")
             print(
-                "python", sys.argv[0], "-p <path to run directory> -i <iteration>\
-                -s <particle species> -m <momentum term>")
+                "python", sys.argv[0],
+                "-p <path to run_directory> -i <iteration>"
+                " -s <particle species> -f <species_filter>"
+                " -m <momentum term>")
 
         path = None
         iteration = None
         species = None
+        filtr = None
         momentum = None
-
         try:
-            opts, args = getopt.getopt(sys.argv[1:], "hp:i:s:m:", [
-                "help", "path", "iteration", "species", "momentum"])
+            opts, args = getopt.getopt(sys.argv[1:], "hp:i:s:f:m:", [
+                "help", "path", "iteration", "species", "filter", "momentum"])
         except getopt.GetoptError as err:
             print(err)
             usage()
@@ -121,6 +130,8 @@ if __name__ == '__main__':
                 iteration = int(arg)
             elif opt in ["-s", "--species"]:
                 species = arg
+            elif opt in ["-f", "--filter"]:
+                filtr = arg
             elif opt in ["-m", "--momentum"]:
                 momentum = arg
 
@@ -129,22 +140,19 @@ if __name__ == '__main__':
             print("Path to 'run' directory and iteration have to be provided!")
             usage()
             sys.exit(2)
-
         if species is None:
             species = 'e'
             print("Particle species was not given, will use", species)
-
+        if filtr is None:
+            filtr = 'all'
+            print("Species filter was not given, will use", filtr)
         if momentum is None:
             momentum = 'ypy'
             print("Momentum term was not given, will use", momentum)
 
-        layout_config = {
-            'particle_species': species,
-            'phase_space_momentum': momentum
-        }
-
         fig, ax = plt.subplots(1, 1)
-        Visualizer(layout_config).visualize(path, iteration, ax)
+        Visualizer(path).visualize(iteration, ax, species=species,
+                                   species_filter=filtr, ps=momentum)
         plt.show()
 
     main()
