7f54c2ded215c8edd57db6d96d65a3138cb1fcd9
Sebastian Starke <s.starke@hzdr.de>
2018-02-01 08:43:39 +0100

    EnergyHistogramVis: member signatures like in reader

--- lib/python/picongpu/visualization/energy_histogram_visualizer.py
+++ lib/python/picongpu/visualization/energy_histogram_visualizer.py
@@ -16,49 +16,50 @@ class Visualizer:
     Class for creation of histogram plots on a logscaled y-axis.
     """
 
-    def __init__(self, layout_config):
+    def __init__(self, run_directory):
         """
         Parameters
         ----------
-        layout_config: dictionary
-            containing information about particle species (e.g. 'e' for
-            electrons)
+        run_directory : string
+            path to the run directory of PIConGPU
+            (the path before ``simOutput/``)
         """
+        if run_directory is None:
+            raise ValueError('The run_directory parameter can not be None!')
 
-        self.species = layout_config.get('particle_species', 'e')
-        self.energy_histogram = None
+        self.energy_histogram = EnergyHistogram(run_directory)
         self.plt_obj = None
 
-    def visualize(self, path, iteration, ax):
+    def visualize(self, iteration, ax, species='e', species_filter="all",
+                  include_overflow=False, **kwargs):
         """
         Creates a semilogy plot on the provided axes object for
         the data of the given iteration using matpotlib.
 
         Parameters
         ----------
-        path: string
-            full path to the 'run' subdirectory of an experiment.
         iteration: int
             the iteration number for which data will be plotted.
-
         ax: matplotlib axes object
             the part of the figure where this plot will be shown.
+        species : string
+            short name of the particle species, e.g. 'e' for electrons
+            (defined in ``speciesDefinition.param``)
+        species_filter: string
+            name of the particle species filter, default is 'all'
+            (defined in ``particleFilters.param``)
+        include_overflow: boolean, default: False
+            Include overflow and underflow bins as the first/last bins.
+        kwargs: dict
+            possible additional keyword args (e.g. for styling).
+            NOTE: no options from this parameter are considered yet!
         """
 
-        self.energy_histogram = EnergyHistogram(path)
-
-        # catch exception when histogram file does not exist yet
-        try:
-            self.energy_histogram.get_data_path(species=self.species)
-        except IOError:
-            print(
-                "No histogram file exists yet. Wait and hit visualize again"
-                " after a while!")
-            return
-
         counts, energy_bins = self.energy_histogram.get(
-            species=self.species, iteration=iteration)
-        # plot energy per bin after cleaning the plot
+            species=species,
+            species_filter=species_filter,
+            iteration=iteration,
+            include_overflow=include_overflow)
 
         # if it is the first time for plotting then object is
         # created, otherwise only data is updated
@@ -70,7 +71,7 @@ class Visualizer:
         ax.set_xlabel('Energy')
         ax.set_ylabel('Count')
         ax.set_title('Energy Histogram for species ' +
-                     self.species + ', iteration ' + str(iteration))
+                     species + ', iteration ' + str(iteration))
 
 
 if __name__ == '__main__':
@@ -82,17 +83,21 @@ if __name__ == '__main__':
         def usage():
             print("usage:")
             print(
-                "python", sys.argv[0], "-p <path to run directory> -i\
-                <iteration>"
-                " -s <particle species>")
+                "python", sys.argv[0],
+                "-p <path to run_directory> -i <iteration>"
+                " -s <particle species> -f <species_filter>"
+                " -o <include_overflow>")
 
         path = None
         iteration = None
         species = None
+        filtr = None
+        overflow = None
 
         try:
-            opts, args = getopt.getopt(sys.argv[1:], "hp:i:s:", [
-                "help", "path", "iteration", "species"])
+            opts, args = getopt.getopt(sys.argv[1:], "hp:i:s:f:o:", [
+                "help", "path", "iteration", "species", "filter",
+                "include_overflow"])
         except getopt.GetoptError as err:
             print(err)
             usage()
@@ -108,6 +113,10 @@ if __name__ == '__main__':
                 iteration = int(arg)
             elif opt in ["-s", "--species"]:
                 species = arg
+            elif opt in ["-f", "--filter"]:
+                filtr = arg
+            elif opt in ["-o", "--include_overflow"]:
+                overflow = bool(arg)
 
         # check that we got all args that we need
         if path is None or iteration is None:
@@ -117,13 +126,17 @@ if __name__ == '__main__':
         if species is None:
             species = 'e'
             print("Particle species was not given, will use", species)
-
-        layout_config = {
-            'particle_species': species
-        }
+        if filtr is None:
+            filtr = 'all'
+            print("Species filter was not given, will use", filtr)
+        if overflow is None:
+            overflow = False
+            print("include_overflow was not given, will use", overflow)
 
         fig, ax = plt.subplots(1, 1)
-        Visualizer(layout_config).visualize(path, iteration, ax)
+        Visualizer(path).visualize(iteration, ax, species=species,
+                                   species_filter=filtr,
+                                   include_overflow=overflow)
         plt.show()
 
     main()
