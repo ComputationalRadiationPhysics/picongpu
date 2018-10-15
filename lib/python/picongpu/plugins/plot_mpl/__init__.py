c4108f9a97a3b5a72c7099345b4d05181c152275
Sebastian Starke <s.starke@hzdr.de>
2018-10-15 13:01:08 +0200

    Add import aliases to plot_mpl init

---
+++ lib/python/picongpu/plugins/plot_mpl/__init__.py
@@ -0,0 +1,9 @@
+from .energy_histogram_visualizer import Visualizer as \
+    EnergyHistogramVisualizer
+from .phase_space_visualizer import Visualizer as PhaseSpaceVisualizer
+from .png_visualizer import Visualizer as PNGVisualizer
+
+
+__all__ = ["EnergyHistogramVisualizer",
+           "PhaseSpaceVisualizer",
+           "PNGVisualizer"]
