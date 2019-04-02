44b3470d6d98995f1b8b3a0ef08ae93506d6b26f
Paweł Ordyna <p.ordyna@hzdr.de>
2019-04-02 13:17:40 +0200

    updated comments, all GPU rows included

--- docs/source/usage/workflows/memoryPerDevice.py
+++ docs/source/usage/workflows/memoryPerDevice.py
@@ -9,6 +9,7 @@ License: GPLv3+
 """
 
 from picongpu.utils import MemoryCalculator
+from math import ceil
 
 """
 @file
@@ -26,91 +27,113 @@ maximum memory load due to hosting a large part of the target.
 
 
 cell_size = 0.8e-6 / 384.  # 2.083e-9 m
-
 y0 = 0.5e-6  # position of foil front surface (m)
-delta_y = 1.0e-6  # target thickness (m)
-y1 = y0 + delta_y  # position of foil rear surface (m)
-L = 10.e-9  # pre-plasma scale length (m)
-L_cutoff = 4.0 * L  # pre-plasma length (m)
+y1 = 1.5e-6  # position of foil rear surface (m)
+L = 10e-9  # pre-plasma scale length (m)
+L_cutoff = 4 * L  # pre-plasma length (m)
 
-# number of cells per device
-Nx = 128
-Ny = 640
-Nz = 1
+sim_dim = 2
+# number of cells in the simulation
+Nx_all, Ny_all, Nz_all = 256, 1280, 1
+# number of GPU rows in each direction
+x_rows, y_rows, z_rows = 2, 2, 1
+# number of cells pro GPU
+Nx, Ny, Nz = Nx_all / x_rows, Ny_all / y_rows, Nz_all / z_rows
 
-vacuum_cells = (y0 - L_cutoff) / cell_size  # with pre-plasma: 221 cells
-target_cells = (y1 - y0 + 2 * L_cutoff) / cell_size  # 398 cells
+vacuum_cells = ceil((y0 - L_cutoff) / cell_size)  # in front of the target
+# target cells (between surfaces + pre-plasma)
+target_cells = ceil((y1 - y0 + 2 * L_cutoff) / cell_size)
+# number of cells (y direction) on each GPU row
+GPU_rows = [0] * y_rows
+cells_to_spread = vacuum_cells + target_cells
+# spread the cells on the GPUs
+for ii, _ in enumerate(GPU_rows):
+    if cells_to_spread >= Ny:
+        GPU_rows[ii] = Ny
+        cells_to_spread -= Ny
+    else:
+        GPU_rows[ii] = cells_to_spread
+        break
+# remove vacuum cells from the front rows
+extra_cells = vacuum_cells
+for ii, _ in enumerate(GPU_rows):
+    if extra_cells >= Ny:
+        GPU_rows[ii] = 0
+        extra_cells -= Ny
+    else:
+        GPU_rows[ii] -= extra_cells
+        break
 
 pmc = MemoryCalculator(Nx, Ny, Nz)
-
-target_x = Nx  # full transversal dimension of the device
-target_y = target_cells  # only the first row of devices holds the target
-target_z = Nz
 
 # typical number of particles per cell which is multiplied later for
 # each species and its relative number of particles
 N_PPC = 6
-
 # conversion factor to megabyte
 megabyte = 1.0 / (1024 * 1024)
 
-print("Memory requirement per device:")
-# field memory per device
-field_device = pmc.mem_req_by_fields(Nx, Ny, Nz, field_tmp_slots=2,
-                                     particle_shape_order=2)
-print("+ fields: {:.2f} MB".format(
-      field_device * megabyte))
+target_x = Nx  # full transverse dimension of the GPU
+target_z = Nz
 
-# electron macroparticles per supercell
-e_PPC = N_PPC * (
-    # H,C,N pre-ionization - higher weighting electrons
-    3 \
-    # electrons created from C ionization
-    + (6 - 2) \
-    # electrons created from N ionization
-    + (7 - 2)
-)
-# particle memory per device - only the target area contributes here
-e_device = pmc.mem_req_by_particles(
-    target_x, target_y, target_z,
-    num_additional_attributes=0,
-    particles_per_cell=e_PPC
-)
-H_device = pmc.mem_req_by_particles(
-    target_x, target_y, target_z,
-    # no bound electrons since H is preionized
-    num_additional_attributes=0,
-    particles_per_cell=N_PPC
-)
-C_device = pmc.mem_req_by_particles(
-    target_x, target_y, target_z,
-    num_additional_attributes=1,
-    particles_per_cell=N_PPC
-)
-N_device = pmc.mem_req_by_particles(
-    target_x, target_y, target_z,
-    num_additional_attributes=1,
-    particles_per_cell=N_PPC
-)
-# memory for calorimeters
-cal_device = pmc.mem_req_by_calorimeter(
-    n_energy=1024, n_yaw=360, n_pitch=1
-) * 2  # electrons and protons
-# memory for random number generator states
-rng_device = pmc.mem_req_by_rng(Nx, Ny, Nz)
+sx = lambda n: {1: "st", 2: "nd", 3: "rd"}.get(n if n < 20 else int(str(n)[-1]), "th")
+for row, target_y in enumerate(GPU_rows):
+    print("{}{} row of GPUs:".format(row + 1, sx(row + 1)))
+    print("* Memory requirement per GPU:")
+    # field memory per GPU
+    field_gpu = pmc.mem_req_by_fields(Nx, Ny, Nz, field_tmp_slots=2,
+                                      particle_shape_order=2, sim_dim=sim_dim)
+    print(" + fields: {:.2f} MB".format(
+        field_gpu * megabyte))
 
-print("+ species:")
-print("- e: {:.2f} MB".format(e_device * megabyte))
-print("- H: {:.2f} MB".format(H_device * megabyte))
-print("- C: {:.2f} MB".format(C_device * megabyte))
-print("- N: {:.2f} MB".format(N_device * megabyte))
-print("+ RNG states: {:.2f} MB".format(
-    rng_device * megabyte))
-print(
-    "+ particle calorimeters: {:.2f} MB".format(
-        cal_device * megabyte))
+    # electron macroparticles per supercell
+    e_PPC = N_PPC * (
+            # H,C,N pre-ionization - higher weighting electrons
+            3
+            # electrons created from C ionization
+            + (6 - 2)
+            # electrons created from N ionization
+            + (7 - 2)
+    )
+    # particle memory per GPU - only the target area contributes here
+    e_gpu = pmc.mem_req_by_particles(
+        target_x, target_y, target_z,
+        num_additional_attributes=0,
+        particles_per_cell=e_PPC
+    )
+    H_gpu = pmc.mem_req_by_particles(
+        target_x, target_y, target_z,
+        # no bound electrons since H is preionized
+        num_additional_attributes=0,
+        particles_per_cell=N_PPC
+    )
+    C_gpu = pmc.mem_req_by_particles(
+        target_x, target_y, target_z,
+        num_additional_attributes=1,  # number of bound electrons
+        particles_per_cell=N_PPC
+    )
+    N_gpu = pmc.mem_req_by_particles(
+        target_x, target_y, target_z,
+        num_additional_attributes=1,
+        particles_per_cell=N_PPC
+    )
+    # memory for calorimeters
+    cal_gpu = pmc.mem_req_by_calorimeter(
+        n_energy=1024, n_yaw=360, n_pitch=1
+    ) * 2  # electrons and protons
+    # memory for random number generator states
+    rng_gpu = pmc.mem_req_by_rng(Nx, Ny, Nz)
 
-mem_sum = (field_device + e_device + H_device + C_device +
-           N_device + rng_device + cal_device)
-print("Required memory per device: {:.2f} MB".format(
-    mem_sum * megabyte))
+    print(" + species:")
+    print("  - e: {:.2f} MB".format(e_gpu * megabyte))
+    print("  - H: {:.2f} MB".format(H_gpu * megabyte))
+    print("  - C: {:.2f} MB".format(C_gpu * megabyte))
+    print("  - N: {:.2f} MB".format(N_gpu * megabyte))
+    print(" + RNG states: {:.2f} MB".format(
+        rng_gpu * megabyte))
+    print(
+        " + particle calorimeters: {:.2f} MB".format(
+            cal_gpu * megabyte))
+
+    mem_sum = field_gpu + e_gpu + H_gpu + C_gpu + N_gpu + rng_gpu + cal_gpu
+    print("* Sum of required GPU memory: {:.2f} MB".format(
+        mem_sum * megabyte))
