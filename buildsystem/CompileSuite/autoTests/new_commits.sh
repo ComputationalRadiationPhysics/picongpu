124e026d685342dd7537b3a6b3b6885acb7d1412
Axel Huebl <axel.huebl@plasma.ninja>
2019-05-13 16:40:57 +0200

    CUDA 9.0+, GCC 5.1+, Boost 1.65.1+
    Increase the dependencies to CUDA 9.0+, which in turn
    requires Boost 1.65.1+ to run.
    
    With that prepare, we also update our CI to run on GCC 5.1+
    which will make the transition to C++14 possible soon. While
    GCC 4.9 is technically still possible, we deprecate support
    and continous CI for it to move forward for the aforementioned
    reasons.

--- buildsystem/CompileSuite/autoTests/new_commits.sh
+++ buildsystem/CompileSuite/autoTests/new_commits.sh
@@ -94,9 +94,9 @@ touch "$thisDir"runGuard
             #export PIC_COMPILE_SUITE_CMAKE="-DPIC_ENABLE_PNG=OFF -DALPAKA_CUDA_ARCH=35"
             export PIC_BACKEND="cuda"
             . /etc/profile
-            module load gcc/4.9.4 boost/1.62.0 cmake/3.11.0 cuda/8.0.44 openmpi/1.10.4
+            module load gcc/5.1.0 boost/1.65.1 cmake/3.11.0 cuda/9.0.176 openmpi/3.0.4
             module load libSplash/1.7.0 adios/1.13.1
-            module load pngwriter/0.7.0
+            module load pngwriter/0.7.0 zlib/1.2.11
             module load libjpeg-turbo/1.5.1 icet/2.1.1 jansson/2.9 isaac/1.4.0
 
             # compile all examples, fetch output and return code
