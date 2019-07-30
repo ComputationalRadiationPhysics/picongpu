aaefa0230d12adc7ec59bac4f03e7dccead594a0
Axel Huebl <axel.huebl@plasma.ninja>
2019-07-30 16:58:53 -0500

    Resource Monitor Plugin: Warnings
    Move the boost property tree for writing of the final
    files out of the NVCC compile path. This avoids awkward
    work-arounds and reduces warnings.
    
    Also fixes a potential use-after-free (`temporary.str().c_str()`).

---
+++ include/picongpu/plugins/ResourceLog.cpp
@@ -0,0 +1,90 @@
+/* Copyright 2016-2019 Erik Zenker, Axel Huebl
+ *
+ * This file is part of PMacc.
+ *
+ * PMacc is free software: you can redistribute it and/or modify
+ * it under the terms of either the GNU General Public License or
+ * the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PMacc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License and the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and the GNU Lesser General Public License along with PMacc.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Boost
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+
+// STL
+#include <string>    /* std::string */
+#include <sstream>   /* std::stringstream */
+#include <map>       /* std::map */
+#include <stdexcept> /* std::runtime_error */
+
+// C LIB
+#include <stdint.h> /* uint32_t */
+
+
+namespace picongpu
+{
+namespace detail
+{
+    std::stringstream
+    writeMapToPropertyTree(
+        std::map< std::string, size_t > valueMap,
+        std::string outputFormat
+    )
+    {
+        // Create property tree which contains the resource information
+        using boost::property_tree::ptree;
+        ptree pt;
+
+        for( auto it = valueMap.begin(); it != valueMap.end(); ++it ) {
+            pt.put( it->first, it->second );
+        }
+
+        // Write property tree to string stream
+        std::stringstream ss;
+        if( outputFormat == "json" )
+        {
+            write_json( ss, pt, false );
+        }
+        else if( outputFormat == "jsonpp" )
+        {
+            write_json( ss, pt, true );
+        }
+        else if( outputFormat == "xml" )
+        {
+            write_xml( ss, pt );
+        }
+        else if( outputFormat == "xmlpp" )
+        {
+            write_xml(
+                ss,
+                pt,
+                boost::property_tree::xml_writer_make_settings< std::string >( '\t', 1 )
+            );
+        }
+        else
+        {
+            throw std::runtime_error(
+                std::string( "resourcelog.format " ) +
+                outputFormat +
+                std::string( " is not known, use json or xml." )
+            );
+        }
+
+        return ss;
+    }
+} // namespace detail
+} // namespace picongpu
+
