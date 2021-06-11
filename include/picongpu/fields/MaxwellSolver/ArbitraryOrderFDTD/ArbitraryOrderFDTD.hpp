6861f073290afc6124950c140504f4f9752c369d
Sergei Bastrakov <sergey.bastrakov@gmail.com>
2021-06-11 13:07:20 +0000

    Add CFL check for the arbitrary-order FDTD solver

--- include/picongpu/fields/MaxwellSolver/ArbitraryOrderFDTD/ArbitraryOrderFDTD.hpp
+++ include/picongpu/fields/MaxwellSolver/ArbitraryOrderFDTD/ArbitraryOrderFDTD.hpp
@@ -23,11 +23,48 @@
 
 #include "picongpu/fields/MaxwellSolver/ArbitraryOrderFDTD/ArbitraryOrderFDTD.def"
 #include "picongpu/fields/MaxwellSolver/ArbitraryOrderFDTD/Derivative.hpp"
+#include "picongpu/fields/MaxwellSolver/ArbitraryOrderFDTD/Weights.hpp"
+#include "picongpu/fields/MaxwellSolver/CFLChecker.hpp"
 #include "picongpu/fields/differentiation/Curl.hpp"
 
 #include <cstdint>
+#include <stdexcept>
 #include <string>
 
+namespace picongpu
+{
+    namespace fields
+    {
+        namespace maxwellSolver
+        {
+            /** Specialization of the CFL condition checker for the arbitrary-order FDTD
+             *
+             * @tparam T_neighbors number of neighbors used to calculate the derivatives
+             */
+            template<uint32_t T_neighbors>
+            struct CFLChecker<ArbitraryOrderFDTD<T_neighbors>>
+            {
+                //! Check the CFL condition, throws when failed
+                void operator()() const
+                {
+                    // The equations are given at https://picongpu.readthedocs.io/en/latest/models/AOFDTD.html
+                    auto weights = aoFDTD::AOFDTDWeights<T_neighbors>{};
+                    auto additionalFactor = 0.0_X;
+                    for(uint32_t i = 0u; i < T_neighbors; i++)
+                    {
+                        auto const term = (i % 2) ? -weights[i] : weights[i];
+                        additionalFactor += term;
+                    }
+                    if(SPEED_OF_LIGHT * SPEED_OF_LIGHT * DELTA_T * DELTA_T * INV_CELL2_SUM * additionalFactor
+                           * additionalFactor
+                       > 1.0_X)
+                        throw std::runtime_error(
+                            "Courant-Friedrichs-Lewy condition check failed, check your grid.param file");
+                }
+            };
+        } // namespace maxwellSolver
+    } // namespace fields
+} // namespace picongpu
 
 namespace pmacc
 {
