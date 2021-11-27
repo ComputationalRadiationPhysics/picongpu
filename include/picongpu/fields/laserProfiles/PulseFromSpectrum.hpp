da3aaf722762bbcba235ecd397ac657b1bda6cda
nils <prinz.nils@web.de>
2021-11-27 20:41:42 +0100

    Formatted the code with ClangFormat-12.

--- include/picongpu/fields/laserProfiles/PulseFromSpectrum.hpp
+++ include/picongpu/fields/laserProfiles/PulseFromSpectrum.hpp
@@ -22,8 +22,8 @@
 
 #include "picongpu/simulation_defines.hpp"
 
-#include <pmacc/mappings/simulation/SubGrid.hpp>
 #include <pmacc/dataManagement/DataConnector.hpp>
+#include <pmacc/mappings/simulation/SubGrid.hpp>
 
 
 namespace picongpu
@@ -34,25 +34,33 @@ namespace picongpu
         {
             namespace pulseFromSpectrum
             {
-                template< typename T_Params >
+                template<typename T_Params>
                 struct Unitless : public T_Params
                 {
                     using Params = T_Params;
 
-                    static constexpr float_X WAVE_LENGTH = float_X( Params::WAVE_LENGTH_SI / UNIT_LENGTH ); // unit: meter
-                    static constexpr float_X v0 = (::picongpu::SI::SPEED_OF_LIGHT_SI / Params::WAVE_LENGTH_SI) * UNIT_TIME; // unit: seconds^-1
-                    static constexpr float_X PULSE_LENGTH = float_X( Params::PULSE_LENGTH_SI / UNIT_TIME ); // unit: seconds (1 sigma)
-                    static constexpr float_X AMPLITUDE = float_X( Params::AMPLITUDE_SI / UNIT_EFIELD ); // unit: volt / meter
-                    static constexpr float_X PULSE_INIT = float_X( Params::PULSE_INIT); // unit: none
-                    static constexpr float_X GDD = float_X( Params::GDD_SI / (UNIT_TIME * UNIT_TIME) ); // unit: seconds^2
-                    static constexpr float_X TOD = float_X( Params::TOD_SI / (UNIT_TIME * UNIT_TIME * UNIT_TIME) ); // unit: seconds^3
-                    static constexpr float_X W0 = float_X( Params::W0_SI / UNIT_LENGTH ); // unit: meter
-                    static constexpr float_X FOCUS_POS = float_X( Params::FOCUS_POS_SI / UNIT_LENGTH ); // unit: meter
-                    static constexpr float_X INIT_TIME = float_X( ( Params::PULSE_INIT * Params::PULSE_LENGTH_SI) / UNIT_TIME ); // unit: seconds (full inizialisation length)
+                    static constexpr float_X WAVE_LENGTH
+                        = float_X(Params::WAVE_LENGTH_SI / UNIT_LENGTH); // unit: meter
+                    static constexpr float_X v0
+                        = (::picongpu::SI::SPEED_OF_LIGHT_SI / Params::WAVE_LENGTH_SI) * UNIT_TIME; // unit: seconds^-1
+                    static constexpr float_X PULSE_LENGTH
+                        = float_X(Params::PULSE_LENGTH_SI / UNIT_TIME); // unit: seconds (1 sigma)
+                    static constexpr float_X AMPLITUDE
+                        = float_X(Params::AMPLITUDE_SI / UNIT_EFIELD); // unit: volt / meter
+                    static constexpr float_X PULSE_INIT = float_X(Params::PULSE_INIT); // unit: none
+                    static constexpr float_X GDD
+                        = float_X(Params::GDD_SI / (UNIT_TIME * UNIT_TIME)); // unit: seconds^2
+                    static constexpr float_X TOD
+                        = float_X(Params::TOD_SI / (UNIT_TIME * UNIT_TIME * UNIT_TIME)); // unit: seconds^3
+                    static constexpr float_X W0 = float_X(Params::W0_SI / UNIT_LENGTH); // unit: meter
+                    static constexpr float_X FOCUS_POS = float_X(Params::FOCUS_POS_SI / UNIT_LENGTH); // unit: meter
+                    static constexpr float_X INIT_TIME = float_X(
+                        (Params::PULSE_INIT * Params::PULSE_LENGTH_SI)
+                        / UNIT_TIME); // unit: seconds (full inizialisation length)
 
                     /* Rayleigh-Length in y-direction
                      */
-                    static constexpr float_X Y_R = float_X( PI * W0 * W0 / WAVE_LENGTH ); // unit: meter
+                    static constexpr float_X Y_R = float_X(PI * W0 * W0 / WAVE_LENGTH); // unit: meter
                 };
             } // namespace pulseFromSpectrum
 
@@ -75,7 +83,7 @@ namespace picongpu
                      */
                     HDINLINE float_X Waist(float_X y_to_focus)
                     {
-                        return Unitless::W0 * math::sqrt( 1.0 + math::pow(y_to_focus / Unitless::Y_R, 2.0) );
+                        return Unitless::W0 * math::sqrt(1.0 + math::pow(y_to_focus / Unitless::Y_R, 2.0));
                     }
 
                     /** inverse radius of the beam-curvature
@@ -83,9 +91,10 @@ namespace picongpu
                      */
                     HDINLINE float_X R_inv(float_X y_to_focus)
                     {
-                        //??? if Unitless::Y_R is inserted in (88) in pow() --> compile error???, why not in line 78, why does this workaround work?
+                        //??? if Unitless::Y_R is inserted in (97) in pow() --> compile error???, why not in line 86,
+                        // why does this workaround work?
                         float_X y_R = Unitless::Y_R;
-                        return y_to_focus / ( math::pow(y_to_focus, 2.0) + math::pow(y_R, 2.0) );
+                        return y_to_focus / (math::pow(y_to_focus, 2.0) + math::pow(y_R, 2.0));
                     }
 
                     /** gauss spectrum
@@ -97,12 +106,15 @@ namespace picongpu
                     {
                         // Norm is choosen so that the maximum after fourier transformation is unity.
                         float_X norm = 0.5 * math::sqrt(float_X(PI)) * Unitless::PULSE_LENGTH;
-                        return norm * math::exp( -1.0 * math::pow( Unitless::PULSE_LENGTH * float_X(PI) * ( v - Unitless::v0 ), 2.0) );
+                        return norm
+                            * math::exp(
+                                   -1.0 * math::pow(Unitless::PULSE_LENGTH * float_X(PI) * (v - Unitless::v0), 2.0));
                     }
 
                     /** transversal spectrum at the init plane
-                     * to implement a laser-pulse with a transversal profile, the spectrum has to be depending on the distance to the beam axis:
-                     * spectrum(v) --> spectrum(v, r) (the transversal profile is radial symmetric)
+                     * to implement a laser-pulse with a transversal profile, the spectrum has to be depending on the
+                     * distance to the beam axis: spectrum(v) --> spectrum(v, r) (the transversal profile is radial
+                     * symmetric)
                      * @param v frequency
                      * @param r2 distance to beam axis to the power of 2
                      * @param y_to_focus y-coordinate relative to focus-position
@@ -110,14 +122,16 @@ namespace picongpu
                     HDINLINE float_X transversal_spectrum(float_X v, float_X r2, float_X y_to_focus)
                     {
                         float_X a = 1.0 + math::pow(y_to_focus / Unitless::Y_R, 2.0);
-                        float_X transversal_spectrum = gauss_spectrum(v) * math::pow(a, -0.5) * math::exp( -1.0*r2 / math::pow(Waist(y_to_focus), 2.0) );
+                        float_X transversal_spectrum = gauss_spectrum(v) * math::pow(a, -0.5)
+                            * math::exp(-1.0 * r2 / math::pow(Waist(y_to_focus), 2.0));
                         return transversal_spectrum;
                     }
 
                     /** phase of the pulse depending on frequency and radius to beam axis at the init plane
                      * the phase is altered for two purposes:
                      * 1. to be able to choose freely the GDD/TOD of the laser-pulse
-                     * 2. to implement the transversal profile (thus the phase depends on the distance to the beam axis)
+                     * 2. to implement the transversal profile (thus the phase depends on the distance to the beam
+                     * axis)
                      * @param v frequency
                      * @param r2 distance to beam axis to the power of 2
                      * @param y_to_focus y-coordinate relative to focus-position
@@ -125,13 +139,16 @@ namespace picongpu
                     HDINLINE float_X phase_v(float_X v, float_X r2, float_X y_to_focus)
                     {
                         float_X TD = -1.0 * y_to_focus / SPEED_OF_LIGHT;
-                        float_X phase_shift_TD = float_X( TD * 2.0 * float_X(PI) * ( v - Unitless::v0 ) );
-                        float_X phase_shift_GDD = float_X( 0.5 * Unitless::GDD * math::pow( 2.0 * float_X(PI) * ( v - Unitless::v0 ), 2.0) );
-                        float_X phase_shift_TOD = float_X( (1.0/6.0) * Unitless::TOD * math::pow( 2.0 * float_X(PI) * (v - Unitless::v0), 3.0) );
-                        float_X phase_shift_transversal_1 = float_X( -1.0 * math::atan( y_to_focus / Unitless::Y_R ) );
-                        float_X phase_shift_transversal_2 = float_X( 2.0 * PI * v * y_to_focus / SPEED_OF_LIGHT );
-                        float_X phase_shift_transversal_3 = float_X( r2 * PI * v * R_inv(y_to_focus) / SPEED_OF_LIGHT );
-                        float_X phase_v = phase_shift_TD + phase_shift_GDD + phase_shift_TOD + phase_shift_transversal_1 + phase_shift_transversal_2 + phase_shift_transversal_3;
+                        float_X phase_shift_TD = float_X(TD * 2.0 * float_X(PI) * (v - Unitless::v0));
+                        float_X phase_shift_GDD
+                            = float_X(0.5 * Unitless::GDD * math::pow(2.0 * float_X(PI) * (v - Unitless::v0), 2.0));
+                        float_X phase_shift_TOD = float_X(
+                            (1.0 / 6.0) * Unitless::TOD * math::pow(2.0 * float_X(PI) * (v - Unitless::v0), 3.0));
+                        float_X phase_shift_transversal_1 = float_X(-1.0 * math::atan(y_to_focus / Unitless::Y_R));
+                        float_X phase_shift_transversal_2 = float_X(2.0 * PI * v * y_to_focus / SPEED_OF_LIGHT);
+                        float_X phase_shift_transversal_3 = float_X(r2 * PI * v * R_inv(y_to_focus) / SPEED_OF_LIGHT);
+                        float_X phase_v = phase_shift_TD + phase_shift_GDD + phase_shift_TOD
+                            + phase_shift_transversal_1 + phase_shift_transversal_2 + phase_shift_transversal_3;
                         return phase_v;
                     }
 
@@ -144,31 +161,33 @@ namespace picongpu
                     HDINLINE float_X E_t_dft(uint32_t currentStep, float_X r2, float_X y_to_focus)
                     {
                         // number of steps for the fourier-transformation
-                        int N = int(( int(Unitless::PULSE_INIT * Unitless::PULSE_LENGTH / DELTA_T) - 1) / 2);
+                        int N = int((int(Unitless::PULSE_INIT * Unitless::PULSE_LENGTH / DELTA_T) - 1) / 2);
 
                         // timesteps for DFT range from -N*dt to N*dt -> 2N+1 timesteps, equally spaced
-                        float_X const runTime = float_X( (int(currentStep) - N) * DELTA_T);
-            
+                        float_X const runTime = float_X((int(currentStep) - N) * DELTA_T);
+
                         /* Calculation of the E(t) using trigonometric Interpolation.
                          * Coefficients can be easily determined cause the spectrum is given.
                          */
-                        float_64 E_a = (1.0/DELTA_T) * transversal_spectrum(0.0, r2, y_to_focus) * math::cos( phase_v(0.0, r2, y_to_focus) );   // from first symm. coeff. (E_a(k=0) = a/2)
-                        float_64 E_b = 0.0;                                                                                                     // from first antisymm. coeff. (E_b(k=0) = 0)
+                        float_64 E_a = (1.0 / DELTA_T) * transversal_spectrum(0.0, r2, y_to_focus)
+                            * math::cos(phase_v(0.0, r2, y_to_focus)); // from first symm. coeff. (E_a(k=0) = a/2)
+                        float_64 E_b = 0.0; // from first antisymm. coeff. (E_b(k=0) = 0)
 
                         // rest of trig. Interpolation
-                        for(int k = 1; k < N+1; ++k)
+                        for(int k = 1; k < N + 1; ++k)
                         {
-                            float_X v_k = k / ((2*N + 1) * DELTA_T);                                                    // discrete values of frequency
-                            float_X a = (2.0/DELTA_T) * transversal_spectrum(v_k, r2, y_to_focus) * math::cos( phase_v(v_k, r2, y_to_focus) );  // symm. coeff. trig. Interpolation
-                            float_X b = (2.0/DELTA_T) * transversal_spectrum(v_k, r2, y_to_focus) * math::sin( phase_v(v_k, r2, y_to_focus) );  // antisymm. coeff. trig. Interpolation
+                            float_X v_k = k / ((2 * N + 1) * DELTA_T); // discrete values of frequency
+                            float_X a = (2.0 / DELTA_T) * transversal_spectrum(v_k, r2, y_to_focus)
+                                * math::cos(phase_v(v_k, r2, y_to_focus)); // symm. coeff. trig. Interpolation
+                            float_X b = (2.0 / DELTA_T) * transversal_spectrum(v_k, r2, y_to_focus)
+                                * math::sin(phase_v(v_k, r2, y_to_focus)); // antisymm. coeff. trig. Interpolation
 
                             E_a += a * math::cos(2.0 * float_X(PI) * v_k * runTime);
                             E_b += b * math::sin(2.0 * float_X(PI) * v_k * runTime);
-
                         }
 
                         // E(t)-Field derived from Spectrum
-                        float_64 E_t = (E_a + E_b) / float_X(2*N + 1);
+                        float_64 E_t = (E_a + E_b) / float_X(2 * N + 1);
                         E_t *= Unitless::AMPLITUDE;
 
                         return E_t;
@@ -186,8 +205,7 @@ namespace picongpu
                         DataSpace<simDim> const& offsetToTotalDomain,
                         float3_X const& elong,
                         float_X const phase,
-                        uint32_t const currentStep
-                        )
+                        uint32_t const currentStep)
                         : m_elong(elong)
                         , m_phase(phase)
                         , m_currentStep(currentStep)
@@ -269,7 +287,7 @@ namespace picongpu
                 float_X phase;
                 uint32_t m_currentStep;
                 typename FieldE::DataBoxType dataBoxE;
-                DataSpace< simDim > offsetToTotalDomain;
+                DataSpace<simDim> offsetToTotalDomain;
 
                 /** constructor
                  *
