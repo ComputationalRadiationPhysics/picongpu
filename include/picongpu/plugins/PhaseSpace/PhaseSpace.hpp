72d35138aa69c8a6fb7cf6d771f823571292f1a1
Franz Pöschel <franz.poeschel@gmail.com>
2022-03-07 16:24:16 +0100

    Phase space plugin

--- include/picongpu/plugins/PhaseSpace/PhaseSpace.hpp
+++ include/picongpu/plugins/PhaseSpace/PhaseSpace.hpp
@@ -97,10 +97,24 @@ namespace picongpu
              * output with old outpu
              */
             plugins::multi::Option<std::string> file_name_extension
-                = {"ext",
-                   "openPMD filename extension (this controls the"
-                   "backend picked by the openPMD API)",
-                   "h5"};
+                = { "ext",
+                    "openPMD filename extension (this controls the"
+                    "backend picked by the openPMD API)",
+#if openPMD_HAVE_HDF5
+                    "h5"
+#elif openPMD_HAVE_ADIOS2
+                    "bp"
+#else
+                    /*
+                     * This branch should never be activated because CMake will
+                     * not enable the openPMD plugin in that case anyway.
+                     */
+                    static_assert(
+                        false,
+                        "openPMD-api has neither ADIOS2 or HDF5 backend available. Use CMake to deactivate the "
+                        "openPMD plugin.")
+#endif
+                  };
 
             plugins::multi::Option<std::string> json_config
                 = {"json", "advanced (backend) configuration for openPMD in JSON format", "{}"};
