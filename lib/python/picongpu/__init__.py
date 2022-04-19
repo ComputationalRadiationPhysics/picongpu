759bc7ac6e95970dff506662f8f53f263a114ec4
Hannes T <80697868+s9105947@users.noreply.github.com>
2022-04-19 14:15:01 +0000

    move python modules into picongpu.extra submodule
    Discussion in #4032
    The following commits will introduce the python submodules "picmi" and
    "pypicongpu", which are NOT related to the existing modules. Their
    naming however may indicate such a connection ("utils" for generic
    python-internal utilities, "plugins" to control PIConGPU plugins etc.)
    which does NOT exist. To make clear that these python modules provide
    auxiliary functionality they are moved into the submodule picongpu.extra

---
+++ lib/python/picongpu/__init__.py
@@ -0,0 +1,9 @@
+"""
+PIConGPU python modules
+
+Contains tools to both to directly interact with PIConGPU and other auxiliary
+tools.
+
+Note that with the PICMI integration the previously existing modules have been
+moved into the "extra" submodule to keep the naming schemes consistent.
+"""
---
+++ lib/python/picongpu/extra/__init__.py
@@ -0,0 +1,6 @@
+"""
+auxiliary tools not directly related to PIConGPU execution
+
+Note: Modules have been moved here to avoid confusion about their relationship
+to PICMI i.e. the simulation at run time.
+"""
