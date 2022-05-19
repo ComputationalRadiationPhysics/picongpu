0eacc197276869aa8ad30f2d1c091e5a3f77cb8d
René Widera <r.widera@hzdr.de>
2022-05-19 11:32:37 +0200

    CI: re-enable ISAAC
    ISAAC tests was disabled with #4081 because of compatibility issues.

--- share/ci/install/isaac.sh
+++ share/ci/install/isaac.sh
@@ -7,21 +7,20 @@ set -o pipefail
 
 cd $CI_PROJECT_DIR
 
-export GLM_ROOT=/opt/glm/0.9.9.8
-export CMAKE_PREFIX_PATH=$GLM_ROOT/cmake/glm:$CMAKE_PREFIX_PATH
-mkdir -p $GLM_ROOT
-cd $GLM_ROOT
-git clone --branch 0.9.9.8 https://github.com/g-truc/glm.git .
-
-# patch glm to support CUDA compile with clang
-sed -i 's/inline/GLM_FUNC_DECL/g' $GLM_ROOT/glm/gtc/type_ptr.inl
+export GLM_ROOT=/opt/glm/0.9.9.9-dev
+export CMAKE_PREFIX_PATH=$GLM_ROOT:$CMAKE_PREFIX_PATH
+git clone https://github.com/g-truc/glm.git
+cd glm
+git checkout 6ad79aae3eb5bf809c30bf1168171e9e55857e45
+mkdir build
+cd build
+cmake ../ -DCMAKE_INSTALL_PREFIX=$GLM_ROOT -DGLM_TEST_ENABLE=OFF
+make install
 
 cd $CI_PROJECT_DIR
 git clone https://github.com/ComputationalRadiationPhysics/isaac.git
 cd isaac
-# ISAAC version with new LIC kernel https://github.com/ComputationalRadiationPhysics/isaac/pull/143
-# and moving window fix https://github.com/ComputationalRadiationPhysics/isaac/pull/148
-git checkout b8c66ba851e9dfcc48e7bc1ab677828e3adbd22f
+git checkout 195420ca1c43f6148c7f6c3a10ad9cad32e04d6a
 mkdir build_isaac
 cd build_isaac
 cmake ../lib/ -DCMAKE_INSTALL_PREFIX=$ISAAC_ROOT
