1556888c349d16dc97f86b657a575547f4d7f5e1
Sergei Bastrakov <sergey.bastrakov@gmail.com>
2022-08-09 13:09:07 +0000

    Refactor particle removal in absorbing boundary into a separate function
    To prepare for weight correction to be done as absorbing boundary application.

--- include/picongpu/particles/boundary/Absorbing.hpp
+++ include/picongpu/particles/boundary/Absorbing.hpp
@@ -37,25 +37,52 @@ namespace picongpu
     {
         namespace boundary
         {
-            //! Functor to be applied to all particles in the active area
-            struct AbsorbParticleIfOutside : public functor::misc::Parametrized<Parameters>
+            namespace detail
             {
-                //! Name, required to be wrapped later
-                static constexpr char const* name = "absorbParticleIfOutside";
-
-                /** Process the current particle located in the given cell
-                 *
-                 * @param offsetToTotalOrigin offset of particle cell in the total domain
-                 * @param particle handle of particle to process (can be used to change attribute values)
-                 */
-                template<typename T_Particle>
-                HDINLINE void operator()(DataSpace<simDim> const& offsetToTotalOrigin, T_Particle& particle)
+                //! Functor to be applied to all particles in the active area
+                struct AbsorbParticleIfOutside : public functor::misc::Parametrized<Parameters>
                 {
-                    if((offsetToTotalOrigin[m_parameters.axis] < m_parameters.beginInternalCellsTotal)
-                       || (offsetToTotalOrigin[m_parameters.axis] >= m_parameters.endInternalCellsTotal))
-                        particle[multiMask_] = 0;
+                    //! Name, required to be wrapped later
+                    static constexpr char const* name = "absorbParticleIfOutside";
+
+                    /** Process the current particle located in the given cell
+                     *
+                     * @param offsetToTotalOrigin offset of particle cell in the total domain
+                     * @param particle handle of particle to process (can be used to change attribute values)
+                     */
+                    template<typename T_Particle>
+                    HDINLINE void operator()(DataSpace<simDim> const& offsetToTotalOrigin, T_Particle& particle)
+                    {
+                        if((offsetToTotalOrigin[m_parameters.axis] < m_parameters.beginInternalCellsTotal)
+                           || (offsetToTotalOrigin[m_parameters.axis] >= m_parameters.endInternalCellsTotal))
+                            particle[multiMask_] = 0;
+                    }
+                };
+
+                /** Remove particles of the given species that are outer wrt the given boundary
+                 *
+                 * @tparam T_Species particle species type
+                 *
+                 * @param species particle species
+                 * @param exchangeType exchange describing the active boundary
+                 * @param currentStep current time iteration
+                 */
+                template<typename T_Species>
+                HINLINE void removeOuterParticles(T_Species& species, uint32_t exchangeType, uint32_t currentStep)
+                {
+                    pmacc::DataSpace<simDim> beginInternalCellsTotal, endInternalCellsTotal;
+                    getInternalCellsTotal(species, exchangeType, &beginInternalCellsTotal, &endInternalCellsTotal);
+                    auto const axis = pmacc::boundary::getAxis(exchangeType);
+                    AbsorbParticleIfOutside::parameters().axis = axis;
+                    AbsorbParticleIfOutside::parameters().beginInternalCellsTotal = beginInternalCellsTotal[axis];
+                    AbsorbParticleIfOutside::parameters().endInternalCellsTotal = endInternalCellsTotal[axis];
+                    auto const mapperFactory = getMapperFactory(species, exchangeType);
+                    using Manipulator = manipulators::unary::FreeTotalCellOffset<AbsorbParticleIfOutside>;
+                    particles::manipulate<Manipulator, T_Species>(currentStep, mapperFactory);
+                    species.fillGaps(mapperFactory);
                 }
-            };
+
+            } // namespace detail
 
             //! Functor to apply absorbing boundary to particle species
             template<>
@@ -72,16 +99,7 @@ namespace picongpu
                 template<typename T_Species>
                 void operator()(T_Species& species, uint32_t exchangeType, uint32_t currentStep)
                 {
-                    pmacc::DataSpace<simDim> beginInternalCellsTotal, endInternalCellsTotal;
-                    getInternalCellsTotal(species, exchangeType, &beginInternalCellsTotal, &endInternalCellsTotal);
-                    auto const axis = pmacc::boundary::getAxis(exchangeType);
-                    AbsorbParticleIfOutside::parameters().axis = axis;
-                    AbsorbParticleIfOutside::parameters().beginInternalCellsTotal = beginInternalCellsTotal[axis];
-                    AbsorbParticleIfOutside::parameters().endInternalCellsTotal = endInternalCellsTotal[axis];
-                    auto const mapperFactory = getMapperFactory(species, exchangeType);
-                    using Manipulator = manipulators::unary::FreeTotalCellOffset<AbsorbParticleIfOutside>;
-                    particles::manipulate<Manipulator, T_Species>(currentStep, mapperFactory);
-                    species.fillGaps(mapperFactory);
+                    detail::removeOuterParticles(species, exchangeType, currentStep);
                 }
             };
 
