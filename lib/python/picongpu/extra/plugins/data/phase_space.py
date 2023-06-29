Fix units of phase space plugin and in documentation

lib/python/picongpu/extra/plugins/data/phase_space.py
@@ -224,7 +224,7 @@ class PhaseSpaceData(DataReader):
             ps_data = mesh[io.Mesh_Record_Component.SCALAR]
 
             # all in SI
-            dV = mesh.get_attribute('dV') * mesh.get_attribute('dr')**3
+            dV = mesh.get_attribute('dV') * mesh.get_attribute('dr_unit')**3
             unitSI = mesh.get_attribute('sim_unit')
             p_range = mesh.get_attribute('p_unit') * \
                 np.array(