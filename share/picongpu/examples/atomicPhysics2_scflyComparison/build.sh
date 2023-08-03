6a666ae0eedccd1be9cb23e5160651b64796f2b7
BrianMarre <b.marre@hzdr.de>
2023-08-03 14:28:30 +0200

    update example SCFly comparison

---
+++ share/picongpu/examples/atomicPhysics2_scflyComparison/build.sh
@@ -0,0 +1,1 @@
+pic-build 2>&1 | tee compile.result
--- share/picongpu/examples/atomicPhysics2_scflyComparison/quick_execute.sh
+++ share/picongpu/examples/atomicPhysics2_scflyComparison/quick_execute.sh
@@ -1,3 +1,1 @@
-cd bin
-./picongpu -g 1 1 1 -d 1 1 1 --periodic 1 1 1 -s 2 2>&1 | tee ../output.result
-cd ..
+./bin/picongpu -g 1 1 1 -d 1 1 1 --periodic 1 1 1 -s 2 2>&1 | tee ../output.result
