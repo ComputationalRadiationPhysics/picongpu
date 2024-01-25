94497efd00cd6e0c0d8a717ab4a687fe519ab219
René Widera <r.widera@hzdr.de>
2024-01-25 11:03:02 +0100

    install openPMD 0.15.0 if not avaiable
    Add recipe to install openMPD-api 0.15.0 if not shipped within the
    container.

---
+++ share/ci/install/openPMD.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+cd $CI_PROJECT_DIR
+
+export openPMD_VERSION=0.15.0
+
+if ! agc-manager -e openPMD-api@${openPMD_VERSION}; then
+    export OPENPMD_ROOT=/opt/openPMD-api/$openPMD_VERSION
+
+    export CMAKE_PREFIX_PATH=$GLM_ROOT:$CMAKE_PREFIX_PATH
+    git clone https://github.com/openPMD/openPMD-api.git --depth 1 --branch $openPMD_VERSION
+    mkdir buildOpenPMD
+    cd buildOpenPMD
+    cmake ../openPMD-api -DCMAKE_CXX_STANDARD=17 -DopenPMD_BUILD_CLI_TOOLS=OFF -DCMAKE_INSTALL_PREFIX=$OPENPMD_ROOT -DopenPMD_BUILD_EXAMPLES=OFF -DopenPMD_BUILD_TESTING=OFF -DopenPMD_USE_PYTHON=OFF -DopenPMD_USE_ADIOS2=ON
+    make install
+    cd ..
+
+    export CMAKE_PREFIX_PATH=$OPENPMD_ROOT:$CMAKE_PREFIX_PATH
+    export LD_LIBRARY_PATH=$OPENPMD_ROOT/lib:$LD_LIBRARY_PATH
+else
+    export OPENPMD_ROOT="$(agc-manager -b openPMD-api@${openPMD_VERSION})"
+    export CMAKE_PREFIX_PATH=$OPENPMD_ROOT:$CMAKE_PREFIX_PATH
+    export LD_LIBRARY_PATH=$OPENPMD_ROOT/lib:$LD_LIBRARY_PATH
+fi
