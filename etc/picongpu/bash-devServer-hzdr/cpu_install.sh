ec4375477d5b33c064b7cef85a4695f677010243
René Widera <r.widera@hzdr.de>
2024-01-26 14:49:00 +0100

    update system profiles
    - update and tested hemera and HZDR dev system profiles
    - update in all other profiles with openmpi-api modules the version to
      0.15.2

--- etc/picongpu/bash-devServer-hzdr/cpu_install.sh
+++ etc/picongpu/bash-devServer-hzdr/cpu_install.sh
@@ -1,10 +1,10 @@
-spack install --reuse cmake@3.25.0 %gcc@12.2.0
-spack load cmake@3.25.0 ^openssl certs=mozilla %gcc@12.2.0
+spack install --reuse cmake@3.26.3 %gcc@12.1.0
+spack load cmake@3.26.3 ^openssl certs=mozilla %gcc@12.1.0
 
 echo "openpmd-api:"
-spack install --reuse openpmd-api@0.14.5 %gcc@12.2.0 \
-    ^adios2@2.8.3 \
-    ^cmake@3.25.0 \
+spack install --reuse openpmd-api@0.15.2 %gcc@12.1.0 \
+    ^adios2@2.9.2 \
+    ^cmake@3.26.3 \
     ^hdf5@1.12.2 \
     ^openmpi@4.1.4 +atomics\
     ^python@3.10.4 \
@@ -15,7 +15,7 @@ spack install --reuse boost@1.80.0 \
     +program_options \
     +atomic \
     ~python \
-    %gcc@12.2.0
+    %gcc@12.1.0
 
 echo "pngwriter"
-spack install --reuse pngwriter@0.7.0 %gcc@12.2.0
+spack install --reuse pngwriter@0.7.0 %gcc@12.1.0
--- etc/picongpu/bash-devServer-hzdr/gpu_v100_install.sh
+++ etc/picongpu/bash-devServer-hzdr/gpu_v100_install.sh
@@ -1,10 +1,10 @@
-spack install --reuse cmake@3.24.2 %gcc@10.2.0
-spack load cmake@3.24.2 %gcc@10.2.0
+spack install --reuse cmake@3.26.3 %gcc@12.2.0
+spack load cmake@3.26.3 %gcc@12.2.0
 
 echo "openpmd-api:"
-spack install --reuse openpmd-api@0.14.5 %gcc@10.2.0 \
-    ^adios2@2.8.3 +cuda cuda_arch=70\
-    ^cmake@3.24.2 \
+spack install --reuse openpmd-api@0.15.2 %gcc@12.2.0 \
+    ^adios2@2.9.2 +cuda cuda_arch=70\
+    ^cmake@3.26.3 \
     ^hdf5@1.12.2 \
     ^openmpi@4.1.3 +atomics +cuda cuda_arch=70\
     ^python@3.10.4 \
@@ -24,7 +24,7 @@ spack install --reuse boost@1.78.0 \
     +atomic \
     +date_time \
     ~python \
-    %gcc@10.2.0
+    %gcc@12.2.0
 
 echo "pngwriter"
-spack install --reuse pngwriter@0.7.0 %gcc@10.2.0
+spack install --reuse pngwriter@0.7.0 %gcc@12.2.0
