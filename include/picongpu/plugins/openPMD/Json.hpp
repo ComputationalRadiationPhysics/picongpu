bca06817be86b16a433847e6196facdd469ba54d
Franz Pöschel <franz.poeschel@gmail.com>
2024-07-10 17:15:03 +0200

    Remove JSON Matcher

--- include/picongpu/plugins/openPMD/Json.hpp
+++ include/picongpu/plugins/openPMD/Json.hpp
@@ -19,59 +19,11 @@
 
 #pragma once
 
-#include <memory> // std::unique_ptr
 #include <string>
 
 #include <mpi.h>
 
-namespace picongpu
+namespace picongpu::openPMD
 {
-    namespace json
-    {
-        /**
-         * @brief Class to handle extended JSON configurations as used by
-         *        the openPMD plugin.
-         *
-         * This class handles parsing of the extended JSON patterns as well as
-         * selection of one JSON configuration by regex.
-         *
-         */
-        class AbstractJsonMatcher
-        {
-        public:
-            /**
-             * @brief Construct a JSON matcher to hand out dataset-specific configurations
-             *
-             * This function will parse the given config, after reading it
-             * from a file if needed. In this case, the constructor is
-             * MPI-collective.
-             * It will distinguish per backend between ordinary openPMD JSON configurations
-             * and extended configurations as defined by PIConGPU.
-             * If an ordinary JSON configuration was detected, given regex
-             * patterns will be matched against "" (the empty string).
-             *
-             * @param config The JSON configuration, exactly as in --openPMD.backendConfig.
-             * @param comm MPI communicator for collective file reading, if needed.
-             * @return std::unique_ptr<AbstractJsonMatcher>
-             */
-            static std::unique_ptr<AbstractJsonMatcher> construct(std::string const& config, MPI_Comm comm);
-
-            virtual ~AbstractJsonMatcher() = default;
-
-            /**
-             * @brief Get the JSON config associated with a regex pattern.
-             *
-             * @param datasetPath The regex.
-             * @return The matched JSON configuration, as a string.
-             */
-            virtual std::string get(std::string const& datasetPath) const = 0;
-
-            /**
-             * @brief Get the default JSON config.
-             *
-             * @return The default JSON configuration, as a string.
-             */
-            virtual std::string getDefault() const = 0;
-        };
-    } // namespace json
-} // namespace picongpu
+    auto resolveJsonConfig(std::string const& commandLineValue, MPI_Comm) -> std::string;
+} // namespace picongpu::openPMD
--- include/picongpu/plugins/openPMD/openPMDWriter.x.cpp
+++ include/picongpu/plugins/openPMD/openPMDWriter.x.cpp
@@ -120,12 +120,10 @@ namespace picongpu
         ::openPMD::RecordComponent& ThreadParams::initDataset(
             ::openPMD::RecordComponent& recordComponent,
             ::openPMD::Datatype datatype,
-            pmacc::math::UInt64<DIM> const& globalDimensions,
-            std::string const& datasetName)
+            pmacc::math::UInt64<DIM> const& globalDimensions)
         {
             std::vector<uint64_t> v = asStandardVector(globalDimensions);
             ::openPMD::Dataset dataset{datatype, std::move(v)};
-            dataset.options = jsonMatcher->get(datasetName);
             recordComponent.resetDataset(std::move(dataset));
             return recordComponent;
         }
@@ -162,7 +160,7 @@ namespace picongpu
                      * backend_config. The reading routines (for restarting from a checkpoint) are configured via
                      * --checkpoint.openPMD.jsonRestart.
                      */
-                    at == ::openPMD::Access::READ_ONLY ? backendConfigRestartString : jsonMatcher->getDefault());
+                    at == ::openPMD::Access::READ_ONLY ? backendConfigRestartString : jsonConfig.value());
                 if(openPMDSeries->backend() == "MPI_ADIOS1")
                 {
                     throw std::runtime_error(R"END(
@@ -654,14 +652,14 @@ make sure that environment variable OPENPMD_BP_BACKEND is not set to ADIOS1.
             {
                 throw std::runtime_error("Command line option openPMD.json was renamed as openPMD.backendConfig.");
             }
-            if(!jsonMatcher)
+            if(!jsonConfig.has_value())
             {
                 // avoid deadlock between not finished pmacc tasks and mpi blocking collectives
                 eventSystem::getTransactionEvent().waitForFinished();
-                jsonMatcher = AbstractJsonMatcher::construct(backendConfigString, communicator);
+                jsonConfig.emplace(resolveJsonConfig(backendConfigString, communicator));
             }
 
-            log<picLog::INPUT_OUTPUT>("openPMD: global JSON output config: %1%") % jsonMatcher->getDefault();
+            log<picLog::INPUT_OUTPUT>("openPMD: global JSON config: %1%") % *jsonConfig;
             if(!help.backendConfigRestartForbidden.empty())
             {
                 throw std::runtime_error(
@@ -1014,8 +1012,6 @@ make sure that environment variable OPENPMD_BP_BACKEND is not set to ADIOS1.
                 auto const timeOffset = 0.0_X;
                 writeFieldAttributes(params, currentStep, unitDimension, timeOffset, mesh);
 
-                std::string datasetName = params->openPMDSeries->meshesPath() + name;
-
                 auto numRNGsPerSuperCell = DataSpace<simDim>::create(1);
                 numRNGsPerSuperCell.x() = numFrameSlots;
                 // rng states are always of the domain size therefore query sizes from domain information
@@ -1041,8 +1037,7 @@ make sure that environment variable OPENPMD_BP_BACKEND is not set to ADIOS1.
                 params->initDataset<simDim>(
                     mesh,
                     ::openPMD::determineDatatype<ReinterpretedType>(),
-                    recordGlobalSizeDims,
-                    datasetName);
+                    recordGlobalSizeDims);
 
                 // define record component level attributes
                 auto const inCellPosition = std::vector<float_X>(simDim, 0.0_X);
@@ -1816,11 +1811,8 @@ make sure that environment variable OPENPMD_BP_BACKEND is not set to ADIOS1.
                             return mesh;
                         }
                     }();
-                    std::string datasetName
-                        = nComponents > 1 ? params->openPMDSeries->meshesPath() + name + "/" + name_lookup_tpl[d]
-                                          : params->openPMDSeries->meshesPath() + name;
 
-                    params->initDataset<simDim>(mrc, openPMDType, recordGlobalSizeDims, datasetName);
+                    params->initDataset<simDim>(mrc, openPMDType, recordGlobalSizeDims);
 
                     // define record component level attributes
                     mrc.setPosition(inCellPosition.at(d));
