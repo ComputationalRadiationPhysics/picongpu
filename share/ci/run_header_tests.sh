2caf7f5fc1b0d92b8f9e64b23e0c8b6732588f99
René Widera <r.widera@hzdr.de>
2024-09-11 16:18:40 +0200

    CI: add PIConGPU header check
    - rename `test/picHeaderCheck` to `est/picongpuHeaderCheck`
    - add ci tests downstream pipe for header checks
    
    ci: picongpu

--- share/ci/run_header_tests.sh
+++ share/ci/run_header_tests.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# $1 - is the name of the subproject to test [pmacc, picongpu]
+
 source $CI_PROJECT_DIR/share/ci/pmacc_env.sh
 
 # compile header include consistency check
 # use one build directory for all build configurations
 cd $HOME
-mkdir buildPMaccHeaderCI
-cd buildPMaccHeaderCI
-cmake $CMAKE_ARGS $code_DIR/test/pmaccHeaderCheck
+mkdir build_${1}_headerCI
+cd build_${1}_headerCI
+cmake $CMAKE_ARGS $code_DIR/test/${1}HeaderCheck
 make -j $(nproc)
