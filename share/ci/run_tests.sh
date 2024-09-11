dbac8c4cf9cc1d5f9f1eea12725c9a526ac8e68d
René Widera <r.widera@hzdr.de>
2024-09-11 16:18:40 +0200

    CI: add PIConGPU header check
    - rename `test/picHeaderCheck` to `est/picongpuHeaderCheck`
    - add ci tests downstream pipe for header checks
    
    ci: picongpu

--- share/ci/run_tests.sh
+++ share/ci/run_tests.sh
@@ -6,7 +6,9 @@ set -o pipefail
 if [ "$1" == "pmacc" ] ; then
     $CI_PROJECT_DIR/share/ci/run_pmacc_tests.sh
 elif [ "$1" == "pmacc_header" ] ; then
-    $CI_PROJECT_DIR/share/ci/run_pmacc_header_tests.sh
+    $CI_PROJECT_DIR/share/ci/run_header_tests.sh "pmacc"
+elif [ "$1" == "picongpu_header" ] ; then
+        $CI_PROJECT_DIR/share/ci/run_header_tests.sh "picongpu"
 elif [ "$1" == "unit" ] ; then
     $CI_PROJECT_DIR/share/ci/run_picongpu_unit_tests.sh
 else
