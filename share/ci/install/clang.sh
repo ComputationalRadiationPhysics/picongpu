c7fbd77b86aded374ed45ca7ad70b88bc18410d2
René Widera <r.widera@hzdr.de>
2024-09-16 14:43:37 +0200

    CI: update compile matrix
    - add tests for
      - HIP 56 - 6.1
      - CUDA 12.2 - 12.5
      - boost 1.79 - 1.85
    - update filter rules
    
    ci: full-compile

--- share/ci/install/clang.sh
+++ share/ci/install/clang.sh
@@ -7,6 +7,13 @@ set -o pipefail
 
 cd $CI_PROJECT_DIR
 
+# provide sources for clang versions missing in the containers version 3.2
+echo -e "deb http://apt.llvm.org/focal/ llvm-toolchain-focal-16 main\ndeb-src http://apt.llvm.org/focal/ llvm-toolchain-focal-16 main" >> /etc/apt/sources.list.d/llvm.list
+echo -e "deb http://apt.llvm.org/focal/ llvm-toolchain-focal-17 main\ndeb-src http://apt.llvm.org/focal/ llvm-toolchain-focal-17 main" >> /etc/apt/sources.list.d/llvm.list
+echo -e "deb http://apt.llvm.org/focal/ llvm-toolchain-focal-18 main\ndeb-src http://apt.llvm.org/focal/ llvm-toolchain-focal-18 main" >> /etc/apt/sources.list.d/llvm.list
+
+apt -y update
+
 clang_version=$(echo $CXX_VERSION | tr -d "clang++-")
 echo "Clang-version: $clang_version"
 
