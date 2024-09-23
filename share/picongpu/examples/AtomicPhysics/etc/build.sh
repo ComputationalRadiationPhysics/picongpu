c7ad9f0cf9ea3de99787c6b26658dcf6262eef5f
BrianMarre <b.marre@hzdr.de>
2024-09-23 18:09:49 +0200

    update atomicPhysics example README

---
+++ share/picongpu/examples/AtomicPhysics/etc/build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+# Copyright 2024 Brian Marre
+#
+# This file is part of PIConGPU.
+#
+# PIConGPU is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PIConGPU is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PIConGPU.
+# If not, see <http://www.gnu.org/licenses/>.
+
+pic-build -c "-DPARAM_FORCE_CONSTANT_ELECTRON_TEMPERATURE"
