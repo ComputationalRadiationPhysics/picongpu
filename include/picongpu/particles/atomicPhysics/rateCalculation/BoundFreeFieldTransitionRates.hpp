f24e4756a1b68f5830e79386c5ec822379ba2994
BrianMarre <b.marre@hzdr.de>
2024-10-08 20:32:34 +0200

    fix maximum ADK rate estimation
    AtomicPhysics(FLYonPIC) mistakenly assumed that the highes ADK rate is
    reached with for the highest EField.
    
    ci: picongpu

--- include/picongpu/particles/atomicPhysics/rateCalculation/BoundFreeFieldTransitionRates.hpp
+++ include/picongpu/particles/atomicPhysics/rateCalculation/BoundFreeFieldTransitionRates.hpp
@@ -39,6 +39,7 @@ namespace picongpu::particles::atomicPhysics::rateCalculation
     template<atomicPhysics::enums::ADKLaserPolarization T_ADKLaserPolarization>
     struct BoundFreeFieldTransitionRates
     {
+    private:
         /** get effective principal quantum number
          *
          * @param ionizationEnergy, in Hartree
@@ -48,17 +49,17 @@ namespace picongpu::particles::atomicPhysics::rateCalculation
          */
         HDINLINE static float_X effectivePrincipalQuantumNumber(
             float_X const screenedCharge,
-            float_x const ionizationEnergy)
+            float_X const ionizationEnergy)
         {
             return screenedCharge / math::sqrt(2._X * ionizationEnergy);
         }
 
-        /** get screened charge for ionization
+        /** get screened charge for ionization electron
          *
          * @return unit: e
          */
         template<typename T_ChargeStateDataBox, typename T_AtomicStateDataBox, typename T_BoundFreeTransitionDataBox>
-        HDINLINE static float_X const screenedCharge(
+        HDINLINE static float_X screenedCharge(
             uint32_t const transitionCollectionIndex,
             T_ChargeStateDataBox const chargeStateDataBox,
             T_AtomicStateDataBox const atomicStateDataBox,
@@ -74,7 +75,42 @@ namespace picongpu::particles::atomicPhysics::rateCalculation
             return chargeStateDataBox.screenedCharge(lowerStateChargeState) - 1._X;
         }
 
+        /** actual rate rateFormula
+         *
+         * @param Z screenedCharge for ionization electron, e
+         * @param nEff effective principal quantum number, unitless
+         * @param eFieldNorm_AU norm of the E-Field strength, in sim.atomicUnit.eField()
+         */
+        HDINLINE static float_X rateFormula(float_X const Z, float_X const nEff, float_X const eFieldNorm_AU)
+        {
+            float_X const nEffCubed = pmacc::math::cPow(nEff, 3u);
+            float_X const ZCubed = pmacc::math::cPow(Z, 3u);
 
+            float_X const dBase = 4.0_X * math::exp(1._X) * ZCubed / (eFieldNorm_AU * pmacc::math::cPow(nEff, 4u));
+            float_X const dFromADK = math::pow(dBase, nEff);
+
+            constexpr float_X pi = pmacc::math::Pi<float_X>::value;
+
+            // 1/sim.atomicUnit.time()
+            float_X rateADK_AU = eFieldNorm_AU * pmacc::math::cPow(dFromADK, 2u) / (8._X * pi * Z)
+                * math::exp(-2._X * ZCubed / (3._X * nEffCubed * eFieldNorm_AU));
+
+            // factor from averaging over one laser cycle with LINEAR polarization
+            if constexpr(
+                u32(T_ADKLaserPolarization) == u32(atomicPhysics::enums::ADKLaserPolarization::linearPolarization))
+                rateADK_AU *= math::sqrt(3._X * nEffCubed * eFieldNorm_AU / (pi * ZCubed));
+
+            /* A * 1/sim.atomicUnit.time() = A * 1/sim.atomicUnit.time() * sim.unit.time() / sim.unit.time()
+             *   = A * [sim.unit.time()/sim.atomicUnit.time()] * 1/sim.unit.time()
+             *   = (A * timeConversion) * 1/sim.unit.time()
+             *   = B * 1/sim.unit.time() */
+            constexpr float_X timeConversion = picongpu::sim.unit.time() / picongpu::sim.atomicUnit.time();
+
+            // 1/ sim.unit.time()
+            return rateADK_AU * timeConversion;
+        }
+
+    public:
         /** field ionization ADK rate for a given electric field strength
          *
          * @tparam T_ChargeStateDataBox instantiated type of dataBox
@@ -126,31 +162,79 @@ namespace picongpu::particles::atomicPhysics::rateCalculation
             // sim.atomicUnit.eField()
             float_X const eFieldNorm_AU = sim.pic.conv().eField2auEField(eFieldNorm);
 
-            float_X const ZCubed = pmacc::math::cPow(Z, 3u);
+            return rateFormula(Z, nEff, eFieldNorm_AU);
+        }
 
-            float_X const dBase = 4.0_X * math::exp(1._X) * ZCubed / (eFieldNorm_AU * pmacc::math::cPow(nEff, 4u));
-            float_X const dFromADK = math::pow(dBase, nEff);
+        /** get maximum field ionization ADK rate for electric field strengths inside the given boundaries
+         *
+         * @tparam T_ChargeStateDataBox instantiated type of dataBox
+         * @tparam T_AtomicStateDataBox instantiated type of dataBox
+         * @tparam T_BoundFreeTransitionDataBox instantiated type of dataBox
+         *
+         * @param maxEFieldNorm maximum E-field vector norm, in sim.units.eField()
+         * @param minEFieldNorm minimum E-field vector norm, in sim.units.eField()
+         * @param ionizationPotentialDepression, in eV
+         * @param transitionCollectionIndex index of transition in boundBoundTransitionDataBox
+         * @param atomicStateDataBox access to atomic state property data
+         * @param boundBoundTransitionDataBox access to bound-bound transition data
+         *
+         * @return unit: 1/picongpu::sim.unit.time()
+         */
+        template<
+            typename T_EFieldType,
+            typename T_ChargeStateDataBox,
+            typename T_AtomicStateDataBox,
+            typename T_BoundFreeTransitionDataBox>
+        HDINLINE static float_X maximumRateADKFieldIonization(
+            T_EFieldType const maxEFieldNorm,
+            T_EFieldType const minEFieldNorm,
+            float_X const ionizationPotentialDepression,
+            uint32_t const transitionCollectionIndex,
+            T_ChargeStateDataBox const chargeStateDataBox,
+            T_AtomicStateDataBox const atomicStateDataBox,
+            T_BoundFreeTransitionDataBox const boundFreeTransitionDataBox)
+        {
+            if(maxEFieldNorm == 0._X)
+                return 0._X;
 
-            constexpr float_X pi = pmacc::math::Pi<float_X>::value;
-            float_X const nEffCubed = pmacc::math::cPow(nEff, 3u);
+            float_X const Z = screenedCharge(
+                transitionCollectionIndex,
+                chargeStateDataBox,
+                atomicStateDataBox,
+                boundFreeTransitionDataBox);
 
-            // 1/sim.atomicUnit.time()
-            float_X rateADK_AU = eFieldNorm_AU * pmacc::math::cPow(dFromADK, 2u) / (8._X * pi * Z)
-                * math::exp(-2._X * ZCubed / (3._X * nEffCubed * eFieldNorm_AU));
+            // Hartree
+            float_X const ionizationEnergy = picongpu::sim.si.conv().eV2auEnergy(DeltaEnergyTransition::get(
+                transitionCollectionIndex,
+                atomicStateDataBox,
+                boundFreeTransitionDataBox,
+                ionizationPotentialDepression,
+                chargeStateDataBox));
 
-            // factor from averaging over one laser cycle with LINEAR polarization
-            if constexpr(
-                u32(T_ADKLaserPolarization) == u32(atomicPhysics::enums::ADKLaserPolarization::linearPolarization))
-                rateADK_AU *= math::sqrt(3._X * nEffCubed * eFieldNorm_AU / (pi * ZCubed));
+            float_X const nEff = effectivePrincipalQuantumNumber(Z, ionizationEnergy);
+            float_X const nEffCubed = pmacc::math::cPow(nEff, u32(3u));
 
-            /* A * 1/sim.atomicUnit.time() = A * 1/sim.atomicUnit.time() * sim.unit.time() / sim.unit.time()
-             *   = A * [sim.unit.time()/sim.atomicUnit.time()] * 1/sim.unit.time()
-             *   = (A * timeConversion) * 1/sim.unit.time()
-             *   = B * 1/sim.unit.time() */
-            constexpr float_X timeConversion = picongpu::sim.unit.time() / picongpu::sim.atomicUnit.time();
+            float_X const maxEField_AU = picongpu::sim.pic.conv().eField2auEField(maxEFieldNorm);
+            float_X const minEField_AU = picongpu::sim.pic.conv().eField2auEField(minEFieldNorm);
 
-            // 1/ sim.unit.time()
-            return rateADK_AU * timeConversion;
+            // theoretical maximum ADK Rate, in sim.atomicUnit.eField()
+            float_X const F_max = 4._X * Z / (3 * nEffCubed * (4._X * nEff - 3._X));
+
+            // fieldStrength for maximum Rate, in sim.atomicUnit.eField(), see Notebook 2024 P.43-48
+            float_X F;
+            if(nEff <= 0.75_X || F_max > maxEField_AU)
+            {
+                F = maxEField_AU;
+            }
+            else
+            {
+                if(F_max > minEField_AU)
+                    F = F_max;
+                else
+                    F = minEField_AU;
+            }
+
+            return rateFormula(Z, nEff, F);
         }
     };
 } // namespace picongpu::particles::atomicPhysics::rateCalculation
