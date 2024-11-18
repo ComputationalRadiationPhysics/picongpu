0d466cbd484ce3ae636eb912190b85fe19faaa3e
Franz Pöschel <franz.poeschel@gmail.com>
2024-11-18 11:05:21 +0100

    hmm

--- include/picongpu/plugins/openPMD/openPMDWriter.x.cpp
+++ include/picongpu/plugins/openPMD/openPMDWriter.x.cpp
@@ -1825,7 +1825,7 @@ make sure that environment variable OPENPMD_BP_BACKEND is not set to ADIOS1.
                     {
                         // ask openPMD to create a buffer for us
                         // in some backends (ADIOS2), this allows avoiding memcopies
-                        auto span = mrc.storeChunk<ComponentType>(
+                        auto span = mrc.template storeChunk<ComponentType>(
                             asStandardVector(recordOffsetDims),
                             asStandardVector(recordLocalSizeDims),
                             [&fieldBuffer](size_t size)
