35447002658756f6df2dfef4899126e94aa37989
Julian Lenz <j.lenz@hzdr.de>
2025-04-07 11:13:18 +0200

    Make each test individually addressable

--- test/python/picongpu/compiling/compileexamples.py
+++ test/python/picongpu/compiling/compileexamples.py
@@ -19,7 +19,29 @@ EXAMPLES = filter(
 )
 
 
-class TestExamples(unittest.TestCase):
+class TestExamplesMeta(type):
+    def __new__(cls, name, bases, dict):
+        # Generate one test for each example in the examples folder
+        for example in EXAMPLES:
+            name = "test_" + example.name
+            dict[name] = (
+                # This is slightly convoluted:
+                # Python's semantics around variables implement
+                # "sharing" semantics (not even quite reference semantics).
+                # Also, lambdas capture the variable and not the value.
+                # So after the execution of a loop all lambdas refer to
+                # the last value of the loop variable
+                # if they tried to capture it.
+                # So, we need to eagerly evaluate the `example` variable
+                # which we achieve via an immediately evaluated lambda expression.
+                # Please excuse my C++ dialect.
+                lambda example: lambda self: print(example)
+                or self.build_simulation(self.load_example_script(example / "main.py"))
+            )(example)
+        return type.__new__(cls, name, bases, dict)
+
+
+class TestExamples(unittest.TestCase, metaclass=TestExamplesMeta):
     def load_example_script(self, path):
         """load and execute example PICMI script from given path"""
         module_spec = importlib.util.spec_from_file_location("example", path)
@@ -35,9 +57,3 @@ class TestExamples(unittest.TestCase):
         runner = pypicongpu.Runner(sim)
         runner.generate(printDirToConsole=True)
         runner.build()
-
-    def test_example(self):
-        """generate a PIConGPU setup from the laser_wakefield PICMI example and compile the setup"""
-        for example in EXAMPLES:
-            with self.subTest(example=example):
-                self.build_simulation(self.load_example_script(example / "main.py"))
