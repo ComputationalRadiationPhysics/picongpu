d816894997ba83c6bf212cdcae5f88fd717e498d
mafshari64 <mafshari44@gmail.com>
2025-08-29 18:02:36 +0200

    all CI tests ran successfullly with no failures 1

--- lib/python/picongpu/pypicongpu/__init__.py
+++ lib/python/picongpu/pypicongpu/__init__.py
@@ -5,14 +5,12 @@ from .output.energy_histogram import EnergyHistogram
 from .output.macro_particle_count import MacroParticleCount
 from .output.png import Png
 from .output.checkpoint import Checkpoint
-<<<<<<< HEAD
 from .field_solver.DefaultSolver import Solver
 from .field_solver.Yee import YeeSolver
 from .field_solver.Lehe import LeheSolver
-=======
 from .output.openpmd import OpenPMD
 from .output.openpmd_sources.source_base import SourceBase
->>>>>>> 584e3a112 (adding schema for openpmd plugin)
+
 
 from . import laser
 from . import grid
