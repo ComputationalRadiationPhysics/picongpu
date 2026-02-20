3f5684c418ca8f1dfa0248507df6d8ea9dfaee71
Richard Pausch <r.pausch@hzdr.de>
2026-02-20 12:45:27 +0100

    use pi via c++20 numbers (#5632)
    * add pi from c++20/numbers to libPMacc
    
    * use libPMacc pi in PIConGPU
    
    * add inv_pi

--- include/pmacc/algorithms/math/defines/pi.hpp
+++ include/pmacc/algorithms/math/defines/pi.hpp
@@ -24,6 +24,8 @@
 
 #include "pmacc/types.hpp"
 
+#include <numbers>
+
 namespace pmacc
 {
     namespace math
@@ -33,11 +35,11 @@ namespace pmacc
         template<typename T_Type>
         struct Pi
         {
-            static constexpr T_Type value = static_cast<T_Type>(3.141592653589793238462643383279502884197169399);
+            static constexpr T_Type value = std::numbers::pi_v<T_Type>;
             static constexpr T_Type doubleValue = static_cast<T_Type>(2.0) * value;
             static constexpr T_Type halfValue = value / static_cast<T_Type>(2.0);
             static constexpr T_Type quarterValue = value / static_cast<T_Type>(4.0);
-            static constexpr T_Type doubleReciprocalValue = static_cast<T_Type>(2.0) / value;
+            static constexpr T_Type doubleReciprocalValue = static_cast<T_Type>(2.0) * std::numbers::inv_pi_v<T_Type>;
         };
 
     } // namespace math
