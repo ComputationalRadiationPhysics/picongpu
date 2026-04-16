a75cf75dd5a6450b6ad4bef1c6219ac539e915f3
Julian Lenz <j.lenz@hzdr.de>
2026-04-16 10:16:16 +0200

    Add parse_script and generate_script

--- lib/python/picongpu/presets/statements.py
+++ lib/python/picongpu/presets/statements.py
@@ -5,14 +5,16 @@ Authors: Julian Lenz
 License: GPLv3+
 """
 
+from itertools import chain, groupby
+from operator import attrgetter
 from pathlib import Path
 from re import Pattern, compile
-from typing import Literal
+from typing import Iterable, Literal
 
 from pydantic import BaseModel, PrivateAttr
 
 
-def from_bash(cls, statement, regex):
+def from_line(cls, statement, regex):
     return cls(**regex.match(statement).groupdict())
 
 
@@ -23,9 +25,12 @@ class Statement(BaseModel):
     def to_bash(self) -> str:
         return self._format_pattern.format(**self.model_dump(mode="json"))
 
+    def __str__(self) -> str:
+        return self.to_bash()
+
     @classmethod
-    def from_bash(cls, statement):
-        return from_bash(cls, statement, cls._regex.default)
+    def from_line(cls, statement):
+        return from_line(cls, statement, cls._regex.default)
 
 
 class ModuleStatement(Statement):
@@ -85,23 +90,45 @@ class AssignmentStatement(Statement):
         return AssignmentStatement(variable=variable, value=value, mode="literal", export=False).to_bash()
 
     @classmethod
-    def from_bash(cls, statement):
-        tmp = from_bash(cls, statement, cls._regex.default)
+    def from_line(cls, statement):
+        tmp = from_line(cls, statement, cls._regex.default)
         export, variable = _parse_export(tmp.variable)
         mode, value = _parse_mode(tmp.value, variable)
         return AssignmentStatement(export=export, variable=variable, mode=mode, value=value)
 
 
-def make_statement(statement):
+AnyStatement = AssignmentStatement | SourceStatement | LiteralCodeBlock | ModuleStatement
+
+
+def parse_line(statement: str) -> AnyStatement | None:
     statement = statement.split("#", maxsplit=1)[0].strip()
     if statement == "":
         return None
     errors = {}
     for StatementType in [AssignmentStatement, ModuleStatement, SourceStatement, LiteralCodeBlock]:
         try:
-            return StatementType.from_bash(statement)
+            return StatementType.from_line(statement)
         except Exception as error:
             errors[StatementType] = error
     # Not sure why but for some reason, ruff (during pre-commit) doesn't pick up that we're on python>=3.11 already.
     # It complains that ExceptionGroup is not known.
     raise ExceptionGroup(f"Couldn't parse {statement=}.", errors)  # noqa: F821
+
+
+def _merge_literal_code_blocks(statements: Iterable[AnyStatement]) -> Iterable[AnyStatement]:
+    return chain(
+        *(
+            [label(code="\n".join(map(attrgetter("code"), list(group))))]
+            if issubclass(label, LiteralCodeBlock)
+            else list(group)
+            for label, group in groupby(statements, type)
+        )
+    )
+
+
+def parse_script(script: str) -> list[AnyStatement]:
+    return list(_merge_literal_code_blocks(filter(lambda s: s is not None, map(parse_line, script.split("\n")))))
+
+
+def generate_script(statements: Iterable[AnyStatement]) -> str:
+    return "\n".join(map(str, statements))
--- lib/python/test/picongpu/quick/presets/test_statements.py
+++ lib/python/test/picongpu/quick/presets/test_statements.py
@@ -12,12 +12,14 @@ from picongpu.presets.statements import (
     LiteralCodeBlock,
     ModuleStatement,
     SourceStatement,
-    make_statement,
+    parse_line,
+    parse_script,
+    generate_script,
 )
 from pytest import mark
 
 VALID_STATEMENTS = {
-    "module purge": ModuleStatement(engine="module", cmd="purge"),
+    "module purge ": ModuleStatement(engine="module", cmd="purge"),
     "module load gcc/14.1": ModuleStatement(engine="module", cmd="load", package="gcc/14.1"),
     "spack unload gcc/14.1": ModuleStatement(engine="spack", cmd="unload", package="gcc/14.1"),
     'VARIABLE="value"': AssignmentStatement(variable="VARIABLE", value="value"),
@@ -30,35 +32,70 @@ VALID_STATEMENTS = {
 
 
 @mark.parametrize(["bash_representation", "python_representation"], VALID_STATEMENTS.items())
-def test_to_bash(bash_representation, python_representation):
-    assert python_representation.to_bash().strip() == bash_representation
+def test_statement_to_str(bash_representation, python_representation):
+    assert str(python_representation) == bash_representation
 
 
 @mark.parametrize(["bash_representation", "python_representation"], VALID_STATEMENTS.items())
-def test_from_bash(bash_representation, python_representation):
-    assert python_representation == make_statement(bash_representation)
+def test_from_line(bash_representation, python_representation):
+    assert python_representation == parse_line(bash_representation)
 
 
 @mark.parametrize("python_representation", VALID_STATEMENTS.values())
 def test_roundtrip_to_bash(python_representation):
-    assert make_statement(python_representation.to_bash()) == python_representation
+    assert parse_line(str(python_representation)) == python_representation
 
 
 @mark.parametrize("bash_representation", VALID_STATEMENTS.keys())
-def test_roundtrip_from_bash(bash_representation):
-    assert make_statement(bash_representation).to_bash().strip() == bash_representation
+def test_roundtrip_from_line(bash_representation):
+    assert str(parse_line(bash_representation)) == bash_representation
 
 
 def test_blank_line():
-    assert not make_statement("")
+    assert not parse_line("")
 
 
 def test_pure_comment():
-    assert not make_statement("# this is a comment")
+    assert not parse_line("# this is a comment")
 
 
 @mark.parametrize(["bash_representation", "python_representation"], VALID_STATEMENTS.items())
 def test_with_weird_spacing_and_comment(bash_representation, python_representation):
     if not isinstance(python_representation, LiteralCodeBlock):
         bash_representation = bash_representation.replace(" ", " \t  ")
-    assert python_representation == make_statement(f"  {bash_representation} # this is a comment  ")
+    assert python_representation == parse_line(f"  {bash_representation} # this is a comment  ")
+
+
+def test_parse_script():
+    script = "\n".join(VALID_STATEMENTS.keys())
+    statements = list(VALID_STATEMENTS.values())
+    assert parse_script(script) == statements
+
+
+def test_generate_script():
+    assert generate_script(VALID_STATEMENTS.values()) == "\n".join(VALID_STATEMENTS.keys())
+
+
+def test_roundtrip_parse_script():
+    script = "\n".join(VALID_STATEMENTS.keys())
+    assert generate_script(parse_script(script)) == script
+
+
+def test_roundtrip_generate_script():
+    statements = list(VALID_STATEMENTS.values())
+    assert parse_script(generate_script(statements)) == statements
+
+
+def test_parse_script_merges_literal_code_blocks():
+    script = """
+first literal code block
+module purge
+second literal code block starts here
+
+and ends here
+"""
+    assert parse_script(script) == [
+        LiteralCodeBlock(code="first literal code block"),
+        ModuleStatement(engine="module", cmd="purge"),
+        LiteralCodeBlock(code="second literal code block starts here\nand ends here"),
+    ]
