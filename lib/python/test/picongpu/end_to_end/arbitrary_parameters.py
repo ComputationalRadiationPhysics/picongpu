cef8c12cb0208ae91b911057b27f1ff01b2f508d
Julian Lenz <j.lenz@hzdr.de>
2026-04-24 09:41:56 +0200

    Add slurm support to end_to_end tests

--- lib/python/test/picongpu/end_to_end/arbitrary_parameters.py
+++ lib/python/test/picongpu/end_to_end/arbitrary_parameters.py
@@ -10,6 +10,7 @@ from subprocess import run
 from time import sleep
 
 import numpy as np
+from picongpu import rc_params
 from psutil import pid_exists
 
 NUMBER_OF_CELLS = [64, 64, 32]
@@ -36,21 +37,55 @@ def _wait_until(function, sleep_interval=5, timeout_count=TIMEOUT_COUNT):
     raise Exception("Waiting for function did not return after {num_attempts=}.")
 
 
-def _make_wait_function_from(submission_information, submission_system="bash"):
-    # This currently only handles the case of a local process running.
-    # For other submission systems, this would have to
-    # - parse the corresponding information from the submission_information.txt
-    # - and query the corresponding batch system.
-    if submission_system == "bash":
-        with submission_information.open("r") as file:
-            pid = int(file.read())
-        return lambda: not pid_exists(pid)
-    raise NotImplementedError("Only bash submission information can be parsed at this point.")
+def _parse_submission_system(submission_cmd):
+    submission_cmd = submission_cmd.strip()
+    if submission_cmd.startswith("s"):
+        return "slurm"
+    if submission_cmd.startswith("bash"):
+        return "bash"
+    if submission_cmd.startswith("zsh"):
+        return "zsh"
+    raise NotImplementedError(f"Unable to parse {submission_cmd=} into a submission system.")
+
+
+def _is_finished_slurm_status(status_str):
+    match status_str:
+        case "COMPLETED":
+            return True
+        case str(cancelled_or_failed) if cancelled_or_failed == "FAILED" or cancelled_or_failed.startswith("CANCELLED"):
+            raise RuntimeError(f"Slurm job did not complete successfully but with status {status_str=}.")
+        case "PENDING" | "RUNNING":
+            return False
+    raise NotImplementedError(f"Unable to parse {status_str=} into slurm status.")
+
+
+def _make_wait_function_from(submission_information, submission_cmd="bash"):
+    with submission_information.open("r") as file:
+        pid = int(file.read().split()[-1])
+    match _parse_submission_system(submission_cmd):
+        case "bash" | "zsh":
+            return lambda: not pid_exists(pid)
+        case "slurm":
+            return lambda: any(
+                map(
+                    lambda s: s.startswith(str(pid)) and _is_finished_slurm_status(s.split("|")[1]),
+                    # There are alternatives to running a subprocess ourselves but they aren't better at the time of writing:
+                    # - pyslurm: Links against slurm development libraries which are not necessarily installed on a cluster.
+                    # - simple_slurm: Just wraps the subprocess.run calls and doesn't have the full interface implemented.
+                    run(["sacct", "-S", "2026-01-01", "-XPno", "jobid,state"], capture_output=True)
+                    .stdout.decode()
+                    .split("\n"),
+                )
+            )
+        case _:
+            raise NotImplementedError("Only bash submission information can be parsed at this point.")
 
 
 def gather_results(result_path: Path):
     # job has to finish
-    _wait_until(_make_wait_function_from(result_path / "submission_information.txt", "bash"))
+    _wait_until(
+        _make_wait_function_from(result_path / "submission_information.txt", rc_params.get("tbg_submit", "bash"))
+    )
     # CWLtool has to copy over the files
     _wait_until(lambda: (result_path / "link_results.sh").exists())
 
