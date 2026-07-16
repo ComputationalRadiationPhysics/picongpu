751787ad3b81f9903c668aefe90e5e32b76c8584
Julian Lenz <j.lenz@hzdr.de>
2026-07-16 17:22:25 +0200

    docs: add sphinx-design extension to conf.py
    Co-authored-by: chillenzer-agents <326908604+chillenzer-agents@users.noreply.github.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,6 +45,7 @@ show_authors = True
 extensions = [
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
+    "sphinx-design",
     "breathe",
     "sphinxcontrib.programoutput",
     "matplotlib.sphinxext.plot_directive",
