3dc34c9bbcee8e8d60a283eb8af4bafc3c9933d9
Edgar Marquardt <edgar-marquardt@gmx.de>
2026-08-12 09:51:51 +0200

    Removing the show_test functions from the test module, using np.testing.assert_allclose now and improving readability.

--- lib/python/test/picongpu/quick/extra/input/test_prepareLasyLaser.py
+++ lib/python/test/picongpu/quick/extra/input/test_prepareLasyLaser.py
@@ -33,17 +33,6 @@ w0 = 5e-4
 tau0 = 1e-13
 
 
-# functions to change some params
-def set_xyt_npoints(npoints):
-    global xyt_npoints
-    xyt_npoints = npoints
-
-
-def set_rt_npoints(npoints):
-    global rt_npoints
-    rt_npoints = npoints
-
-
 # helper functions
 def read_file(
     directory, filename, iteration=0, direction="x", cleanup=True, reshape_npoints=xyt_npoints, ret_ext=False
@@ -68,7 +57,6 @@ def read_file(
 
 def get_grid_axes(dim, Nt=None):
     """returns the axes of the grid following the test params"""
-
     if dim == "xyt":
         if Nt is not None:
             npoints = (xyt_npoints[0], xyt_npoints[1], Nt)
@@ -113,9 +101,12 @@ def grid_cell_volume(dim, Nt=None):
         return dV[:, None]
 
 
-def normalize_energy(dim, field, energy, Nt=None):
+def scale_to_energy(dim, field, energy, Nt=None):
     """returns the same field but scaled to have the given energy"""
     dV = grid_cell_volume(dim, Nt=Nt)
+    # np.abs(field) ** 2 is already the complete energy density of the laser field,
+    # because the B-field is found in the imaginary component of the field in this complex notation.
+    # Of course, this assumes only one direction of the E-field and one direction of the B-field being > 0.
     E_curr = np.sum(np.abs(field) ** 2 * dV) * epsilon_0 / 2
     scale = np.sqrt(energy / E_curr)
 
@@ -136,7 +127,7 @@ def gaussian_pulse(dim, Nt=None):
         r = axes[0]
         tr_gauss = np.exp(-r * r / (w0 * w0))
 
-    E = np.real(normalize_energy(dim, t_gauss * tr_gauss, E_laser, Nt=Nt))
+    E = np.real(scale_to_energy(dim, t_gauss * tr_gauss, E_laser, Nt=Nt))
 
     return E
 
@@ -168,16 +159,31 @@ def double_inputs(lo, hi, npoints):
     return lo, hi, npoints
 
 
-def isclose(f1, f2, rtol=1e-5):
-    """determines, whether the fields f1 and f2 are close to being f1 = const * f2 with some constant."""
-    maxerr = np.max(np.abs((f1 - f2) / f2))
-    print("maximum relative error is " + str(maxerr) + "; should be less than " + str(rtol))
-    return np.all(np.isclose(f1, f2, rtol=rtol))
+def assert_isclose(actual, desired, rtol=1e-7):
+    """determines, whether the fields actual (using Lasy and pll) and desired (using gaussian_pulse) are close with a maximum
+    relative error below rtol. Otherwise display the fields for further investigation."""
+    try:
+        np.testing.assert_allclose(actual, desired, rtol=rtol)
+    except AssertionError as error:
+        assert actual.shape == desired.shape
+        if len(actual.shape) == 3:
+            print("A sample of the mismatching fields at [:, :, actual.shape[2] // 2]:")
+            show(np.abs(actual)[:, :, actual.shape[2] // 2], marks=["x", "y", "", "E_lasy"], norm="log")
+            show(np.abs(desired)[:, :, desired.shape[2] // 2], marks=["x", "y", "", "E_gauss"], norm="log")
+            show(
+                np.abs((actual - desired) / desired)[:, :, desired.shape[2] // 2],
+                marks=["x", "y", "", "relative error"],
+                norm="log",
+            )
+        else:
+            print("The mismatching fields:")
+            show(np.abs(actual), marks=["t", "r", "", "E_lasy"], norm="log")
+            show(np.abs(desired), marks=["t", "r", "", "E_gauss"], norm="log")
+            show(np.abs((actual - desired) / desired), marks=["t", "r", "", "relative error"], norm="log")
+        raise error
 
 
 # test functions
-# show_test_* functions display the fields that are compared in the end to see, where the problems may be.
-# That is a lot of output, though.
 def test_basic():
     """test basic process"""
     laser = Laser("xyt", xyt_lo, xyt_hi, xyt_npoints, GaussianProfile(lambda_0, (1, 0), E_laser, w0, tau0, 0))
@@ -186,28 +192,7 @@ def test_basic():
     pll.laser_to_openPMD(laser, "test_laser", write_dir=td.name)
 
     E = read_file(td, "test_laser.bp")
-    assert isclose(E, gaussian_pulse("xyt"))
-
-
-def show_test_basic():
-    """test basic process"""
-    laser = Laser("xyt", xyt_lo, xyt_hi, xyt_npoints, GaussianProfile(lambda_0, (1, 0), E_laser, w0, tau0, 0))
-
-    td = tempfile.TemporaryDirectory()
-    pll.laser_to_openPMD(laser, "test_laser", write_dir=td.name)
-
-    E = read_file(td, "test_laser.bp")
-    E_th = gaussian_pulse("xyt")
-
-    for i in range(E.shape[-1]):
-        show(np.abs(E)[:, :, i], marks=["x", "y", "", "E_lasy " + str(i)], norm="log")
-        show(np.abs(E_th)[:, :, i], marks=["x", "y", "", "E_gauss " + str(i)], norm="log")
-        show(np.abs((E - E_th) / E_th)[:, :, i], marks=["x", "y", "", "error " + str(i)], norm="log")
-        print("""
-
-
-        """)
-    assert isclose(E, E_th)
+    assert_isclose(E, gaussian_pulse("xyt"))
 
 
 def test_rt_to_xyt():
@@ -219,33 +204,7 @@ def test_rt_to_xyt():
 
     E = read_file(td, "test_laser.bp")
     # tolerance because of linear interpolation. it can not get much better.
-    assert isclose(E, gaussian_pulse("xyt"), rtol=2e-2)
-
-
-def show_test_rt_to_xyt():
-    """test rt->xyt conversion and show effects"""
-    laser = Laser("rt", rt_lo, rt_hi, rt_npoints, GaussianProfile(lambda_0, (1, 0), E_laser, w0, tau0, 0))
-
-    td = tempfile.TemporaryDirectory()
-    pll.laser_to_openPMD(laser, "test_laser", write_dir=td.name, Nx=xyt_npoints[0], Ny=xyt_npoints[1])
-
-    show(np.abs(pll.get_full_field(laser)[0]), norm="log")
-
-    E, ext = read_file(td, "test_laser.bp", ret_ext=True)
-    E_th = gaussian_pulse("xyt")
-
-    for i in range(E.shape[-1]):
-        show(np.abs(E)[:, :, i], marks=["x", "y", "", "E_lasy " + str(i)], norm="log")
-        show(np.abs(E_th)[:, :, i], marks=["x", "y", "", "E_gauss " + str(i)], norm="log")
-        show(np.abs((E - E_th) / E_th)[:, :, i], marks=["x", "y", "", "error " + str(i)], norm="log")
-        print("""
-
-
-        """)
-    print(ext)
-
-    # tolerance because of linear interpolation. it can not get much better.
-    assert isclose(E, E_th, rtol=2e-2)
+    assert_isclose(E, gaussian_pulse("xyt"), rtol=2e-2)
 
 
 def test_rt_to_xyt_advanced():
@@ -260,37 +219,9 @@ def test_rt_to_xyt_advanced():
     )
 
     E, ext = read_file(td, "test_laser.bp", ret_ext=True)
-    assert np.all(np.isclose(ext, [xyt_lo, xyt_hi]))
+    assert_isclose(np.array(ext), np.array([xyt_lo, xyt_hi]))
     # tolerance because of linear interpolation. it can not get much better.
-    assert isclose(E, gaussian_pulse("xyt"), rtol=2e-2)
-
-
-def show_test_rt_to_xyt_advanced():
-    """test rt->xyt conversion with all the options"""
-    laser = Laser(
-        "rt", rt_lo, rt_hi, (rt_npoints[0] * 2, rt_npoints[1]), GaussianProfile(lambda_0, (1, 0), E_laser, w0, tau0, 0)
-    )
-
-    td = tempfile.TemporaryDirectory()
-    pll.laser_to_openPMD(
-        laser, "test_laser", write_dir=td.name, Nx=xyt_npoints[0], Ny=xyt_npoints[1], points_between_r=0.5
-    )
-
-    E, ext = read_file(td, "test_laser.bp", ret_ext=True)
-    E_th = gaussian_pulse("xyt")
-
-    for i in range(E.shape[-1]):
-        show(np.abs(E)[:, :, i], marks=["x", "y", "", "E_lasy " + str(i)], norm="log")
-        show(np.abs(E_th)[:, :, i], marks=["x", "y", "", "E_gauss " + str(i)], norm="log")
-        show(np.abs((E - E_th) / E_th)[:, :, i], marks=["x", "y", "", "error " + str(i)], norm="log")
-        print("""
-
-
-        """)
-    print(ext)
-    assert np.all(np.isclose(ext, [xyt_lo, xyt_hi]))
-    # tolerance because of linear interpolation. it can not get much better.
-    assert isclose(E, E_th, rtol=2e-2)
+    assert_isclose(E, gaussian_pulse("xyt"), rtol=2e-2)
 
 
 def test_field_rt():
@@ -299,20 +230,7 @@ def test_field_rt():
     E_th = gaussian_pulse("rt")
     E, ext = pll.get_full_field(laser)
 
-    assert isclose(E, E_th)
-
-
-def show_test_field_rt():
-    """test get_full_field for dim rt"""
-    laser = Laser("rt", rt_lo, rt_hi, rt_npoints, GaussianProfile(lambda_0, (1, 0), E_laser, w0, tau0, 0))
-    E_th = gaussian_pulse("rt")
-    E, ext = pll.get_full_field(laser)
-
-    show(np.abs(E)[:, :], marks=["x", "y", "", "E_lasy"], norm="log")
-    show(np.abs(E_th)[:, :], marks=["x", "y", "", "E_gauss"], norm="log")
-    show(np.abs((E - E_th) / E_th)[:, :], marks=["x", "y", "", "error"], norm="log")
-
-    assert isclose(E, E_th)
+    assert_isclose(E, E_th)
 
 
 def test_field_xyt():
@@ -321,20 +239,7 @@ def test_field_xyt():
     E_th = gaussian_pulse("xyt")
     E, ext = pll.get_full_field(laser)
 
-    assert isclose(E, E_th)
-
-
-def show_test_field_xyt():
-    """test get_full_field for dim xyt"""
-    laser = Laser("xyt", xyt_lo, xyt_hi, xyt_npoints, GaussianProfile(lambda_0, (1, 0), E_laser, w0, tau0, 0))
-    E_th = gaussian_pulse("xyt")
-    E, ext = pll.get_full_field(laser)
-
-    show(np.abs(E)[:, :], marks=["x", "y", "", "E_lasy"], norm="log")
-    show(np.abs(E_th)[:, :], marks=["x", "y", "", "E_gauss"], norm="log")
-    show(np.abs((E - E_th) / E_th)[:, :], marks=["x", "y", "", "error"], norm="log")
-
-    assert isclose(E, E_th)
+    assert_isclose(E, E_th)
 
 
 def test_t_interpolate():
@@ -346,29 +251,7 @@ def test_t_interpolate():
 
     E = read_file(td, "test_laser.bp", reshape_npoints=(xyt_npoints[0], xyt_npoints[1], 20))
     # tolerance because of linear interpolation. it can not get much better.
-    assert isclose(E, gaussian_pulse("xyt", Nt=20), rtol=2e-2)
-
-
-def show_test_t_interpolate():
-    """test the basic process with time interpolation"""
-    laser = Laser("xyt", xyt_lo, xyt_hi, xyt_npoints, GaussianProfile(lambda_0, (1, 0), E_laser, w0, tau0, 0))
-
-    td = tempfile.TemporaryDirectory()
-    pll.laser_to_openPMD(laser, "test_laser", write_dir=td.name, Nt=20)
-
-    E = read_file(td, "test_laser.bp", reshape_npoints=(xyt_npoints[0], xyt_npoints[1], 20))
-    E_th = gaussian_pulse("xyt", Nt=20)
-
-    for i in range(E.shape[0]):
-        show(np.abs(E)[i, :, :], marks=["x", "y", "", "E_lasy " + str(i)], norm="log")
-        show(np.abs(E_th)[i, :, :], marks=["x", "y", "", "E_gauss " + str(i)], norm="log")
-        show(np.abs((E - E_th) / E_th)[i, :, :], marks=["x", "y", "", "error " + str(i)], norm="log")
-        print("""
-
-
-        """)
-    # tolerance because of linear interpolation. it can not get much better.
-    assert isclose(E, E_th, rtol=2e-2)
+    assert_isclose(E, gaussian_pulse("xyt", Nt=20), rtol=2e-2)
 
 
 def test_cut_xy():
@@ -379,33 +262,10 @@ def test_cut_xy():
     E_th = gaussian_pulse("xyt")
     E, ext = pll.get_full_field(laser, Nx=xyt_npoints[0], Ny=xyt_npoints[1])
 
-    assert np.all(np.isclose(ext, [xyt_lo[2], xyt_hi[2], xyt_lo[1], xyt_hi[1], xyt_lo[0], xyt_hi[0]]))
+    assert_isclose(np.array(ext), np.array([xyt_lo[2], xyt_hi[2], xyt_lo[1], xyt_hi[1], xyt_lo[0], xyt_hi[0]]))
     # tolerance because of slightly different normalisation
     # (A different amount of field is used for the total energy calculation)
-    assert isclose(E, E_th, rtol=5e-5)
-
-
-def show_test_cut_xy():
-    """test the cutting in the x and y directions in the full_field method"""
-    laser = Laser(
-        "xyt", *double_inputs(xyt_lo, xyt_hi, xyt_npoints), GaussianProfile(lambda_0, (1, 0), E_laser, w0, tau0, 0)
-    )
-    E_th = gaussian_pulse("xyt")
-    E, ext = pll.get_full_field(laser, Nx=xyt_npoints[0], Ny=xyt_npoints[1])
-    print(ext)
-    assert np.all(np.isclose(ext, [xyt_lo[2], xyt_hi[2], xyt_lo[1], xyt_hi[1], xyt_lo[0], xyt_hi[0]]))
-
-    for i in range(E.shape[0]):
-        show(np.abs(E[i, :, :]), marks=["x", "y", "", "E_lasy " + str(i)], norm="log")
-        show(np.abs(E_th[i, :, :]), marks=["x", "y", "", "E_gauss " + str(i)], norm="log")
-        show(np.abs((E - E_th) / E_th)[i, :, :], marks=["x", "y", "", "error " + str(i)], norm="log")
-        print("""
-
-
-        """)
-    # tolerance because of slightly different normalisation
-    # (A different amount of field is used for the total energy calculation)
-    assert isclose(E, E_th, rtol=5e-5)
+    assert_isclose(E, E_th, rtol=5e-5)
 
 
 def test_gaussian_pulse_xyt():
@@ -413,7 +273,7 @@ def test_gaussian_pulse_xyt():
     profile = GaussianProfile(lambda_0, (1, 0), E_laser, w0, tau0, 0)
     axes = get_grid_axes("xyt")
     E = profile.evaluate(*axes)
-    E = np.real(normalize_energy("xyt", E * np.exp(-2j * np.pi * axes[-1] * c / lambda_0), E_laser))
+    E = np.real(scale_to_energy("xyt", E * np.exp(-2j * np.pi * axes[-1] * c / lambda_0), E_laser))
 
     E_th = gaussian_pulse("xyt")
-    assert np.all(np.isclose(E, E_th))
+    assert_isclose(E, E_th)
